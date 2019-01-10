54f594d720f6a8b0af66727cc6bbbb6cc0c50f3a
André Kilchenmann <a.kilchenmann@unibas.ch>
2019-01-10 09:22:35 +0100

    fix (config): fix performance issue with knora web api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     production: false,
     appName: 'KUIRL',
     appUrl: 'http://localhost:4200',
-    apiUrl: 'http://0.0.0.0:3333',
+    apiUrl: 'http://localhost:3333',
     iiifUrl: 'http://localhost:1024'
 };
 
