5ef72d2e72233cbcc028b0e01a4f996f39fa74df
André Kilchenmann <github@milchkannen.ch>
2019-03-12 14:49:38 +0100

    test: fix issue in test file

--- src/app/main/dialog/confirm-dialog/confirm-dialog.component.spec.ts
+++ src/app/main/dialog/confirm-dialog/confirm-dialog.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ConfirmComponent } from './confirm-dialog.component';
+import { ConfirmDialogComponent } from './confirm-dialog.component';
 
 describe('ConfirmComponent', () => {
-  let component: ConfirmComponent;
-  let fixture: ComponentFixture<ConfirmComponent>;
+  let component: ConfirmDialogComponent;
+  let fixture: ComponentFixture<ConfirmDialogComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfirmComponent ]
+      declarations: [ ConfirmDialogComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ConfirmComponent);
+    fixture = TestBed.createComponent(ConfirmDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
