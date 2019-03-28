1951d5990db049493659603debabddd86dbc6188
André Kilchenmann <github@milchkannen.ch>
2019-03-28 08:33:13 +0100

    fix(user): bring back gravatar to user menu

--- src/app/user/user-menu/user-menu.component.ts
+++ src/app/user/user-menu/user-menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '@knora/authentication';
 import { MenuItem } from '../../main/declarations/menu-item';
 import { AppGlobal } from 'src/app/app-global';
+import { User, UsersService, ApiServiceError } from '@knora/core';
+import { CacheService } from 'src/app/main/cache/cache.service';
 
 @Component({
   selector: 'app-user-menu',
@@ -10,6 +12,8 @@ import { AppGlobal } from 'src/app/app-global';
 })
 export class UserMenuComponent implements OnInit {
 
+    user: User;
+
     username: string;
 
     sysAdmin: boolean = false;
@@ -17,11 +21,23 @@ export class UserMenuComponent implements OnInit {
     navigation: MenuItem[] = AppGlobal.userNav;
 
 
-    constructor(private _auth: AuthenticationService) {
+    constructor(private _auth: AuthenticationService,
+                private _cache: CacheService,
+                private _usersService: UsersService) {
     }
 
     ngOnInit() {
+        // TODO: fix username... this should be the email
+
         this.username = JSON.parse(localStorage.getItem('session')).user.name;
+        this._cache.get(this.username, this._usersService.getUserByUsername(this.username)).subscribe(
+            (response: User) => {
+                this.user = response;
+            },
+            (error: ApiServiceError) => {
+                console.error(error);
+            }
+        );
         this.sysAdmin = JSON.parse(localStorage.getItem('session')).user.sysAdmin;
         if (this.sysAdmin) {
             this.navigation.push({
