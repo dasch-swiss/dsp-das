c68fefe85675c3ea0d0fbe8ae7bfd0f4143bc595
André Kilchenmann <github@milchkannen.ch>
2019-03-28 08:35:41 +0100

    refactor(user): user menu

--- src/app/user/user-menu/user-menu.component.ts
+++ src/app/user/user-menu/user-menu.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '@knora/authentication';
-import { MenuItem } from '../../main/declarations/menu-item';
+import { ApiServiceError, User, UsersService } from '@knora/core';
 import { AppGlobal } from 'src/app/app-global';
-import { User, UsersService, ApiServiceError } from '@knora/core';
 import { CacheService } from 'src/app/main/cache/cache.service';
+import { MenuItem } from '../../main/declarations/menu-item';
 
 @Component({
-  selector: 'app-user-menu',
-  templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss']
+    selector: 'app-user-menu',
+    templateUrl: './user-menu.component.html',
+    styleUrls: ['./user-menu.component.scss']
 })
 export class UserMenuComponent implements OnInit {
-
     user: User;
 
     username: string;
@@ -20,37 +19,39 @@ export class UserMenuComponent implements OnInit {
 
     navigation: MenuItem[] = AppGlobal.userNav;
 
-
-    constructor(private _auth: AuthenticationService,
-                private _cache: CacheService,
-                private _usersService: UsersService) {
-    }
+    constructor(
+        private _auth: AuthenticationService,
+        private _cache: CacheService,
+        private _usersService: UsersService
+    ) {}
 
     ngOnInit() {
-        // TODO: fix username... this should be the email
-
         this.username = JSON.parse(localStorage.getItem('session')).user.name;
-        this._cache.get(this.username, this._usersService.getUserByUsername(this.username)).subscribe(
-            (response: User) => {
-                this.user = response;
-            },
-            (error: ApiServiceError) => {
-                console.error(error);
-            }
-        );
-        this.sysAdmin = JSON.parse(localStorage.getItem('session')).user.sysAdmin;
-        if (this.sysAdmin) {
-            this.navigation.push({
-                    label: 'System',
-                    route: '/system',
-                    icon: 'all_inbox'
-            });
-        }
+        this._cache
+            .get(
+                this.username,
+                this._usersService.getUserByUsername(this.username)
+            )
+            .subscribe(
+                (response: User) => {
+                    this.user = response;
+                    if (this.user.systemAdmin) {
+                        this.navigation.push({
+                            label: 'System',
+                            route: '/system',
+                            icon: 'all_inbox'
+                        });
+                    }
+                },
+                (error: ApiServiceError) => {
+                    console.error(error);
+                }
+            );
     }
 
     logout() {
         this._auth.logout();
+        // TODO: location.reload is deprecated... find new solution to refresh the whole page
         location.reload(true);
     }
-
 }
