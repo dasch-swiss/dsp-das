82fec4ee564566e0fc961e9460d3ca5c303790d0
André Kilchenmann <github@milchkannen.ch>
2019-04-14 13:56:55 +0200

    test: fix broken tests

--- src/app/project/project-form/project-form.component.spec.ts
+++ src/app/project/project-form/project-form.component.spec.ts
@@ -1,25 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+    MatChipsModule,
+    MatIconModule,
+    MatInputModule
+} from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { KuiActionModule } from '@knora/action';
+import { KuiCoreConfig, KuiCoreConfigToken, KuiCoreModule } from '@knora/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { ProjectFormComponent } from './project-form.component';
 
 describe('ProjectFormComponent', () => {
-  let component: ProjectFormComponent;
-  let fixture: ComponentFixture<ProjectFormComponent>;
+    let component: ProjectFormComponent;
+    let fixture: ComponentFixture<ProjectFormComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProjectFormComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ProjectFormComponent],
+            imports: [
+                KuiActionModule,
+                KuiCoreModule,
+                MatChipsModule,
+                MatIconModule,
+                MatInputModule,
+                ReactiveFormsModule,
+                RouterTestingModule,
+                TranslateModule.forRoot()
+            ],
+            providers: [
+                {
+                    provide: KuiCoreConfigToken,
+                    useValue: KuiCoreConfig
+                }
+            ]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProjectFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProjectFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
