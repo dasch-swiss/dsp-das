78d2313650586eb7b8ba1bc4075c2ade552fa858
André Kilchenmann <github@milchkannen.ch>
2019-04-14 13:40:37 +0200

    refactor: clean up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,15 @@ import { KuiActionModule } from '@knora/action';
 import { KuiAuthenticationModule } from '@knora/authentication';
 import { KuiCoreConfigToken, KuiCoreModule } from '@knora/core';
 import { KuiSearchModule } from '@knora/search';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppInitService } from './app-init.service';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DesignQuestionComponent } from './dev/design-question/design-question.component';
 import { LoremIpsumComponent } from './dev/lorem-ipsum/lorem-ipsum.component';
-
+import { DialogHeaderComponent } from './main/dialog/dialog-header/dialog-header.component';
+import { DialogComponent } from './main/dialog/dialog.component';
 import { ErrorComponent } from './main/error/error.component';
 import { HeaderComponent } from './main/header/header.component';
 import { LoginComponent } from './main/login/login.component';
@@ -25,40 +26,35 @@ import { MaterialModule } from './material-module';
 import { BoardComponent } from './project/board/board.component';
 import { AddUserComponent } from './project/collaboration/add-user/add-user.component';
 import { CollaborationComponent } from './project/collaboration/collaboration.component';
-import { UserListComponent } from './project/collaboration/user-list/user-list.component';
+import { SelectGroupComponent } from './project/collaboration/select-group/select-group.component';
 import { OntologyListComponent } from './project/ontology-list/ontology-list.component';
 import { OntologyComponent } from './project/ontology/ontology.component';
 import { PropertyItemComponent } from './project/ontology/property-item/property-item.component';
 import { PropertyListComponent } from './project/ontology/property-list/property-list.component';
 import { ResourceItemComponent } from './project/ontology/resource-item/resource-item.component';
 import { ResourceListComponent } from './project/ontology/resource-list/resource-list.component';
+import { ResourceTypeComponent } from './project/ontology/resource-type/resource-type.component';
 import { SelectItemComponent } from './project/ontology/select-item/select-item.component';
 import { SelectListComponent } from './project/ontology/select-list/select-list.component';
 import { ProjectFormComponent } from './project/project-form/project-form.component';
+import { ProjectMenuComponent } from './project/project-menu/project-menu.component';
 import { ProjectComponent } from './project/project.component';
+import { ProjectsListComponent } from './system/projects/projects-list/projects-list.component';
+import { ProjectsComponent } from './system/projects/projects.component';
+import { StatusComponent } from './system/status/status.component';
+import { SystemComponent } from './system/system.component';
+import { UsersListComponent } from './system/users/users-list/users-list.component';
+import { UsersComponent } from './system/users/users.component';
 import { AccountComponent } from './user/account/account.component';
 import { CollectionListComponent } from './user/collection-list/collection-list.component';
 import { CreateMenuComponent } from './user/create-menu/create-menu.component';
+import { DashboardComponent } from './user/dashboard/dashboard.component';
 import { ProfileComponent } from './user/profile/profile.component';
-import { ProjectsComponent } from './system/projects/projects.component';
-import { ProjectsListComponent } from './system/projects/projects-list/projects-list.component';
 import { UserFormComponent } from './user/user-form/user-form.component';
 import { UserPasswordComponent } from './user/user-form/user-password/user-password.component';
 import { UserMenuComponent } from './user/user-menu/user-menu.component';
 import { UserComponent } from './user/user.component';
-import { SelectGroupComponent } from './project/collaboration/select-group/select-group.component';
-
-import { ResourceTypeComponent } from './project/ontology/resource-type/resource-type.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
-import { DesignQuestionComponent } from './dev/design-question/design-question.component';
-import { DashboardComponent } from './user/dashboard/dashboard.component';
-import { ProjectMenuComponent } from './project/project-menu/project-menu.component';
-import { DialogComponent } from './main/dialog/dialog.component';
-import { SystemComponent } from './system/system.component';
-import { UsersComponent } from './system/users/users.component';
-import { StatusComponent } from './system/status/status.component';
-import { UsersListComponent } from './system/users/users-list/users-list.component';
-import { DialogHeaderComponent } from './main/dialog/dialog-header/dialog-header.component';
 
 // Translate: AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
@@ -79,7 +75,6 @@ export function initializeApp(appInitService: AppInitService) {
         ProjectFormComponent,
         CollaborationComponent,
         AddUserComponent,
-        UserListComponent,
         OntologyListComponent,
         OntologyComponent,
         PropertyListComponent,
@@ -131,10 +126,7 @@ export function initializeApp(appInitService: AppInitService) {
         ReactiveFormsModule,
         TranslateModule.forRoot()
     ],
-    entryComponents: [
-        DialogComponent,
-        ResourceTypeComponent
-    ],
+    entryComponents: [DialogComponent, ResourceTypeComponent],
     providers: [
         AppInitService,
         {
