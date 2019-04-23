9e47d02bee72be4251a237e4217108a587caf946
André Kilchenmann <github@milchkannen.ch>
2019-04-23 17:07:10 +0200

    feat(landing-page): cookie policy

--- src/app/main/cookie-policy/cookie-policy.component.ts
+++ src/app/main/cookie-policy/cookie-policy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-cookie-policy',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CookiePolicyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this._location.back();
   }
 
 }
