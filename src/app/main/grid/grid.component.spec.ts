6c62489dc2665c8e468ff4901b50f83359d31427
André Kilchenmann <github@milchkannen.ch>
2019-06-08 13:50:06 +0200

    test: Fix broken tests

--- src/app/main/grid/grid.component.spec.ts
+++ src/app/main/grid/grid.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GridComponent } from './grid.component';
 import { MatIconModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('GridComponent', () => {
   let component: GridComponent;
@@ -13,10 +14,11 @@ describe('GridComponent', () => {
         GridComponent
       ],
       imports: [
-        MatIconModule
+        MatIconModule,
+        RouterTestingModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
