4689ef739bb4137ef0be5366c5c7c9cb9ebcd2e5
André Kilchenmann <github@milchkannen.ch>
2019-06-14 00:11:31 +0200

    test(codacy): add eslintrc to configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-    parserOptions: {
-      ecmaVersion: 6,
-      project: "./tsconfig.json",
-      sourceType: "module"
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parserOptions: {
+    ecmaVersion: 6,
+    project: "./tsconfig.json",
+    sourceType: "module",
+    "ecmaFeatures": {
+      "jsx": true
     }
   }
+}
