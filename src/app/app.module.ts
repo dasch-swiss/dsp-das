bbf4562cc4f0f40d42f2470359c9c6372d1e5bc6
André Kilchenmann <github@milchkannen.ch>
2019-06-18 09:01:55 +0200

    feat(system/project): Init groups list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,6 +61,8 @@ import { GridComponent } from './main/grid/grid.component';
 import { ResourceComponent } from './workspace/resource/resource.component';
 import { CookiePolicyComponent } from './main/cookie-policy/cookie-policy.component';
 import { InfoMenuComponent } from './main/info-menu/info-menu.component';
+import { GroupsComponent } from './system/groups/groups.component';
+import { GroupsListComponent } from './system/groups/groups-list/groups-list.component';
 
 // translate: AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
@@ -113,7 +115,6 @@ export function initializeApp(appInitService: AppInitService) {
         ResourceComponent,
         ExpertSearchComponent,
         AdvancedSearchComponent,
-
         DashboardComponent,
         DialogComponent,
         SystemComponent,
@@ -123,7 +124,9 @@ export function initializeApp(appInitService: AppInitService) {
         DialogHeaderComponent,
         GridComponent,
         CookiePolicyComponent,
-        InfoMenuComponent
+        InfoMenuComponent,
+        GroupsComponent,
+        GroupsListComponent
     ],
     imports: [
         AppRoutingModule,
---
+++ src/app/system/groups/groups-list/groups-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-groups-list',
+  templateUrl: './groups-list.component.html',
+  styleUrls: ['./groups-list.component.scss']
+})
+export class GroupsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
