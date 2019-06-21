4176afd4ba17b73bfe2d54222f0a828c8a9a4810
André Kilchenmann <github@milchkannen.ch>
2019-06-21 10:14:15 +0200

    feat(main): Show only important projects and sort by longname

--- src/app/system/projects/projects-list/projects-list.component.ts
+++ src/app/system/projects/projects-list/projects-list.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material';
 import { Router } from '@angular/router';
-import { KnoraConstants, Project, User, ProjectsService, ApiServiceError } from '@knora/core';
-import { MatDialogConfig, MatDialog } from '@angular/material';
-import { DialogComponent } from '../../../main/dialog/dialog.component';
 import { Session } from '@knora/authentication';
+import { ApiServiceError, KnoraConstants, Project, ProjectsService, User } from '@knora/core';
 import { CacheService } from 'src/app/main/cache/cache.service';
+import { DialogComponent } from '../../../main/dialog/dialog.component';
 
 @Component({
     selector: 'app-projects-list',
