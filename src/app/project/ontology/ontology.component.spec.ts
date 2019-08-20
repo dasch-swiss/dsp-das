1bd4b67b020a6cc44b760a01b33b0c0b079c77d7
André Kilchenmann <github@milchkannen.ch>
2019-08-20 13:28:11 +0200

    test(ontology): Fix broken tests

--- src/app/project/ontology/ontology.component.spec.ts
+++ src/app/project/ontology/ontology.component.spec.ts
@@ -2,28 +2,30 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OntologyComponent } from './ontology.component';
 import { MatCardModule } from '@angular/material';
+import { KuiActionModule } from '@knora/action';
 
 describe('OntologyComponent', () => {
-  let component: OntologyComponent;
-  let fixture: ComponentFixture<OntologyComponent>;
+    let component: OntologyComponent;
+    let fixture: ComponentFixture<OntologyComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OntologyComponent ],
-      imports: [
-        MatCardModule
-      ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [OntologyComponent],
+            imports: [
+                KuiActionModule,
+                MatCardModule
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OntologyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(OntologyComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
