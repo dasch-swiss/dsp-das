7921ea9449a24ed17b9759b29b6f9acd0f18aee6
André Kilchenmann <github@milchkannen.ch>
2019-09-22 08:00:37 +0200

    test: Fix broken tests

--- src/app/main/footer/footer.component.spec.ts
+++ src/app/main/footer/footer.component.spec.ts
@@ -1,25 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { MatIconModule } from '@angular/material';
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+    let component: FooterComponent;
+    let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [FooterComponent],
+            imports: [
+                MatIconModule
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FooterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/main/info-menu/info-menu.component.spec.ts
+++ src/app/main/info-menu/info-menu.component.spec.ts
@@ -4,42 +4,44 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterTestingModule } from '@angular/router/testing';
 import { KuiCoreConfig, KuiCoreConfigToken, KuiCoreModule } from '@knora/core';
 import { InfoMenuComponent } from './info-menu.component';
 
 
 describe('InfoMenuComponent', () => {
-  let component: InfoMenuComponent;
-  let fixture: ComponentFixture<InfoMenuComponent>;
+    let component: InfoMenuComponent;
+    let fixture: ComponentFixture<InfoMenuComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [InfoMenuComponent],
-      imports: [
-        HttpClientTestingModule,
-        KuiCoreModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatMenuModule
-      ],
-      providers: [
-        {
-          provide: KuiCoreConfigToken,
-          useValue: KuiCoreConfig
-        }
-      ]
-    })
-      .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [InfoMenuComponent],
+            imports: [
+                HttpClientTestingModule,
+                KuiCoreModule,
+                MatButtonModule,
+                MatIconModule,
+                MatListModule,
+                MatMenuModule,
+                RouterTestingModule
+            ],
+            providers: [
+                {
+                    provide: KuiCoreConfigToken,
+                    useValue: KuiCoreConfig
+                }
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InfoMenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(InfoMenuComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
