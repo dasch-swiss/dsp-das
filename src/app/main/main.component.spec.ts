ee4c0fc68afdeb7c4f269c1fe114a2bf0677cec9
André Kilchenmann <github@milchkannen.ch>
2019-11-12 21:15:09 +0100

    test: Try to fix test

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -6,9 +6,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterTestingModule } from '@angular/router/testing';
 import { KuiActionModule } from '@knora/action';
-import { KnoraApiConnection } from '@knora/api';
+import { KnoraApiConnection, KnoraApiConfig } from '@knora/api';
 import { KuiAuthenticationModule } from '@knora/authentication';
-import { KnoraApiConnectionToken, KuiConfigToken } from '@knora/core';
+import { KnoraApiConnectionToken, KnoraApiConfigToken } from '@knora/core';
 import { AppInitService } from '../app-init.service';
 import { FooterComponent } from './footer/footer.component';
 import { GridComponent } from './grid/grid.component';
@@ -34,8 +34,8 @@ fdescribe('MainComponent', () => {
             providers: [
                 AppInitService,
                 {
-                    provide: KuiConfigToken,
-                    useValue: AppInitService.kuiConfig
+                    provide: KnoraApiConfigToken,
+                    useValue: KnoraApiConfig
                 },
                 {
                     provide: KnoraApiConnectionToken,
@@ -46,12 +46,15 @@ fdescribe('MainComponent', () => {
     }));
 
     beforeEach(() => {
+
+        // console.log()
+
         fixture = TestBed.createComponent(MainComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    fit('should create', () => {
+    fit('should create', inject([KnoraApiConfigToken], (KnoraApiConfig: KnoraApiConfig) => {
         expect(component).toBeTruthy();
     });
 });
