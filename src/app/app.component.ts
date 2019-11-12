8d3a5b697706192609b398e689940f5999d4bc33
André Kilchenmann <github@milchkannen.ch>
2019-11-12 02:17:56 +0100

    refactor: Delete unused session validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { SessionService } from '@knora/authentication';
 
 @Component({
     selector: 'app-root',
@@ -12,13 +11,12 @@ export class AppComponent implements OnInit {
     session: boolean = false;
 
     constructor(
-        private _titleService: Title,
-        private _session: SessionService) {
+        private _titleService: Title) {
 
         // set the page title
         this._titleService.setTitle('Knora App | DaSCH\'s generic user interface');
 
-        this.session = this._session.validateSession();
+        // this.session = this._session.validateSession();
     }
 
     ngOnInit() {
