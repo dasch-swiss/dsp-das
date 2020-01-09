03f620167477be818e6d7ff9b2b6da2b70350c41
Flavie Laurens <flavilo@hotmail.fr>
2020-01-09 15:12:13 +0100

    test(e2e): Test the main dashboard page

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,85 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('logged out dashboard', () => {
     let page: AppPage;
 
     beforeEach(() => {
         page = new AppPage();
     });
 
-    it('should display welcome message', () => {
+    it('should display welcome title', () => {
         page.navigateTo();
-        expect(page.getParagraphText()).toEqual('Welcome to knora-app!');
+        expect(page.getMainTitle()).toEqual('BRING ALL TOGETHER AND SIMPLIFY YOUR RESEARCH');
+    });
+
+    it('should display help button', () => {
+        page.navigateTo();
+        expect(page.getHelpButton().getText()).toEqual('Help');
+    });
+
+    it('should route to help page', () => {
+        page.navigateTo();
+        page.getHelpButton().click();
+        expect(page.getHelpPageTitle()).toEqual('Need help?');
+    });
+
+    it('should display login button', () => {
+        page.navigateTo();
+        expect(page.getLoginButton().getText()).toEqual('LOGIN');
+    });
+
+    it('should route to login page', () => {
+        page.navigateTo();
+        page.getLoginButton().click();
+        expect(page.getLoginPageTitle()).toEqual('Login here');
+    });
+
+    it('should display accept button in the cookie banner', () => {
+        page.navigateTo();
+        expect(page.getAcceptCookieButton().getText()).toEqual('ACCEPT');
+    });
+
+    it('should go back to home page', () => {
+        page.navigateTo();
+        page.getAcceptCookieButton().click();
+        expect(page.getSubtitle()).toContain('a software framework for storing');
+    });
+
+    it('should display dasch logo', () => {
+        page.navigateTo();
+        expect(page.getUnibasLogo().getAttribute('class')).toEqual('logo logo-unibas');
+    });
+
+    it('should route to the dasch website and check the URL', () => {
+        page.navigateTo();
+
+        browser.getWindowHandle().then(function (parentGUID) {
+            // click the link that opens in a new window
+            element(by.css('img[src="/assets/images/logo-unibas.jpg"]')).click();
+            browser.sleep(5000);
+            // get the all the session ids of the opened tabs
+            browser.getAllWindowHandles().then(function (allGUID) {
+                console.log('Number of tabs opened: ' + allGUID.length);
+                // iterate through the tabs
+                for (const guid of allGUID) {
+                    // find the new browser tab
+                    if (guid !== parentGUID) {
+                        // switch to the tab
+                        browser.switchTo().window(guid);
+                        // break the loop
+                        break;
+                    }
+                }
+                // perform here any actions needed on the new tab
+                expect(browser.driver.getCurrentUrl()).toMatch('https://www.unibas.ch/de');
+
+                // close the new tab
+                browser.close();
+
+                // switch back to the parent tab
+                browser.switchTo().window(parentGUID);
+            });
+        });
     });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,45 @@
 import { browser, by, element } from 'protractor';
+import { dashCaseToCamelCase } from '@angular/compiler/src/util';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getMainTitle() {
+    return element(by.css('app-main h1')).getText();
   }
+
+  getSubtitle() {
+    return element(by.css('app-main h2')).getText();
+  }
+
+  getHelpButton() {
+    return element(by.buttonText('Help'));
+  }
+
+  getHelpPageTitle() {
+    return element(by.css('app-help h1')).getText();
+  }
+
+  getLoginButton() {
+    return element(by.buttonText('LOGIN'));
+  }
+
+  getLoginPageTitle() {
+    return element(by.css('app-login h2')).getText();
+  }
+
+  getAcceptCookieButton() {
+    return element(by.buttonText('ACCEPT'));
+  }
+
+  getUnibasLogo() {
+    return element(by.css('img[src="/assets/images/logo-unibas.jpg"]'));
+  }
+
+  getUnibasWebsite() {
+    return element(by.css('img[title="Universität Basel"]')).getText();
+  }
+
 }
