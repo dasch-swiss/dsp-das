3456e3ee83e6f66e5d9379705777b47e86ae6d28
Flavie Laurens <flavilo@hotmail.fr>
2020-01-10 12:10:29 +0100

    test(e2e): Add e2e tests to test the tool links (except knora and sipi release notes)

--- e2e/src/help.e2e-spec.ts
+++ e2e/src/help.e2e-spec.ts
@@ -1,6 +1,8 @@
 import { HelpPage } from './help.po';
 import { browser, element, by } from 'protractor';
 
+const { version: appVersion, name: appName } = require('../../package.json');
+
 describe('help page', () => {
     let page: HelpPage;
 
@@ -43,6 +45,126 @@ describe('help page', () => {
         });
     });
 
-    // TODO: route to knora-app release note page, knora release note page, sipi release note page, dasch forum page, github page, dasch swiss website page
+    it('should route to knora-app release note page', () => {
+        // const knoraAppVersion = '2.0.0';
+
+        browser.getWindowHandle().then(function (parentGUID) {
+            // click the link that opens in a new window
+            page.getKnoraAppReleaseNotesButton().click();
+            browser.sleep(5000);
+            // get the all the session ids of the opened tabs
+            browser.getAllWindowHandles().then(function (allGUID) {
+                // console.log('Number of tabs opened: ' + allGUID.length);
+                // iterate through the tabs
+                for (const guid of allGUID) {
+                    // find the new browser tab
+                    if (guid !== parentGUID) {
+                        // switch to the tab
+                        browser.switchTo().window(guid);
+                        // break the loop
+                        break;
+                    }
+                }
+                // perform here any actions needed on the new tab
+                expect(browser.driver.getCurrentUrl()).toMatch('https://github.com/dasch-swiss/' + appName + '/releases/tag/v' + appVersion);
+
+                // close the new tab
+                browser.close();
+
+                // switch back to the parent tab
+                browser.switchTo().window(parentGUID);
+            });
+        });
+    });
+
+    it('should route to the dasch forum', () => {
+        browser.getWindowHandle().then(function (parentGUID) {
+            // click the link that opens in a new window
+            page.getDaschForumButton().click();
+            browser.sleep(5000);
+            // get the all the session ids of the opened tabs
+            browser.getAllWindowHandles().then(function (allGUID) {
+                // console.log('Number of tabs opened: ' + allGUID.length);
+                // iterate through the tabs
+                for (const guid of allGUID) {
+                    // find the new browser tab
+                    if (guid !== parentGUID) {
+                        // switch to the tab
+                        browser.switchTo().window(guid);
+                        // break the loop
+                        break;
+                    }
+                }
+                // perform here any actions needed on the new tab
+                expect(browser.driver.getCurrentUrl()).toMatch('https://discuss.dasch.swiss/');
+
+                // close the new tab
+                browser.close();
+
+                // switch back to the parent tab
+                browser.switchTo().window(parentGUID);
+            });
+        });
+    });
+
+    it('should route to the DaSCH website', () => {
+        browser.getWindowHandle().then(function (parentGUID) {
+            // click the link that opens in a new window
+            page.getDaschSwissButton().click();
+            browser.sleep(5000);
+            // get the all the session ids of the opened tabs
+            browser.getAllWindowHandles().then(function (allGUID) {
+                // console.log('Number of tabs opened: ' + allGUID.length);
+                // iterate through the tabs
+                for (const guid of allGUID) {
+                    // find the new browser tab
+                    if (guid !== parentGUID) {
+                        // switch to the tab
+                        browser.switchTo().window(guid);
+                        // break the loop
+                        break;
+                    }
+                }
+                // perform here any actions needed on the new tab
+                expect(browser.driver.getCurrentUrl()).toMatch('https://dasch.swiss/');
+
+                // close the new tab
+                browser.close();
+
+                // switch back to the parent tab
+                browser.switchTo().window(parentGUID);
+            });
+        });
+    });
+
+    it('should route to the dasch-swiss Github repo', () => {
+        browser.getWindowHandle().then(function (parentGUID) {
+            // click the link that opens in a new window
+            page.getGithubContributeButton().click();
+            browser.sleep(5000);
+            // get the all the session ids of the opened tabs
+            browser.getAllWindowHandles().then(function (allGUID) {
+                // console.log('Number of tabs opened: ' + allGUID.length);
+                // iterate through the tabs
+                for (const guid of allGUID) {
+                    // find the new browser tab
+                    if (guid !== parentGUID) {
+                        // switch to the tab
+                        browser.switchTo().window(guid);
+                        // break the loop
+                        break;
+                    }
+                }
+                // perform here any actions needed on the new tab
+                expect(browser.driver.getCurrentUrl()).toMatch('https://github.com/dasch-swiss');
+
+                // close the new tab
+                browser.close();
+
+                // switch back to the parent tab
+                browser.switchTo().window(parentGUID);
+            });
+        });
+    });
 
 });
--- e2e/src/help.po.ts
+++ e2e/src/help.po.ts
@@ -1,6 +1,7 @@
 import { browser, by, element } from 'protractor';
 
 export class HelpPage {
+
     navigateTo() {
         return browser.get('/help');
     }
@@ -14,6 +15,35 @@ export class HelpPage {
         const projManagementEl = appGridDocEl.get(0);
 
         return projManagementEl.element(by.css('.action .mat-button'));
+    }
+
+    // TODO: grab the version number
+    getKnoraAppReleaseNotesButton() {
+        const appGridProductEl = element.all(by.css('#app-grid-tools .app-grid-item'));
+        const knoraAppEl = appGridProductEl.get(0);
+
+        return knoraAppEl.element(by.css('.action .mat-button'));
+    }
+
+    getDaschForumButton() {
+        const appGridProductEl = element.all(by.css('#app-grid-tools .app-grid-item'));
+        const knoraAppEl = appGridProductEl.get(3);
+
+        return knoraAppEl.element(by.css('.action .mat-button'));
+    }
+
+    getDaschSwissButton() {
+        const appGridProductEl = element.all(by.css('#app-grid-tools .app-grid-item'));
+        const knoraAppEl = appGridProductEl.get(4);
+
+        return knoraAppEl.element(by.css('.action .mat-button'));
+    }
+
+    getGithubContributeButton() {
+        const appGridProductEl = element.all(by.css('#app-grid-tools .app-grid-item'));
+        const knoraAppEl = appGridProductEl.get(5);
+
+        return knoraAppEl.element(by.css('.action .mat-button'));
     }
 
 }
