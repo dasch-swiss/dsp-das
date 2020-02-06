43a749bf63aed3c293366a1f0ab84e1def5a054a
Flavie Laurens <flavilo@hotmail.fr>
2020-02-06 11:40:37 +0100

    test(main): Add tests to main component

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,18 +6,21 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterTestingModule } from '@angular/router/testing';
 import { KuiActionModule } from '@knora/action';
-import { KnoraApiConnection } from '@knora/api';
+import { KnoraApiConnection, ReadProject, ProjectResponse, ApiResponseData, ProjectsResponse } from '@knora/api';
 import { KnoraApiConfigToken, KnoraApiConnectionToken } from '@knora/core';
 import { TestConfig } from 'test.config';
 import { AppInitService } from '../app-init.service';
 import { FooterComponent } from './footer/footer.component';
 import { GridComponent } from './grid/grid.component';
 import { MainComponent } from './main.component';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('MainComponent', () => {
     let component: MainComponent;
     let fixture: ComponentFixture<MainComponent>;
     let element: HTMLElement;
+    let projectSpy: jasmine.Spy;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -45,32 +48,95 @@ describe('MainComponent', () => {
         }).compileComponents();
     }));
 
-    beforeEach(() => {
+    beforeEach(inject([KnoraApiConnectionToken], (knoraApiConn) => {
+        projectSpy = spyOn(knoraApiConn.admin.projectsEndpoint, 'getProjects').and.callFake(
+            () => {
+                const projectList = {
+                    body: {
+                        projects: [
+                            {
+                                id: 'http://rdfh.ch/projects/0801',
+                                description: [
+                                    {
+                                        value: 'Bernoulli-Euler Online'
+                                    }
+                                ],
+                                keywords: [],
+                                logo: null,
+                                longname: 'Bernoulli-Euler Online',
+                                ontologies: [
+                                    'http://www.knora.org/ontology/0801/leibniz',
+                                    'http://www.knora.org/ontology/0801/biblio',
+                                    'http://www.knora.org/ontology/0801/newton',
+                                    'http://www.knora.org/ontology/0801/beol'
+                                ],
+                                selfjoin: false,
+                                shortcode: '0801',
+                                shortname: 'beol',
+                                status: true
+                            },
+                            {
+                                id: 'http://rdfh.ch/projects/0001',
+                                description: [
+                                    {
+                                        value: 'Anything Project'
+                                    }
+                                ],
+                                keywords: [],
+                                logo: null,
+                                longname: 'Anything Project',
+                                ontologies: [
+                                    'http://www.knora.org/ontology/0001/anything',
+                                    'http://www.knora.org/ontology/0001/minimal',
+                                    'http://www.knora.org/ontology/0001/something'
+                                ],
+                                selfjoin: false,
+                                shortcode: '0001',
+                                shortname: 'anything',
+                                status: true
+                            }
+                        ]
+                    }
+                };
+
+                return of(projectList);
+            });
 
         fixture = TestBed.createComponent(MainComponent);
         component = fixture.componentInstance;
         element = fixture.nativeElement; // the HTML reference
         fixture.detectChanges();
-    });
+    }));
 
     it('should create', () => {
         expect(component).toBeTruthy();
+        expect(projectSpy).toHaveBeenCalledTimes(1);
     });
 
     it('should display the title "bring everything together and simplify your research"', () => {
         const h1 = element.querySelector('h1.app-headline');
         expect(h1.textContent).toEqual('bring everything together and simplify your research');
+        expect(projectSpy).toHaveBeenCalledTimes(1);
     });
 
-    // to complete:
-    /* xit('should display public projects', inject([KnoraApiConnectionToken], (knoraApiConn) => {
-        const projectSpy = spyOn(knoraApiConn.admin.projectsEndpoint, 'getProjects').and.callFake(
-            () => {
-                // TODO: mock data
-            });
+    it('should load projects', () => {
+        expect(projectSpy).toHaveBeenCalledTimes(1);
 
-        expect(component).toBeTruthy();
-    })); */
+        expect(component.projects[0].title).toEqual('Bernoulli-Euler Online');
+        expect(component.projects[0].url).toEqual('project/0801');
+    });
 
-    // todo: it should show the cookie banner, display Accept button, not shown again when clicking (show it once, after clicking, not displayed anymore even when the page is refreshed)
+    it('should display the cookie banner', () => {
+        const cookieBanner = fixture.debugElement.query(By.css('div.cookie-banner'));
+        const acceptBtn = fixture.debugElement.query(By.css('div.action button'));
+
+        expect(cookieBanner).toBeDefined();
+        expect(acceptBtn.nativeElement.innerText).toEqual('ACCEPT');
+
+        // todo: find a way to check if the banner is gone after clicking on the button
+        /* acceptBtn.triggerEventHandler('click', {});
+        fixture.detectChanges(); */
+
+    });
+
 });
