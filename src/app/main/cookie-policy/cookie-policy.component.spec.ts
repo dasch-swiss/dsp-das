1607081db5af4d5b5056a418d5163969db0a9ac6
André Kilchenmann <github@milchkannen.ch>
2020-02-07 14:04:43 +0100

    refactor(test): Clean up imports

--- src/app/main/cookie-policy/cookie-policy.component.spec.ts
+++ src/app/main/cookie-policy/cookie-policy.component.spec.ts
@@ -1,54 +1,53 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { Location } from '@angular/common';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-
 import { CookiePolicyComponent } from './cookie-policy.component';
 
+
 describe('CookiePolicyComponent', () => {
-  let component: CookiePolicyComponent;
-  let fixture: ComponentFixture<CookiePolicyComponent>;
-  let element: HTMLElement;
+    let component: CookiePolicyComponent;
+    let fixture: ComponentFixture<CookiePolicyComponent>;
+    let element: HTMLElement;
 
-  const locationStub = {
-    back: jasmine.createSpy('back')
-  };
+    const locationStub = {
+        back: jasmine.createSpy('back')
+    };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [CookiePolicyComponent],
-      imports: [
-        MatIconModule,
-        MatButtonModule,
-        MatDividerModule
-      ],
-      providers: [
-        { provide: Location, useValue: locationStub },
-      ]
-    })
-      .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [CookiePolicyComponent],
+            imports: [
+                MatIconModule,
+                MatButtonModule,
+                MatDividerModule
+            ],
+            providers: [
+                { provide: Location, useValue: locationStub },
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CookiePolicyComponent);
-    component = fixture.componentInstance;
-    element = fixture.nativeElement; // the HTML reference
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CookiePolicyComponent);
+        component = fixture.componentInstance;
+        element = fixture.nativeElement; // the HTML reference
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should display the title "Cookie Policy for Knora web application"', () => {
-    const h1 = element.querySelector('h1');
-    expect(h1.textContent).toEqual('Cookie Policy for Knora web application');
-  });
+    it('should display the title "Cookie Policy for Knora web application"', () => {
+        const h1 = element.querySelector('h1');
+        expect(h1.textContent).toEqual('Cookie Policy for Knora web application');
+    });
 
-  it('should have goBack method and should call location.back', inject([Location], (loc: Location) => {
-    component.goBack();
-    expect(loc.back).toHaveBeenCalledTimes(1);
-  }));
+    it('should have goBack method and should call location.back', inject([Location], (loc: Location) => {
+        component.goBack();
+        expect(loc.back).toHaveBeenCalledTimes(1);
+    }));
 });
