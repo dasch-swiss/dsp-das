cd7765d2ca6f1bd333d41dd03cb93a8ae67c7531
flaurens <flavie.laurens@unibas.ch>
2020-08-05 17:13:39 +0200

    refactor(main.ts): Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,26 +7,5 @@ if (environment.production) {
     enableProdMode();
 }
 
-function bootstrapFailed(response: any) {
-    console.error('bootstrap-fail', response);
-}
-
-fetch(`config/config.${environment.name}.json`)
-    .then(response => response.json())
-    .then(config => {
-        if (!config || !config['knora']) {
-            bootstrapFailed(config);
-            return;
-        }
-
-        // store the response somewhere that your ConfigService can read it.
-        window['tempConfigStorage'] = config;
-
-        // console.log('config', config);
-
-
-        platformBrowserDynamic()
-            .bootstrapModule(AppModule)
-            .catch(err => bootstrapFailed(err));
-    })
-    .catch(bootstrapFailed);
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
