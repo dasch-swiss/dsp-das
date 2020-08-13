bec86a81aaac2fe6c3c8eeac7e6eb12b42a4a7ec
André Kilchenmann <github@milchkannen.ch>
2020-08-13 10:55:02 +0200

    test: Fix tests in main component

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -69,6 +69,25 @@ describe('MainComponent', () => {
         spyOn(sessionStorage, 'clear').and.callFake(() => {
             store = {};
         });
+
+        spyOn(localStorage, 'getItem').and.callFake(
+            (key: string): string => {
+                return store[key] || null;
+            }
+        );
+        spyOn(localStorage, 'removeItem').and.callFake(
+            (key: string): void => {
+                delete store[key];
+            }
+        );
+        spyOn(localStorage, 'setItem').and.callFake(
+            (key: string, value: string): string => {
+                return (store[key] = <any>value);
+            }
+        );
+        spyOn(localStorage, 'clear').and.callFake(() => {
+            store = {};
+        });
     });
 
     beforeEach(inject([DspApiConnectionToken], (knoraApiConn) => {
