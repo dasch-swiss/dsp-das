1136ad0a701c432559383781b429e3ee13427b55
André Kilchenmann <github@milchkannen.ch>
2020-10-30 17:24:46 +0100

    refactor(search): Delete redundant code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,9 +20,7 @@ import { ProjectComponent } from './project/project.component';
 import { DashboardComponent } from './user/dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 
-
-import { AdvancedSearchComponent } from './workspace/search/advanced-search/advanced-search.component';
-import { ExpertSearchComponent } from './workspace/search/expert-search/expert-search.component';
+// search results and resource viewer
 import { ResourceComponent } from './workspace/resource/resource.component';
 import { ResultsComponent } from './workspace/results/results.component';
 
