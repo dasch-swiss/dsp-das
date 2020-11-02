15306fc343ab276434f920118c68db5e490990d2
André Kilchenmann <github@milchkannen.ch>
2020-11-02 11:40:24 +0100

    fix(tests): Fix warnings in tests (missing imports)

--- src/app/project/ontology/ontology-visualizer/ontology-visualizer.component.spec.ts
+++ src/app/project/ontology/ontology-visualizer/ontology-visualizer.component.spec.ts
@@ -1,5 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
 import { RouterTestingModule } from '@angular/router/testing';
 import {
     IHasProperty,
@@ -74,6 +77,9 @@ describe('OntologyVisualizerComponent', () => {
                 HttpClientTestingModule,
                 DspActionModule,
                 DspCoreModule,
+                MatRadioModule,
+                MatSliderModule,
+                MatSlideToggleModule,
                 RouterTestingModule
             ],
             providers: [
