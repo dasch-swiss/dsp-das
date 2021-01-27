0d3c2f6c5156a2abe86bbd83f4f5d6c491285ced
flaurens <flavie.laurens@unibas.ch>
2021-01-27 09:23:51 +0100

    refactor(login): set login page as the landing page + redirect to the dashboard

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SessionService } from '@dasch-swiss/dsp-ui';
 import { ComponentCommunicationEventService, EmitEvent, Events } from 'src/app/main/services/component-communication-event.service';
 
 @Component({
@@ -15,6 +16,7 @@ export class LoginComponent implements OnInit {
     constructor (private _titleService: Title,
         private _route: ActivatedRoute,
         private _router: Router,
+        private _session: SessionService,
         private _componentCommsService: ComponentCommunicationEventService) {
 
         // set the page title
@@ -26,13 +28,21 @@ export class LoginComponent implements OnInit {
     ngOnInit() {
     }
 
-    refresh(status: boolean) {
+    refresh() {
+        // check if a session is active
+        if (this._session.getSession()) {
+            this._router.navigate(['dashboard']);
+        }
+    }
+
+    // TO REPLACE THE METHOD ABOVE ONCE WE WILL USE DSP-APP AS A RESEARCH PLATFORM AGAIN
+    /* refresh(status: boolean) {
 
         // go to previous route:
         if (status) {
             this._router.navigate([this.returnUrl]);
             this._componentCommsService.emit(new EmitEvent(Events.LoginSuccess, true));
         }
-    }
+    } */
 
 }
