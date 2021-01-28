bc62c021ef290c9652b0c2b890829e9f96c43dc5
André Kilchenmann <github@milchkannen.ch>
2021-01-28 15:11:30 +0100

    chore(login): fix login redirection after login and logout

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SessionService } from '@dasch-swiss/dsp-ui';
 import { ComponentCommunicationEventService, EmitEvent, Events } from 'src/app/main/services/component-communication-event.service';
 
 @Component({
@@ -13,25 +12,36 @@ export class LoginComponent {
 
     returnUrl: string;
 
-    constructor (private _titleService: Title,
+    constructor(private _titleService: Title,
         private _route: ActivatedRoute,
         private _router: Router,
-        private _session: SessionService,
         private _componentCommsService: ComponentCommunicationEventService) {
 
         // set the page title
         this._titleService.setTitle('Login');
 
-        // this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/';
+        this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/';
     }
 
-    refresh(status: boolean) {
+    login(status: boolean) {
 
         // go to the dashboard:
-        if (status && this._session.getSession()) {
-            this._router.navigate(['dashboard']);
-            // go to the previous route: this._router.navigate([this.returnUrl]);
+        if (status) {
+            // go to the previous route; if it's not the help page
+            if (this.returnUrl !== 'help') {
+                this._router.navigate([this.returnUrl]);
+            } else {
+                // otherwise go to the dashboard
+                this._router.navigate(['dashboard']);
+            }
             this._componentCommsService.emit(new EmitEvent(Events.LoginSuccess, true));
+        }
+    }
+
+    logout(status: boolean) {
+        if (status) {
+            // reload the page
+            window.location.reload();
         }
     }
 
