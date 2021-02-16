e7c22b6b148ce7a9e2a2d6ee2cfb5181df7ffdff
mdelez <60604010+mdelez@users.noreply.github.com>
2021-02-16 17:59:56 +0100

    test: starting point for unit tests

--- src/app/project/list/list.component.spec.ts
+++ src/app/project/list/list.component.spec.ts
@@ -1,9 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
@@ -13,33 +14,76 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
+import { ApiResponseData, KnoraApiConnection, ListNodeInfo, ListNodeInfoResponse, ListResponse, ListsEndpointAdmin, ListsResponse, MockProjects, ProjectResponse, ProjectsEndpointAdmin, ReadProject, StoredProject, UserResponse } from '@dasch-swiss/dsp-js';
 import {
     AppInitService,
     DspActionModule,
     DspApiConfigToken,
     DspApiConnectionToken,
-    DspCoreModule
+    DspCoreModule,
+    Session,
+    SessionService
 } from '@dasch-swiss/dsp-ui';
 import { of } from 'rxjs';
+import { AjaxResponse } from 'rxjs/ajax';
+import { CacheService } from 'src/app/main/cache/cache.service';
+import { DialogHeaderComponent } from 'src/app/main/dialog/dialog-header/dialog-header.component';
 import { DialogComponent } from 'src/app/main/dialog/dialog.component';
 import { ErrorComponent } from 'src/app/main/error/error.component';
 import { TestConfig } from 'test.config';
-import { ListItemFormComponent } from './list-item-form/list-item-form.component';
-import { ListItemComponent } from './list-item/list-item.component';
 import { ListComponent } from './list.component';
 
-describe('ListComponent', () => {
-    let component: ListComponent;
-    let fixture: ComponentFixture<ListComponent>;
+/**
+ * Test Host Component
+ */
+@Component({
+    template: `<app-list #listComponent></app-list>`
+})
+class TestHostComponent {
+    @ViewChild('listComponent') listComponent: ListComponent;
+}
+
+/**
+ * Mock ListItem.
+ */
+@Component({
+    template: `<app-list-item></app-list-item>`
+})
+class MockListItemComponent { }
+
+/**
+ * Mock ListItemForm.
+ */
+@Component({
+    template: `<app-list-item-form></app-list-item-form>`
+})
+class MockListItemFormComponent { }
+
+fdescribe('ListComponent', () => {
+    let testHostComponent: TestHostComponent;
+    let testHostFixture: ComponentFixture<TestHostComponent>;
 
     beforeEach(async(() => {
+
+        const dspConnSpyObj = {
+            admin: {
+                listsEndpoint: jasmine.createSpyObj('listsEndpoint', ['getListsInProject', 'deleteListNode']),
+                projectsEndpoint: jasmine.createSpyObj('projectsEndpoint', ['getProjectByShortcode'])
+            }
+        };
+
+        const sessionServiceSpy = jasmine.createSpyObj('SessionService', ['getSession']);
+
+        const cacheServiceSpy = jasmine.createSpyObj('CacheService', ['get']);
+
         TestBed.configureTestingModule({
             declarations: [
                 ListComponent,
-                ListItemComponent,
-                ListItemFormComponent,
+                TestHostComponent,
+                MockListItemComponent,
+                MockListItemFormComponent,
                 DialogComponent,
+                DialogHeaderComponent,
                 ErrorComponent
             ],
             imports: [
@@ -74,55 +118,113 @@ describe('ListComponent', () => {
                         }
                     }
                 },
-                AppInitService,
-                {
-                    provide: DspApiConfigToken,
-                    useValue: TestConfig.ApiConfig
-                },
                 {
                     provide: DspApiConnectionToken,
-                    useValue: new KnoraApiConnection(TestConfig.ApiConfig)
+                    useValue: dspConnSpyObj
+                },
+                {
+                    provide: SessionService,
+                    useValue: sessionServiceSpy
+                },
+                {
+                    provide: CacheService,
+                    useValue: cacheServiceSpy
+                },
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {}
+                },
+                {
+                    provide: MatDialogRef,
+                    useValue: {}
                 }
-            ]
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
         }).compileComponents();
     }));
 
-    // mock localStorage
     beforeEach(() => {
-        let store = {};
 
-        spyOn(localStorage, 'getItem').and.callFake(
-            (key: string): string => {
-                return store[key] || null;
+        const sessionSpy = TestBed.inject(SessionService);
+
+        (sessionSpy as jasmine.SpyObj<SessionService>).getSession.and.callFake(
+            () => {
+                const session: Session = {
+                    id: 12345,
+                    user: {
+                        name: 'username',
+                        jwt: 'myToken',
+                        lang: 'en',
+                        sysAdmin: true,
+                        projectAdmin: []
+                    }
+                };
+
+                return session;
             }
         );
-        spyOn(localStorage, 'removeItem').and.callFake(
-            (key: string): void => {
-                delete store[key];
+
+        const cacheSpy = TestBed.inject(CacheService);
+
+        (cacheSpy as jasmine.SpyObj<CacheService>).get.and.callFake(
+            () => {
+                const response: ProjectResponse = new ProjectResponse();
+
+                const mockProjects = MockProjects.mockProjects();
+
+                response.project = mockProjects.body.projects[0];
+
+                return of(ApiResponseData.fromAjaxResponse({response} as AjaxResponse));
             }
         );
-        spyOn(localStorage, 'setItem').and.callFake(
-            (key: string, value: string): string => {
-                return (store[key] = <any>value);
+
+        const dspConnSpy = TestBed.inject(DspApiConnectionToken);
+
+        (dspConnSpy.admin.listsEndpoint as jasmine.SpyObj<ListsEndpointAdmin>).getListsInProject.and.callFake(
+            () => {
+                const response = new ListsResponse();
+
+                response.lists = new Array<ListNodeInfo>();
+
+                const mockList1 = new ListNodeInfo();
+                mockList1.comments = [];
+                mockList1.id = 'http://rdfh.ch/lists/0001/mockList01';
+                mockList1.isRootNode = true;
+                mockList1.labels = [{language: 'en', value: 'Mock List 01'}];
+                mockList1.projectIri = 'http://rdfh.ch/projects/myProjectIri';
+
+                const mockList2 = new ListNodeInfo();
+                mockList2.comments = [];
+                mockList2.id = 'http://rdfh.ch/lists/0001/mockList02';
+                mockList2.isRootNode = true;
+                mockList2.labels = [{language: 'en', value: 'Mock List 02'}];
+                mockList2.projectIri = 'http://rdfh.ch/projects/myProjectIri';
+
+                response.lists.push(mockList1, mockList2);
+
+                return of(ApiResponseData.fromAjaxResponse({response} as AjaxResponse));
             }
         );
-        spyOn(localStorage, 'clear').and.callFake(() => {
-            store = {};
-        });
-    });
 
-    beforeEach(() => {
-        localStorage.setItem('session', JSON.stringify(TestConfig.CurrentSession));
+        testHostFixture = TestBed.createComponent(TestHostComponent);
+        testHostComponent = testHostFixture.componentInstance;
+        testHostFixture.detectChanges();
 
-        fixture = TestBed.createComponent(ListComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        (dspConnSpy.admin.projectsEndpoint as jasmine.SpyObj<ProjectsEndpointAdmin>).getProjectByShortcode.and.callFake(
+            () => {
+                const response = new ProjectResponse();
+
+                const mockProjects = MockProjects.mockProjects();
+
+                response.project = mockProjects.body.projects[0];
+
+                return of(ApiResponseData.fromAjaxResponse({response} as AjaxResponse));
+            }
+        );
     });
 
     it('should create', () => {
-        expect<any>(localStorage.getItem('session')).toBe(
-            JSON.stringify(TestConfig.CurrentSession)
-        );
-        expect(component).toBeTruthy();
+        expect(testHostComponent.listComponent.session).toBeTruthy();
+        expect(testHostComponent).toBeTruthy();
     });
 });
