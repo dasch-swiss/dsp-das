bf7490bda3f1f574db82a87e793319de9c1b8d9e
André Kilchenmann <github@milchkannen.ch>
2021-02-17 17:32:09 +0100

    chore(ontology): update property info component

--- src/app/project/ontology/property-info/property-info.component.ts
+++ src/app/project/ontology/property-info/property-info.component.ts
@@ -1,10 +1,15 @@
-import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, Inject, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, Input, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ApiResponseData, ApiResponseError, Constants, IHasProperty, KnoraApiConnection, ListsResponse, ReadOntology, ResourcePropertyDefinitionWithAllLanguages } from '@dasch-swiss/dsp-js';
-import { DspApiConnectionToken } from '@dasch-swiss/dsp-ui';
+import {
+    ApiResponseData,
+    Constants,
+    IHasProperty,
+    ListsResponse,
+    ReadOntology,
+    ResourcePropertyDefinitionWithAllLanguages
+} from '@dasch-swiss/dsp-js';
 import { CacheService } from 'src/app/main/cache/cache.service';
-import { ErrorHandlerService } from 'src/app/main/error/error-handler.service';
 import { Category, DefaultProperties, PropertyType } from '../default-data/default-properties';
 import { Property } from '../resource-class-form/resource-class-form.service';
 
@@ -31,10 +36,8 @@ export class PropertyInfoComponent implements OnInit, AfterContentInit {
     propAttribute: string;
 
     constructor(
-        @Inject(DspApiConnectionToken) private _dspApiConnection: KnoraApiConnection,
         private _cache: CacheService,
         private _domSanitizer: DomSanitizer,
-        private _errorHandler: ErrorHandlerService,
         private _matIconRegistry: MatIconRegistry
     ) {
 
@@ -94,8 +97,7 @@ export class PropertyInfoComponent implements OnInit, AfterContentInit {
             );
         }
 
-        if(this.propDef.objectType === Constants.ListValue) {
-            console.log(this.propDef)
+        if (this.propDef.objectType === Constants.ListValue) {
             // this property is a list property
             // get current ontology lists to get linked list information
             this._cache.get('currentOntologyLists').subscribe(
@@ -103,8 +105,6 @@ export class PropertyInfoComponent implements OnInit, AfterContentInit {
                     const re: RegExp = /\<([^)]+)\>/;
                     const listIri = this.propDef.guiAttributes[0].match(re)[1];
                     const listUrl = `/project/${this.projectcode}/lists/${encodeURIComponent(listIri)}`;
-                    console.log(listIri)
-                    console.log(response)
                     this.propAttribute = `<a href="${listUrl}">${response[0].labels[0].value}</a>`;
                 }
             );
