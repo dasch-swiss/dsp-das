7abdc04766cc41220c40652c2b278574a40ee2fc
André Kilchenmann <github@milchkannen.ch>
2021-02-24 12:01:12 +0100

    fix(eslint): delete old tslint config rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -184,15 +184,6 @@ module.exports = {
                     "/"
                 ]
             }
-        ],
-        "@typescript-eslint/tslint/config": [
-            "error",
-            {
-                "rules": {
-                    "import-spacing": true,
-                    "whitespace": true
-                }
-            }
         ]
     }
 };
