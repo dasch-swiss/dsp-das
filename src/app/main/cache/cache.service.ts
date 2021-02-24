40fd28cbab0963007e0033d445162c4c102165e0
André Kilchenmann <github@milchkannen.ch>
2021-02-24 09:12:56 +0100

    refactor(main): refactor code by following ESLint rules

--- src/app/main/cache/cache.service.ts
+++ src/app/main/cache/cache.service.ts
@@ -12,21 +12,21 @@ interface CacheContent {
     providedIn: 'root'
 })
 /**
- * Cache Service is an observables based in-memory cache implementation
+ * cache Service is an observables based in-memory cache implementation
  * Keeps track of in-flight observables and sets a default expiry for cached values
  * @export
  * @class CacheService
  */
 export class CacheService {
+    readonly defaultMaxAge: number = 3600000;  // 3600000ms => 1 Stunde
     private cache: Map<string, CacheContent> = new Map<string, CacheContent>();
     private inFlightObservables: Map<string, Subject<any>> = new Map<string, Subject<any>>();
-    readonly DEFAULT_MAX_AGE: number = 3600000;  // 3600000ms => 1 Stunde
 
     constructor(
         private _errorHandler: ErrorHandlerService
     ) { }
     /**
-     * Gets the value from cache if the key is provided.
+     * gets the value from cache if the key is provided.
      * If no value exists in cache, then check if the same call exists
      * in flight, if so return the subject. If not create a new
      * Subject inFlightObservable and return the source observable.
@@ -43,7 +43,7 @@ export class CacheService {
         }
 
         if (!maxAge) {
-            maxAge = this.DEFAULT_MAX_AGE;
+            maxAge = this.defaultMaxAge;
         }
 
         if (this.inFlightObservables.has(key)) {
@@ -77,16 +77,16 @@ export class CacheService {
     }
 
     /**
-     * Sets the value with key in the cache
+     * sets the value with key in the cache
      * Notifies all observers of the new value
      */
-    set(key: string, value: any, maxAge: number = this.DEFAULT_MAX_AGE): void {
+    set(key: string, value: any, maxAge: number = this.defaultMaxAge): void {
         this.cache.set(key, { value: value, expiry: Date.now() + maxAge });
         this.notifyInFlightObservers(key, value);
     }
 
     /**
-     * Checks if the key exists in cache
+     * checks if the key exists in cache
      */
     has(key: string): boolean {
         return this.cache.has(key);
@@ -101,7 +101,7 @@ export class CacheService {
     }
 
     /**
-     * Clear the whole cache
+     * clear the whole cache
      */
     destroy() {
         sessionStorage.clear();
@@ -109,7 +109,7 @@ export class CacheService {
     }
 
     /**
-     * Publishes the value to all observers of the given
+     * publishes the value to all observers of the given
      * in progress observables if observers exist.
      */
     private notifyInFlightObservers(key: string, value: any): void {
@@ -126,7 +126,7 @@ export class CacheService {
     }
 
     /**
-     * Checks if the key exists and   has not expired.
+     * checks if the key exists and   has not expired.
      */
     private hasValidCachedValue(key: string): boolean {
         if (this.cache.has(key)) {
