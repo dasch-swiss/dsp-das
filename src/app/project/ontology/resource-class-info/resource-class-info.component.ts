e1c15ac2a4328aa92a1f35be2333e004062da9bb
André Kilchenmann <github@milchkannen.ch>
2021-03-02 10:57:22 +0100

    refactor(ontology): new res class info setup

--- src/app/project/ontology/resource-class-info/resource-class-info.component.ts
+++ src/app/project/ontology/resource-class-info/resource-class-info.component.ts
@@ -1,6 +1,7 @@
 import { EventEmitter, Input, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { ClassDefinition, ReadOntology } from '@dasch-swiss/dsp-js';
+import { ApiResponseError, ClassDefinition, ReadOntology } from '@dasch-swiss/dsp-js';
+import { CacheService } from 'src/app/main/cache/cache.service';
 import { DefaultInfo } from '../default-data/default-resource-classes';
 
 @Component({
@@ -12,20 +13,29 @@ export class ResourceClassInfoComponent implements OnInit {
 
     @Input() resourceClass: ClassDefinition;
 
-    @Input() ontology: ReadOntology;
-
     @Output() editResourceClass: EventEmitter<DefaultInfo> = new EventEmitter<DefaultInfo>();
     @Output() updateCardinality: EventEmitter<ClassDefinition> = new EventEmitter<ClassDefinition>();
     @Output() deleteResourceClass: EventEmitter<DefaultInfo> = new EventEmitter<DefaultInfo>();
 
+    ontology: ReadOntology;
 
     // open / close res class card
     expanded = false;
 
-    constructor() { }
+    constructor(
+        private _cache: CacheService
+    ) { }
 
     ngOnInit(): void {
-        console.log(this.resourceClass);
+        this._cache.get('currentOntology').subscribe(
+            (response: ReadOntology) => {
+                this.ontology = response;
+            },
+            (error: ApiResponseError) => {
+                console.error(error);
+            }
+        )
+        // console.log(JSON.stringify(this.resourceClass));
     }
 
 }
