de1d68b83deee21e6bf0b2051e3bb42981e33818
André Kilchenmann <github@milchkannen.ch>
2021-03-05 12:03:21 +0100

    chore: replace autocomplete-item with the one from ui-lib

--- src/app/project/collaboration/select-group/select-group.component.ts
+++ src/app/project/collaboration/select-group/select-group.component.ts
@@ -1,9 +1,8 @@
 import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ApiResponseData, ApiResponseError, GroupsResponse, KnoraApiConnection } from '@dasch-swiss/dsp-js';
-import { DspApiConnectionToken } from '@dasch-swiss/dsp-ui';
+import { AutocompleteItem, DspApiConnectionToken } from '@dasch-swiss/dsp-ui';
 import { CacheService } from 'src/app/main/cache/cache.service';
-import { AutocompleteItem } from 'src/app/main/declarations/autocomplete-item';
 import { ErrorHandlerService } from 'src/app/main/error/error-handler.service';
 
 @Component({
