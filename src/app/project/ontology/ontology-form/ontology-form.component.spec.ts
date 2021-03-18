86a5fb819146707d8b44d5f027f2fccfb4b00791
André Kilchenmann <github@milchkannen.ch>
2021-03-18 21:32:19 +0100

    feat(ontology): edit data model info (DSP-1208) (#385)

--- src/app/project/ontology/ontology-form/ontology-form.component.spec.ts
+++ src/app/project/ontology/ontology-form/ontology-form.component.spec.ts
@@ -3,9 +3,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
+import { KnoraApiConnection, MockProjects } from '@dasch-swiss/dsp-js';
 import {
     AppInitService,
     DspActionModule,
@@ -14,23 +15,23 @@ import {
     DspCoreModule
 } from '@dasch-swiss/dsp-ui';
 import { TranslateModule } from '@ngx-translate/core';
-import { DialogComponent } from 'src/app/main/dialog/dialog.component';
-import { ErrorComponent } from 'src/app/main/error/error.component';
+import { of } from 'rxjs';
+import { CacheService } from 'src/app/main/cache/cache.service';
 import { TestConfig } from 'test.config';
 import { OntologyFormComponent } from './ontology-form.component';
 
 describe('OntologyFormComponent', () => {
-    let component: OntologyFormComponent;
-    let fixture: ComponentFixture<OntologyFormComponent>;
+    let ontologyFormComponent: OntologyFormComponent;
+    let ontologyFormFixture: ComponentFixture<OntologyFormComponent>;
 
     const formBuilder: FormBuilder = new FormBuilder();
+
+    const cacheServiceSpy = jasmine.createSpyObj('CacheService', ['get']);
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                OntologyFormComponent,
-                DialogComponent,
-                ErrorComponent
+                OntologyFormComponent
             ],
             imports: [
                 BrowserAnimationsModule,
@@ -54,6 +55,10 @@ describe('OntologyFormComponent', () => {
                 {
                     provide: DspApiConnectionToken,
                     useValue: new KnoraApiConnection(TestConfig.ApiConfig)
+                },
+                {
+                    provide: CacheService,
+                    useValue: cacheServiceSpy
                 }
             ]
         })
@@ -61,19 +66,84 @@ describe('OntologyFormComponent', () => {
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(OntologyFormComponent);
-        component = fixture.componentInstance;
+        // mock cache service for currentOntology
+        const cacheSpy = TestBed.inject(CacheService);
 
-        // pass in the form dynamically
-        component.ontologyForm = formBuilder.group({
-            name: null,
-            label: null
-        });
+        (cacheSpy as jasmine.SpyObj<CacheService>).get.and.callFake(
+            () => {
+                const response = MockProjects.mockProject();
+                return of(response);
+            }
+        );
 
-        fixture.detectChanges();
+        ontologyFormFixture = TestBed.createComponent(OntologyFormComponent);
+        ontologyFormComponent = ontologyFormFixture.componentInstance;
+        ontologyFormComponent.projectcode = '00FF';
+        // ontologyFormComponent.iri = 'http://0.0.0.0:3333/ontology/0001/anything/v2';
+        ontologyFormComponent.existingOntologyNames = ['images'];
+
+        ontologyFormComponent.ngOnInit();
+        ontologyFormFixture.detectChanges();
     });
+
 
     it('should create', () => {
-        expect(component).toBeTruthy();
+        expect(ontologyFormComponent).toBeTruthy();
     });
+
+    it('should render input elements', () => {
+        const compiled = ontologyFormFixture.debugElement;
+        const nameInput = compiled.query(By.css('.ontology-name input'));
+        const labelInput = compiled.query(By.css('.ontology-label input'));
+        const commentInput = compiled.query(By.css('.ontology-comment textarea'));
+
+        expect(nameInput).toBeTruthy();
+        expect(labelInput).toBeTruthy();
+        expect(commentInput).toBeTruthy();
+    });
+
+    it('should test form validity with allowed name', () => {
+        const form = ontologyFormComponent.ontologyForm;
+        expect(form.valid).toBeFalsy();
+
+        const nameInput = form.controls.name;
+        nameInput.setValue('biblio');
+
+        expect(form.valid).toBeTruthy();
+    });
+
+    it('should test form validity with forbidden names', () => {
+        const form = ontologyFormComponent.ontologyForm;
+        expect(form.valid).toBeFalsy();
+
+        const nameInput = form.controls.name;
+
+        nameInput.setValue('ontology');
+        expect(form.valid).toBeFalsy();
+
+        nameInput.setValue('images');
+        expect(form.valid).toBeFalsy();
+
+    });
+
+    it('should test form validity without label', () => {
+        const form = ontologyFormComponent.ontologyForm;
+        expect(form.valid).toBeFalsy();
+
+        const nameInput = form.controls.name;
+        const labelInput = form.controls.label;
+
+        nameInput.setValue('biblio');
+
+        const compiled = ontologyFormFixture.debugElement;
+        expect(ontologyFormComponent.ontologyLabel).toEqual('Biblio');
+        expect(form.valid).toBeTruthy();
+
+
+        labelInput.setValue('Biblio Ontology');
+        expect(ontologyFormComponent.ontologyForm.controls.label.value).toEqual('Biblio Ontology');
+        expect(form.valid).toBeTruthy();
+
+    });
+
 });
