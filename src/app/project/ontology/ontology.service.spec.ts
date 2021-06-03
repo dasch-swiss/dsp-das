94a155465eb69bd0dc3318c4f84ca87002a236f4
André Kilchenmann <github@milchkannen.ch>
2021-06-03 14:58:36 +0200

    fix(ontology): make ts-lint happy

--- src/app/project/ontology/ontology.service.spec.ts
+++ src/app/project/ontology/ontology.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { OntologyService } from './ontology.service';
 
 describe('OntologyService', () => {
-  let service: OntologyService;
+    let service: OntologyService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OntologyService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(OntologyService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
