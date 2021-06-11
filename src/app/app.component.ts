ac99fbc74c0322eaa50a6bb510124714c72aa070
André Kilchenmann <github@milchkannen.ch>
2021-06-11 15:21:50 +0200

    chore(cookie-policy): reactivate the cookie policy banner (DSP-1727) (#461)
    * chore(cookie-policy): reactivate the cookie policy banner (DSP-1727)
    
    * fix(cookie-policy): bug fix in cookie policy link
    
    * chore(cookie-policy): better sentence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -8,10 +9,13 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent implements OnInit {
 
+    showCookieBanner = true;
     session = false;
 
     constructor(
-        private _titleService: Title) {
+        private _router: Router,
+        private _titleService: Title
+    ) {
 
         // set the page title
         this._titleService.setTitle('DaSCH Service Platform');
@@ -19,6 +23,19 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
+        if (sessionStorage.getItem('cookieBanner') === null) {
+            sessionStorage.setItem('cookieBanner', JSON.stringify(this.showCookieBanner));
+        } else {
+            this.showCookieBanner = JSON.parse(sessionStorage.getItem('cookieBanner'));
+        }
+    }
 
+    goToCookiePolicy() {
+        this._router.navigate(['cookie-policy']);
+    }
+
+    closeCookieBanner() {
+        this.showCookieBanner = !this.showCookieBanner;
+        sessionStorage.setItem('cookieBanner', JSON.stringify(this.showCookieBanner));
     }
 }
