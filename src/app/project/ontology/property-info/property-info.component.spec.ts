5db53537709273913fbb1fc2c46b84234b5a46f0
André Kilchenmann <github@milchkannen.ch>
2021-06-19 14:26:28 +0200

    test(ontology): enable all tests

--- src/app/project/ontology/property-info/property-info.component.spec.ts
+++ src/app/project/ontology/property-info/property-info.component.spec.ts
@@ -162,7 +162,7 @@ class ListHostComponent {
 
 }
 
-fdescribe('PropertyInfoComponent', () => {
+describe('PropertyInfoComponent', () => {
     let simpleTextHostComponent: SimpleTextHostComponent;
     let simpleTextHostFixture: ComponentFixture<SimpleTextHostComponent>;
 
