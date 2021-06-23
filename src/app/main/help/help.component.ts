4552c3ceecdece52d6be87c8e45035edf28a9b41
André Kilchenmann <github@milchkannen.ch>
2021-06-23 11:12:05 +0200

    chore(help): fix dead links

--- src/app/main/help/help.component.ts
+++ src/app/main/help/help.component.ts
@@ -27,21 +27,21 @@ export class HelpComponent implements OnInit {
             icon: 'assignment',
             title: 'Project administration',
             text: 'Read more about project administration and how to manage project members.',
-            url: 'https://docs.dasch.swiss/dsp-app/user-guide/project',
+            url: 'https://docs.dasch.swiss/DSP-APP/user-guide/project',
             urlText: 'Open Documentation'
         },
         {
             icon: 'bubble_chart',
             title: 'Data model creation',
             text: 'Find everything about data modelling and how to setup the project database.',
-            url: 'https://docs.dasch.swiss/dsp-app/user-guide/project/#data-model',
+            url: 'https://docs.dasch.swiss/DSP-APP/user-guide/project/#data-model',
             urlText: 'Open Documentation'
         },
         {
             icon: 'image_search',
             title: 'Research tools',
             text: 'Get more information about data handling, search methods and how to use the research tools.',
-            url: 'https://docs.dasch.swiss/dsp-app/user-guide/',
+            url: 'https://docs.dasch.swiss/DSP-APP/user-guide/',
             urlText: 'Open Documentation'
         }
     ];
