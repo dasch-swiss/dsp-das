0ed90280cc2a5fa9aa8bb5c07efbb8bca1c11b77
André Kilchenmann <github@milchkannen.ch>
2021-07-16 15:19:09 +0200

    style(resource): same style in document and still image viewer

--- src/app/workspace/resource/representation/document/document.component.ts
+++ src/app/workspace/resource/representation/document/document.component.ts
@@ -1,6 +1,10 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ViewChild } from '@angular/core';
 import { Component, Input, OnInit } from '@angular/core';
 import { ReadDocumentFileValue } from '@dasch-swiss/dsp-js';
 import { Region } from '@dasch-swiss/dsp-ui';
+import { dir } from 'console';
+import { PdfViewerComponent } from 'ng2-pdf-viewer';
 import { FileRepresentation } from '../file-representation';
 
 /**
@@ -27,13 +31,40 @@ export class DocumentComponent implements OnInit {
 
     @Input() document: FileRepresentation;
 
-    constructor() { }
+    @ViewChild(PdfViewerComponent) private _pdfComponent: PdfViewerComponent;
+
+    zoomFactor = 1.0;
+
+    pdfQuery = '';
+
+    constructor(
+        private readonly _http: HttpClient
+    ) { }
 
     ngOnInit(): void {
     }
 
     download() {
         // console.log(this.document.fileValue.fileUrl);
+        let headers = new HttpHeaders();
+        headers = headers.set('Accept', 'application/pdf');
+        return this._http.get(this.document.fileValue.fileUrl, { headers: headers, responseType: 'blob' });
+    }
+
+
+    searchQueryChanged(newQuery: string) {
+        if (newQuery !== this.pdfQuery) {
+            this.pdfQuery = newQuery;
+            this._pdfComponent.pdfFindController.executeCommand('find', {
+                query: this.pdfQuery,
+                highlightAll: true,
+            });
+        } else {
+            this._pdfComponent.pdfFindController.executeCommand('findagain', {
+                query: this.pdfQuery,
+                highlightAll: true,
+            });
+        }
     }
 
 }
