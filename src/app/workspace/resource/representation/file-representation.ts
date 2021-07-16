64b440d2956867ffb3dfbc946bb2081000065aba
André Kilchenmann <github@milchkannen.ch>
2021-07-16 12:02:09 +0200

    refactor(resource): better file representation handling

---
+++ src/app/workspace/resource/representation/file-representation.ts
@@ -0,0 +1,35 @@
+import {
+    Constants,
+    ReadAudioFileValue,
+    ReadDocumentFileValue,
+    ReadMovingImageFileValue,
+    ReadStillImageFileValue
+} from '@dasch-swiss/dsp-js';
+import { Region } from '@dasch-swiss/dsp-ui';
+
+/**
+ * represents a file value including its annotations.
+ */
+export class FileRepresentation {
+
+    /**
+     *
+     * @param fileValue a [[ReadAudioFileValue | ReadDocumentFileValue | ReadMovingImageFileValue | ReadStillImageFileValue]] representing a file value
+     * @param annotations[] an array of [[Region]] --> TODO: will be expanded with [[Sequence]]
+     */
+    constructor(
+        readonly fileValue: ReadAudioFileValue | ReadDocumentFileValue | ReadMovingImageFileValue | ReadStillImageFileValue,
+        readonly annotations: Region[]
+    ) {
+
+    }
+
+}
+
+export class RepresentationConstants {
+    static audio = Constants.AudioFileValue;
+    static document = Constants.DocumentFileValue;
+    static movingImage = Constants.MovingImageFileValue;
+    static stillImage = Constants.StillImageFileValue;
+    static text = Constants.TextFileValue;
+};
