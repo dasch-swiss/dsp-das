c46b9d640cc18fdd78db99e65f0196a545f94049
André Kilchenmann <github@milchkannen.ch>
2021-08-04 17:39:02 +0200

    refactor(resource): use the project service

--- src/app/workspace/resource/resource-instance-form/resource-instance-form.component.ts
+++ src/app/workspace/resource/resource-instance-form/resource-instance-form.component.ts
@@ -27,6 +27,7 @@ import {
 import { Subscription } from 'rxjs';
 import { CacheService } from 'src/app/main/cache/cache.service';
 import { ErrorHandlerService } from 'src/app/main/error/error-handler.service';
+import { ProjectService } from '../project.service';
 import { SelectOntologyComponent } from './select-ontology/select-ontology.component';
 import { SelectPropertiesComponent } from './select-properties/select-properties.component';
 import { SelectResourceClassComponent } from './select-resource-class/select-resource-class.component';
@@ -90,6 +91,7 @@ export class ResourceInstanceFormComponent implements OnInit, OnDestroy {
         private _errorHandler: ErrorHandlerService,
         private _fb: FormBuilder,
         private _router: Router,
+        private _project: ProjectService,
         private _session: SessionService
     ) {
         this.session = this._session.getSession();
@@ -104,7 +106,16 @@ export class ResourceInstanceFormComponent implements OnInit, OnDestroy {
         this.propertiesParentForm = this._fb.group({});
 
         // initialize projects to be used for the project selection in the creation form
-        this.initializeProjects();
+        this._project.initializeProjects().subscribe(
+            (proj: StoredProject[]) => {
+                this.usersProjects = proj;
+
+                // notifies the user that he/she is not part of any project
+                if (proj.length === 0) {
+                    this.errorMessage = 'You are not a part of any active projects or something went wrong';
+                }
+            }
+        );
 
         // boolean to show only the first step of the form (= selectResourceForm)
         this.showNextStepForm = true;
@@ -197,48 +208,6 @@ export class ResourceInstanceFormComponent implements OnInit, OnDestroy {
         } else {
             this.propertiesParentForm.markAllAsTouched();
         }
-    }
-
-    /**
-     * get the user's project(s)
-     */
-    initializeProjects(): void {
-        this.usersProjects = [];
-
-        if (this.username && this.session.user.sysAdmin === false) {
-            this._cache.get(this.username, this._dspApiConnection.admin.usersEndpoint.getUserByUsername(this.username)).subscribe(
-                (response: ApiResponseData<UserResponse>) => {
-
-                    for (const project of response.body.user.projects) {
-                        if (project.status) {
-                            this.usersProjects.push(project);
-                        }
-                    }
-
-                    // notifies the user that he/she is not part of any project
-                    if (this.usersProjects.length === 0) {
-                        this.errorMessage = 'You are not a part of any active projects.';
-                    }
-                },
-                (error: ApiResponseError) => {
-                    this._errorHandler.showMessage(error);
-                }
-            );
-        } else if (this.session.user.sysAdmin === true) {
-            this._dspApiConnection.admin.projectsEndpoint.getProjects().subscribe(
-                (response: ApiResponseData<ProjectsResponse>) => {
-                    for (const project of response.body.projects) {
-                        if (project.status && project.id !== Constants.SystemProjectIRI && project.id !== Constants.DefaultSharedOntologyIRI) {
-                            this.usersProjects.push(project);
-                        }
-                    }
-                },
-                (error: ApiResponseError) => {
-                    this._errorHandler.showMessage(error);
-                }
-            );
-        }
-
     }
 
     /**
