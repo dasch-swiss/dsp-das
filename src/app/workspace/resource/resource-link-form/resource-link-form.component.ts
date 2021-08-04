ff465d8894145901bdb686f4b2a24d988399781a
André Kilchenmann <github@milchkannen.ch>
2021-08-04 17:39:55 +0200

    feat(resource): form to create link resource

--- src/app/workspace/resource/resource-link-form/resource-link-form.component.ts
+++ src/app/workspace/resource/resource-link-form/resource-link-form.component.ts
@@ -1,7 +1,9 @@
-import { Component, Inject, Input, OnInit } from '@angular/core';
-import { ApiResponseError, Constants, CreateLinkValue, CreateResource, CreateTextValueAsString, KnoraApiConnection, ReadResource } from '@dasch-swiss/dsp-js';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiResponseError, Constants, CreateLinkValue, CreateResource, CreateTextValueAsString, KnoraApiConnection, ReadResource, StoredProject } from '@dasch-swiss/dsp-js';
 import { DspApiConnectionToken, FilteredResouces } from '@dasch-swiss/dsp-ui';
 import { ErrorHandlerService } from 'src/app/main/error/error-handler.service';
+import { ProjectService } from '../project.service';
 
 @Component({
     selector: 'app-resource-link-form',
@@ -12,39 +14,106 @@ export class ResourceLinkFormComponent implements OnInit {
 
     @Input() resources: FilteredResouces;
 
+    @Output() closeDialog: EventEmitter<any> = new EventEmitter<any>();
+
+    /**
+     * form group, errors and validation messages
+     */
+    form: FormGroup;
+
+    formErrors = {
+        'label': ''
+    };
+
+    validationMessages = {
+        'label': {
+            'required': 'A label is required.'
+        }
+    };
+
+    usersProjects: StoredProject[];
+
+    selectedProject: string;
+
+    error = false;
+    loading = false;
+
     constructor(
         @Inject(DspApiConnectionToken) private _dspApiConnection: KnoraApiConnection,
-        private _errorHandler: ErrorHandlerService
+        private _errorHandler: ErrorHandlerService,
+        private _fb: FormBuilder,
+        private _project: ProjectService
     ) { }
 
     ngOnInit(): void {
+
+        // initialize projects to be used for the project selection in the creation form
+        this._project.initializeProjects().subscribe(
+            (proj: StoredProject[]) => {
+                this.usersProjects = proj;
+            }
+        );
+
+        this.form = this._fb.group({
+            'label': new FormControl({
+                value: '', disabled: false
+            }, [
+                Validators.required
+            ]),
+            'comment': new FormControl(),
+            'project': new FormControl()
+        });
+
+        this.form.valueChanges
+            .subscribe(data => this.onValueChanged(data));
     }
 
     /**
-     * create a link resource from selected resources
-     * @param resources
+     * this method is for the form error handling
+     *
+     * @param data Data which changed.
      */
-    linkResources(resources: FilteredResouces) {
+    onValueChanged(data?: any) {
 
-        // step 1: open dialog box with form to define label and comment
+        if (!this.form) {
+            return;
+        }
 
+        const form = this.form;
 
-        // step 2: submit link resource
+        Object.keys(this.formErrors).map(field => {
+            this.formErrors[field] = '';
+            const control = form.get(field);
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                Object.keys(control.errors).map(key => {
+                    this.formErrors[field] += messages[key] + ' ';
+                });
+
+            }
+        });
+    }
+
+    /**
+     * submits the data
+     */
+    submitData() {
+
+        this.loading = true;
+
+        // build link resource as type CreateResource
         const linkObj = new CreateResource();
 
-        // --> TODO: will be replaced by label input from form value
-        linkObj.label = 'Label from link resource form: input';
+        linkObj.label = this.form.controls['label'].value;
 
         linkObj.type = Constants.KnoraApiV2 + Constants.HashDelimiter + 'LinkObj';
 
-        linkObj.attachedToProject = 'http://rdfh.ch/projects/1111';
+        linkObj.attachedToProject = this.selectedProject;
 
-        const propertyValues = [];
-        const hasComment = [];
         // hasComment[Constants.KnoraApiV2 + Constants.HashDelimiter + 'hasComment'] = [];
         const hasLinkToValue = [];
         // hasLinkToValue[Constants.KnoraApiV2 + Constants.HashDelimiter + 'hasLinkToValue'] = [];
-        resources.resIds.forEach(id => {
+        this.resources.resIds.forEach(id => {
             const linkVal = new CreateLinkValue();
             linkVal.type = Constants.LinkValue;
             linkVal.linkedResourceIri = id;
@@ -54,27 +123,31 @@ export class ResourceLinkFormComponent implements OnInit {
             hasLinkToValue.push(linkVal);
         });
 
-        // --> TODO: will be replaced by comment input from form value
-        const comment = 'Comment from link resource form: textarea';
+
+        const comment = this.form.controls['comment'].value;
         if (comment) {
             const commentVal = new CreateTextValueAsString();
             commentVal.type = Constants.TextValue;
             commentVal.text = comment;
-            hasComment.push(commentVal);
+            linkObj.properties = {
+                [Constants.KnoraApiV2 + Constants.HashDelimiter + 'hasLinkToValue']: hasLinkToValue,
+                [Constants.KnoraApiV2 + Constants.HashDelimiter + 'hasComment']: [commentVal],
+            };
+        } else {
+            linkObj.properties = {
+                [Constants.KnoraApiV2 + Constants.HashDelimiter + 'hasLinkToValue']: hasLinkToValue,
+            };
         }
-
-        linkObj.properties = {
-            [Constants.KnoraApiV2 + Constants.HashDelimiter + 'hasLinkToValue']: hasLinkToValue,
-            [Constants.KnoraApiV2 + Constants.HashDelimiter + 'hasComment']: hasComment,
-        };
-
 
         this._dspApiConnection.v2.res.createResource(linkObj).subscribe(
             (response: ReadResource) => {
                 // --> TODO: do something with the successful response
+                this.loading = false;
             },
             (error: ApiResponseError) => {
                 this._errorHandler.showMessage(error);
+                this.error = true;
+                this.loading = false;
             }
         );
 
