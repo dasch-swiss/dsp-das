03f271ce166ada4e99809cb189ce616698a1d7c9
mdelez <60604010+mdelez@users.noreply.github.com>
2021-09-02 16:05:25 +0200

    fix(test): fix resource-class-form comp test

--- src/app/project/ontology/resource-class-form/resource-class-form.component.spec.ts
+++ src/app/project/ontology/resource-class-form/resource-class-form.component.spec.ts
@@ -1,4 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Component } from '@angular/core';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -13,9 +14,10 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterTestingModule } from '@angular/router/testing';
 import { KnoraApiConnection, MockOntology, ReadOntology } from '@dasch-swiss/dsp-js';
-import { AppInitService, DspActionModule } from '@dasch-swiss/dsp-ui';
+import { DspActionModule } from '@dasch-swiss/dsp-ui';
 import { TranslateModule } from '@ngx-translate/core';
 import { of } from 'rxjs';
+import { AppInitService } from 'src/app/app-init.service';
 import { CacheService } from 'src/app/main/cache/cache.service';
 import { DspApiConfigToken, DspApiConnectionToken } from 'src/app/main/declarations/dsp-api-tokens';
 import { DialogComponent } from 'src/app/main/dialog/dialog.component';
@@ -24,7 +26,15 @@ import { TestConfig } from 'test.config';
 import { PropertyFormComponent } from '../property-form/property-form.component';
 import { ResourceClassFormComponent } from './resource-class-form.component';
 
-describe('ResourceClassFormComponent', () => {
+/**
+ * test host component to simulate parent component.
+ */
+@Component({
+    template: '<app-resource-class-form></app-resource-class-form>'
+})
+class TestHostResourceClassFormComponent { }
+
+fdescribe('ResourceClassFormComponent', () => {
     let component: ResourceClassFormComponent;
     let fixture: ComponentFixture<ResourceClassFormComponent>;
 
@@ -33,7 +43,7 @@ describe('ResourceClassFormComponent', () => {
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [
-                ResourceClassFormComponent,
+                TestHostResourceClassFormComponent,
                 PropertyFormComponent,
                 DialogComponent,
                 ErrorComponent
