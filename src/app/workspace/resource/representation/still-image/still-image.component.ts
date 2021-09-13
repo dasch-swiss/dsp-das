47d7d71ba36df40446c37f40ad1bec1299f94c95
mdelez <60604010+mdelez@users.noreply.github.com>
2021-09-13 15:10:00 +0200

    fix(still-image-viewer): fix zoom buttons

--- src/app/workspace/resource/representation/still-image/still-image.component.ts
+++ src/app/workspace/resource/representation/still-image/still-image.component.ts
@@ -254,8 +254,7 @@ export class StillImageComponent implements OnChanges, OnDestroy {
         this._viewer = new OpenSeadragon.Viewer(osdOptions);
 
         // do not zoom in on click
-        // https://github.com/openseadragon/openseadragon/issues/1115#issuecomment-275725918
-        this._viewer.zoomPerClick = 1;
+        this._viewer.gestureSettingsMouse.clickToZoom = false;
 
         this._viewer.addHandler('full-screen', (args) => {
             if (args.fullScreen) {
