414996c128b0b688ff60a2da946cfe85d1b45119
André Kilchenmann <github@milchkannen.ch>
2021-09-27 21:49:37 +0200

    refactor(date): make tslint happy

--- src/app/workspace/resource/values/yet-another-date-value/yet-another-date-value.component.spec.ts
+++ src/app/workspace/resource/values/yet-another-date-value/yet-another-date-value.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { YetAnotherDateValueComponent } from './yet-another-date-value.component';
 
 describe('YetAnotherDateValueComponent', () => {
-  let component: YetAnotherDateValueComponent;
-  let fixture: ComponentFixture<YetAnotherDateValueComponent>;
+    let component: YetAnotherDateValueComponent;
+    let fixture: ComponentFixture<YetAnotherDateValueComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ YetAnotherDateValueComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ YetAnotherDateValueComponent ]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(YetAnotherDateValueComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(YetAnotherDateValueComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
