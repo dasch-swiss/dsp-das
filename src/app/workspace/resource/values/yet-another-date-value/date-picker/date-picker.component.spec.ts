1b2286cee55af822e6e0f3a7336b6383447073e6
André Kilchenmann <github@milchkannen.ch>
2021-10-05 10:59:25 +0200

    test(date): fix tests of new date picker

--- src/app/workspace/resource/values/yet-another-date-value/date-picker/date-picker.component.spec.ts
+++ src/app/workspace/resource/values/yet-another-date-value/date-picker/date-picker.component.spec.ts
@@ -1,5 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DatePickerComponent } from './date-picker.component';
 
 describe('DatePickerComponent', () => {
@@ -8,7 +15,19 @@ describe('DatePickerComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [ DatePickerComponent ]
+            declarations: [
+                DatePickerComponent
+            ],
+            imports: [
+                BrowserAnimationsModule,
+                MatButtonModule,
+                MatButtonToggleModule,
+                MatFormFieldModule,
+                MatInputModule,
+                MatMenuModule,
+                MatSelectModule,
+                ReactiveFormsModule,
+            ],
         })
             .compileComponents();
     });
