dc8cd61949b313d84430c4e6991ccea9c3e70a88
André Kilchenmann <github@milchkannen.ch>
2021-10-08 13:47:08 +0200

    fix(region): display the correct properties

--- src/app/workspace/resource/representation/file-representation.ts
+++ src/app/workspace/resource/representation/file-representation.ts
@@ -33,4 +33,5 @@ export class RepresentationConstants {
     static stillImage = Constants.StillImageFileValue;
     static text = Constants.TextFileValue;
     static region = Constants.Region;
+    static color = Constants.ColorValue;
 };
