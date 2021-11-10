9281eeb8858ce2475d6051799b20bb8b8a2c15cb
Kilchenmann <github@milchkannen.ch>
2021-11-10 11:27:04 +0100

    refactor(test): clean up code

--- test.config.ts
+++ test.config.ts
@@ -1,11 +1,9 @@
 import { KnoraApiConfig } from '@dasch-swiss/dsp-js';
-import { DspAppConfig } from 'src/app/main/declarations/dsp-app-config';
 import { Session } from 'src/app/main/services/session.service';
 
 export class TestConfig {
 
     public static ApiConfig = new KnoraApiConfig('http', '0.0.0.0', 3333);
-    public static AppConfig = new DspAppConfig('knora', 'http://rdfh.ch');
 
     public static ProjectCode = '0001';
 
