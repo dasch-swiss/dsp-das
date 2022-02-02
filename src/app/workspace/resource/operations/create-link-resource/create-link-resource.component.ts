490f5eee01cd423e8949405474db3f04218ae69e
mdelez <60604010+mdelez@users.noreply.github.com>
2022-02-02 14:15:11 +0100

    feat(add-link-resource-button): adds submit button to submit the form and create the resource

--- src/app/workspace/resource/operations/create-link-resource/create-link-resource.component.ts
+++ src/app/workspace/resource/operations/create-link-resource/create-link-resource.component.ts
@@ -1,15 +1,22 @@
 import { Component, EventEmitter, Inject, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import {
+    ApiResponseError,
     Constants,
+    CreateFileValue,
+    CreateResource,
+    CreateTextValueAsString,
+    CreateValue,
     KnoraApiConnection,
     ReadOntology,
     ReadResource,
     ResourceClassAndPropertyDefinitions,
     ResourceClassDefinition,
-    ResourcePropertyDefinition,
-    ResourcePropertyDefinitionWithAllLanguages } from '@dasch-swiss/dsp-js';
+    ResourcePropertyDefinition
+} from '@dasch-swiss/dsp-js';
 import { CacheService } from 'src/app/main/cache/cache.service';
 import { DspApiConnectionToken } from 'src/app/main/declarations/dsp-api-tokens';
+import { ErrorHandlerService } from 'src/app/main/error/error-handler.service';
 import { SelectPropertiesComponent } from '../../resource-instance-form/select-properties/select-properties.component';
 
 @Component({
@@ -22,6 +29,7 @@ export class CreateLinkResourceComponent implements OnInit {
     @Input() parentResource: ReadResource;
     @Input() propIri: string;
     @Input() resourceClassIri: string;
+    @Input() propertiesForm: FormGroup;
 
     @Output() closeDialog: EventEmitter<any> = new EventEmitter<any>();
 
@@ -31,16 +39,24 @@ export class CreateLinkResourceComponent implements OnInit {
     property: ResourcePropertyDefinition;
     resourceClass: ResourceClassDefinition;
     ontologyInfo: ResourceClassAndPropertyDefinitions;
+    fileValue: CreateFileValue;
+
+    hasFileValue: 'stillImage' | 'movingImage' | 'audio' | 'document' | 'text' | 'archive';
+
+    propertiesObj = {};
 
     constructor(
         @Inject(DspApiConnectionToken) private _dspApiConnection: KnoraApiConnection,
-        private _cache: CacheService
+        private _fb: FormBuilder,
+        private _errorHandler: ErrorHandlerService,
     ) { }
 
     ngOnInit(): void {
         console.log('parentResource: ', this.parentResource);
         console.log('propIri: ', this.propIri);
         console.log('resourceClassIri: ', this.resourceClassIri);
+
+        this.propertiesForm = this._fb.group({});
 
         this._dspApiConnection.v2.ontologyCache.getResourceClassDefinition(this.resourceClassIri).subscribe(
             (onto: ResourceClassAndPropertyDefinitions) => {
@@ -58,11 +74,82 @@ export class CreateLinkResourceComponent implements OnInit {
                         prop.id !== Constants.HasArchiveFileValue
                 );
 
+                if (onto.properties[Constants.HasStillImageFileValue]) {
+                    this.hasFileValue = 'stillImage';
+                } else if (onto.properties[Constants.HasDocumentFileValue]) {
+                    this.hasFileValue = 'document';
+                } else if (onto.properties[Constants.HasAudioFileValue]) {
+                    this.hasFileValue = 'audio';
+                } else if (onto.properties[Constants.HasArchiveFileValue]) {
+                    this.hasFileValue = 'archive';
+                } else {
+                    this.hasFileValue = undefined;
+                }
+
                 console.log('properties: ', this.properties);
             }
         );
     }
 
-    // the goal of this is to send a CreateResource request in the end
+    onSubmit() {
+        console.log('onSubmit clicked');
+        if (this.propertiesForm.valid) {
+            const createResource = new CreateResource();
 
+            const resLabelVal = <CreateTextValueAsString>this.selectPropertiesComponent.createValueComponent.getNewValue();
+
+            createResource.label = resLabelVal.text;
+
+            createResource.type = this.resourceClassIri;
+
+            createResource.attachedToProject = 'http://rdfh.ch/projects/0123';
+
+            this.selectPropertiesComponent.switchPropertiesComponent.forEach((child) => {
+                const createVal = child.createValueComponent.getNewValue();
+                const iri = child.property.id;
+                if (createVal instanceof CreateValue) {
+                    if (this.propertiesObj[iri]) {
+                        // if a key already exists, add the createVal to the array
+                        this.propertiesObj[iri].push(createVal);
+                    } else {
+                        // if no key exists, add one and add the createVal as the first value of the array
+                        this.propertiesObj[iri] = [createVal];
+                    }
+                }
+
+            });
+
+            if (this.fileValue) {
+                switch (this.hasFileValue) {
+                    case 'stillImage':
+                        this.propertiesObj[Constants.HasStillImageFileValue] = [this.fileValue];
+                        break;
+                    case 'document':
+                        this.propertiesObj[Constants.HasDocumentFileValue] = [this.fileValue];
+                        break;
+                    case 'audio':
+                        this.propertiesObj[Constants.HasAudioFileValue] = [this.fileValue];
+                        break;
+                    case 'archive':
+                        this.propertiesObj[Constants.HasArchiveFileValue] = [this.fileValue];
+                }
+            }
+
+            createResource.properties = this.propertiesObj;
+
+            this._dspApiConnection.v2.res.createResource(createResource).subscribe(
+                (res: ReadResource) => {
+                    console.log('resource created!: ', res);
+
+                    this.closeDialog.emit();
+                },
+                (error: ApiResponseError) => {
+                    this._errorHandler.showMessage(error);
+                }
+            );
+        // the goal of this is to send a CreateResource request in the end
+        } else {
+            this.propertiesForm.markAllAsTouched();
+        }
+    }
 }
