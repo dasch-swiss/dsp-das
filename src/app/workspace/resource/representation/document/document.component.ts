975f3f03b1f614c36e485b1a9611d7692c609a9e
mdelez <60604010+mdelez@users.noreply.github.com>
2022-08-31 09:23:19 +0200

    feat(document): add default document viewer if document is not a pdf

--- src/app/workspace/resource/representation/document/document.component.ts
+++ src/app/workspace/resource/representation/document/document.component.ts
@@ -47,6 +47,8 @@ export class DocumentComponent implements OnInit, AfterViewInit {
 
     elem: any;
 
+    fileType: string;
+
     constructor(
         @Inject(DOCUMENT) private document: any,
         @Inject(DspApiConnectionToken) private _dspApiConnection: KnoraApiConnection,
@@ -58,7 +60,10 @@ export class DocumentComponent implements OnInit, AfterViewInit {
     ) { }
 
     ngOnInit(): void {
-        this.elem = document.getElementsByClassName('pdf-viewer')[0];
+        this.fileType = this._getFileType(this.src.fileValue.filename);
+        if (this.fileType === 'pdf') {
+            this.elem = document.getElementsByClassName('pdf-viewer')[0];
+        }
         this._getOriginalFilename();
         this.failedToLoad = !this._rs.doesFileExist(this.src.fileValue.fileUrl);
     }
@@ -108,7 +113,7 @@ export class DocumentComponent implements OnInit, AfterViewInit {
         document.body.removeChild(e);
     }
 
-    openReplaceFileDialog(){
+    openReplaceFileDialog() {
         const propId = this.parentResource.properties[Constants.HasDocumentFileValue][0].id;
 
         const dialogConfig: MatDialogConfig = {
@@ -117,7 +122,7 @@ export class DocumentComponent implements OnInit, AfterViewInit {
             position: {
                 top: '112px'
             },
-            data: { mode: 'replaceFile', title: 'Document', subtitle: 'Update the document file of this resource' , representation: 'document', id: propId },
+            data: { mode: 'replaceFile', title: 'Document', subtitle: 'Update the document file of this resource', representation: 'document', id: propId },
             disableClose: true
         };
         const dialogRef = this._dialog.open(
@@ -147,6 +152,10 @@ export class DocumentComponent implements OnInit, AfterViewInit {
         }
     }
 
+    private _getFileType(filename: string): string {
+        return filename.split('.').pop();
+    }
+
     private _getOriginalFilename() {
         const requestOptions = {
             headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -157,6 +166,7 @@ export class DocumentComponent implements OnInit, AfterViewInit {
 
         this._http.get(pathToJson, requestOptions).subscribe(
             res => {
+                console.log('res: ', res);
                 this.originalFilename = res['originalFilename'];
             }
         );
@@ -179,6 +189,10 @@ export class DocumentComponent implements OnInit, AfterViewInit {
                 this.src.fileValue.strval = (res2.properties[Constants.HasDocumentFileValue][0] as ReadDocumentFileValue).strval;
                 this.src.fileValue.valueCreationDate = (res2.properties[Constants.HasDocumentFileValue][0] as ReadDocumentFileValue).valueCreationDate;
 
+                this.fileType = this._getFileType(this.src.fileValue.filename);
+                if (this.fileType === 'pdf') {
+                    this.elem = document.getElementsByClassName('pdf-viewer')[0];
+                }
                 this._getOriginalFilename();
 
                 this.zoomFactor = 1.0;
