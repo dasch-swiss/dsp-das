b81828840428ca21e3cf6819c6beea8aca1540e0
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2022-09-20 11:39:50 +0200

    feat(lists): read mode project member (DEV-1343) (#825)
    * chore: change typo in comment
    
    * chore: change typo in comment
    
    * chore: clean up
    
    * chore: clean up
    
    * refactor(project): enable lists tab
    
    * feat(list): Showing list for project member
    
    * feat(list-item): hide crud operations for project member
    
    * feat(list): display list section for project member
    
    * test(list-item): fix unit tests
    
    * test(list-item-form): fix unit tests
    
    * test(list-item-component): fix lint errors
    
    * fix(list-item): correct gap
    
    * refactor(list-item): delete member

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -26,8 +26,10 @@ import { OntologyService } from './ontology/ontology.service';
     styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
+    readonly TAB_DATA_MODEL = 3;
+    readonly TAB_LISTS = 4;
 
-    // loading for progess indicator
+    // loading for progress indicator
     loading: boolean;
     // error in case of wrong project code
     error: boolean;
