d0a4a8f6e2fdc23e5a7d04ad4d56c87c4e904e05
domsteinbach <dominique.steinbach@unibas.ch>
2022-09-26 11:15:34 +0200

    fest(commentForm): introduce tests (unit and integration tests)

--- src/app/workspace/resource/values/comment-form/comment-form.component.spec.ts
+++ src/app/workspace/resource/values/comment-form/comment-form.component.spec.ts
@@ -1,25 +1,144 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CommentFormComponent } from './comment-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component, DebugElement, OnInit, ViewChild } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+/**
+ * test host component to simulate parent component.
+ */
+@Component({
+    template: `
+        <div [formGroup]="form">
+            <input formControlName="value">
+            <app-comment-form #commentComp [valueFormControl]="valueFormControl" [form]="form" [(commentFormControl)]="commentFormControl" [forceUpdate]="valueFormControl.value"></app-comment-form>
+        </div>
+    `
+})
+class TestHostValueComponent implements OnInit {
+
+    @ViewChild('commentComp') commentFormComponent: CommentFormComponent;
+    form: FormGroup;
+    valueFormControl: FormControl;
+    commentFormControl: FormControl;
+
+    constructor(private _fb: FormBuilder) {
+    }
+
+    ngOnInit() {
+
+        this.valueFormControl = new FormControl(null);
+        this.commentFormControl = new FormControl(null);
+
+        this.form = this._fb.group({
+            value: this.valueFormControl,
+            comment: this.commentFormControl
+        });
+
+    }
+}
 
 describe('CommentFormComponent', () => {
-  let component: CommentFormComponent;
-  let fixture: ComponentFixture<CommentFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CommentFormComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ CommentFormComponent, TestHostValueComponent ],
+            imports: [
+                ReactiveFormsModule,
+                MatInputModule,
+                MatFormFieldModule,
+                BrowserAnimationsModule
+            ]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CommentFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    describe('display and edit a comment', () => {
+        let testHostFixture: ComponentFixture<TestHostValueComponent>;
+        let component: TestHostValueComponent;
+        let fixture: ComponentFixture<CommentFormComponent>;
+        let commentDe: DebugElement;
+        let commentComponent: CommentFormComponent; // what we actually like to test
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        beforeEach(() => {
+
+            testHostFixture = TestBed.createComponent(TestHostValueComponent);
+            component = testHostFixture.componentInstance;
+            // initialize the form control elements and the FormGroup
+            testHostFixture.detectChanges();
+            expect(component.commentFormControl).toBeTruthy();
+            expect(component.valueFormControl).toBeTruthy();
+            const hostCompDe = testHostFixture.debugElement;
+            commentDe = hostCompDe.query(By.directive(CommentFormComponent));
+            commentComponent = commentDe.componentInstance;
+            testHostFixture.detectChanges();
+
+        });
+        it('should be disabled if no value is set in the valueFormControl', () => {
+            // hence there is no value set in valueFormControl, the commentForm should be disabled/readOnly
+            commentComponent.disallowCommentIfEmptyValue(); // run manually here; usually run by ngOnChanges
+            expect(commentComponent.disallowed).toBeTruthy();
+            testHostFixture.detectChanges();
+            // testing if the lock icon is set correctly
+            const lockIcon = testHostFixture.debugElement.nativeElement.querySelector('.comment-lock');
+            expect(lockIcon).toBeTruthy();
+        });
+
+        it('should be enabled if a value is set in the valueFormControl', () => {
+            // hence there is no value for valueFormControl set, the commentForm should be disabled/readOnly
+            component.valueFormControl.setValue('a value');
+            commentComponent.disallowCommentIfEmptyValue(); // run manually here; usually run by ngOnChanges
+            expect(commentComponent.disallowed).toBeFalsy();
+        });
+
+        it('should warn if there is a comment but an invalid value', () => {
+            // setting value and component in the parent component; testing in comment component
+            component.commentFormControl.setValue('this is a comment');
+            component.valueFormControl.setValue('a wrong value');
+            // setting an error, so the formControl is set to Invalid
+            component.valueFormControl.setErrors({ pattern: { value: 'you entered an invalid value' } } );
+            commentComponent.disallowCommentIfEmptyValue();
+            expect(component.commentFormControl.value).toBeTruthy(); // make sure there is a comment value
+            expect(commentComponent.hasError()).toBeTruthy(); // testing hasError()
+            expect(commentComponent.disallowed).toBeTruthy(); // testing disallowed
+            testHostFixture.detectChanges();
+            const warnText = testHostFixture.debugElement.nativeElement.querySelector('.custom-error-message');
+            // test if warn text is displayed
+            expect(warnText.innerText).toEqual('This comment won\'t be saved if there is an invalid or an empty property value above.');
+        });
+
+        it('should warn if there is a comment but no value (null, undefined, empty string)', () => {
+            // there should be already an error in the valueFormControl though, but it should still warn
+            // even if there is no error, but a comment and no value
+            component.commentFormControl.setValue('this is a comment');
+            const emptyValues = [ null, '', undefined ];
+            emptyValues.forEach(val => {
+                component.valueFormControl.setValue(val);
+                commentComponent.disallowCommentIfEmptyValue();
+                expect(component.commentFormControl.value).toBeTruthy(); // make sure there is a comment value
+                expect(commentComponent.hasError()).toBeFalsy(); // testing hasError()
+                expect(commentComponent.disallowed).toBeTruthy(); // testing disallowed
+                testHostFixture.detectChanges();
+                const warnText = testHostFixture.debugElement.nativeElement.querySelector('.custom-error-message');
+                // test if warn text is displayed
+                expect(warnText.innerText).toEqual('This comment won\'t be saved if there is an invalid or an empty property value above.');
+                // testing if the lock icon is unset if there is a comment
+                const lockIcon = testHostFixture.debugElement.nativeElement.querySelector('.comment-lock');
+                expect(lockIcon).toBeFalsy();
+            });
+        });
+
+        it('should lock the comment field if there is no value and no comment yet', () => {
+            commentComponent.disallowCommentIfEmptyValue();
+            expect(component.commentFormControl.value).toBeFalsy(); // make sure there is no comment value
+            expect(commentComponent.hasError()).toBeFalsy(); // make sure there is no error
+            expect(commentComponent.disallowed).toBeTruthy(); // testing if disallowed
+            testHostFixture.detectChanges();
+            // testing if the lock icon is set correctly
+            const lockIcon = testHostFixture.debugElement.nativeElement.querySelector('.comment-lock');
+            expect(lockIcon).toBeTruthy();
+        });
+    });
 });
