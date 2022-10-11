5399ecfc42b1be38e319cf6dc8151c6729d0e511
domsteinbach <dominique.steinbach@unibas.ch>
2022-10-11 10:49:35 +0200

    fix (timeout on github)

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -13,7 +13,8 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      timeoutInterval: 10000 // so larger tests won't time out
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
