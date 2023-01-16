85b3246a7dded806585754b4f917690cc1aa5ce2
Marcin Procyk <marcin.procyk@dasch.swiss>
2023-01-16 15:24:43 +0100

    mock ListViewComponent

--- src/app/workspace/results/results.component.spec.ts
+++ src/app/workspace/results/results.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component, Input } from '@angular/core';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
@@ -6,9 +7,20 @@ import { AppInitService } from 'src/app/app-init.service';
 import { DspApiConfigToken, DspApiConnectionToken } from 'src/app/main/declarations/dsp-api-tokens';
 import { TestConfig } from 'test.config';
 import { ResourceComponent } from '../resource/resource.component';
+import { SearchParams } from './list-view/list-view.component';
 import { ResultsComponent } from './results.component';
 
-describe('ResultsComponent', () => {
+/**
+ * test component that mocks ListViewComponent
+ */
+@Component({ selector: 'app-list-view', template: '' })
+class MockListViewComponent {
+    @Input() search: SearchParams;
+    @Input() withMultipleSelection?: boolean = false;
+    constructor() { }
+}
+
+fdescribe('ResultsComponent', () => {
     let component: ResultsComponent;
     let fixture: ComponentFixture<ResultsComponent>;
 
@@ -16,7 +28,8 @@ describe('ResultsComponent', () => {
         TestBed.configureTestingModule({
             declarations: [
                 ResultsComponent,
-                ResourceComponent
+                ResourceComponent,
+                MockListViewComponent
             ],
             imports: [
                 AngularSplitModule,
