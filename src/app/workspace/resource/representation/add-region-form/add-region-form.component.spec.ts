38d45f6bff29b0b6c90ea7c3bf1095ad279137d8
Marcin Procyk <marcin.procyk@dasch.swiss>
2023-01-23 10:13:51 +0100

    mock AddRegionFormComponent

--- src/app/workspace/resource/representation/add-region-form/add-region-form.component.spec.ts
+++ src/app/workspace/resource/representation/add-region-form/add-region-form.component.spec.ts
@@ -1,18 +1,25 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UntypedFormBuilder } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
-import { AddRegionFormComponent } from './add-region-form.component';
+/**
+ * test component that mocks AddRegionFromComponent
+ */
+@Component({ selector: 'app-add-region-form', template: '' })
+class MockAddRegionFromComponent {
+    @Input() resourceIri: string;
+}
 
 // use test host component approach - create separate simple component in the test file and uses it instead the real one
 describe('AddRegionFormComponent', () => {
-    let component: AddRegionFormComponent;
-    let fixture: ComponentFixture<AddRegionFormComponent>;
+    let component: MockAddRegionFromComponent;
+    let fixture: ComponentFixture<MockAddRegionFromComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [
-                AddRegionFormComponent
+                MockAddRegionFromComponent
             ],
             imports: [
                 TranslateModule.forRoot()
@@ -25,7 +32,7 @@ describe('AddRegionFormComponent', () => {
     });
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(AddRegionFormComponent);
+        fixture = TestBed.createComponent(MockAddRegionFromComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
