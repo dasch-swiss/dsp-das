6b79d91bd45de71b394826e7f066f9906f9f2138
Mike Delez <60604010+mdelez@users.noreply.github.com>
2023-02-03 16:36:24 +0100

    feat(zio): add support for new zio routes (DEV-1636) (#898)
    Co-authored-by: domsteinbach <36757218+domsteinbach@users.noreply.github.com>
    Co-authored-by: Marcin Procyk <marcin.procyk@dasch.swiss>

--- src/app/system/projects/projects.component.ts
+++ src/app/system/projects/projects.component.ts
@@ -102,7 +102,7 @@ export class ProjectsComponent implements OnInit {
         } else {
 
             // logged-in user is system admin (or guest): show all projects
-            this._dspApiConnection.admin.projectsEndpoint.getProjects().subscribe(
+            this._dspApiConnection.admin.projectsEndpoint.getProjects(false).subscribe(
                 (response: ApiResponseData<ProjectsResponse>) => {
 
                     // reset the lists:
