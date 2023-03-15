8538f0c06115de34697316d68f59838e431cbad2
mdelez <60604010+mdelez@users.noreply.github.com>
2023-03-15 11:52:53 +0100

    make test more stable

--- src/app/project/description/description.component.spec.ts
+++ src/app/project/description/description.component.spec.ts
@@ -49,7 +49,7 @@ class MockStringLiteralInputComponent {
     constructor() { }
 }
 
-describe('DescriptionComponent', () => {
+fdescribe('DescriptionComponent', () => {
     let testHostComponent: TestHostDescriptionComponent;
     let testHostFixture: ComponentFixture<TestHostDescriptionComponent>;
     let descriptionComponentDe: DebugElement;
@@ -189,6 +189,11 @@ describe('DescriptionComponent', () => {
     });
 
     it('should not display the edit button as a regular user', async () => {
+        testHostComponent.descriptionComp.projectAdmin = false;
+        testHostComponent.descriptionComp.sysAdmin = false;
+
+        testHostFixture.detectChanges();
+
         const editBtn = await rootLoader.getAllHarnesses(MatButtonHarness.with({ selector: '.app-toolbar-action button' }));
 
         expect(editBtn.length).toEqual(0);
