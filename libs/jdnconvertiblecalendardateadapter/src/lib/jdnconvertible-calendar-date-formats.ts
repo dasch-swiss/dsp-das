274f58aaa505eeadc6a56a47c4bdfd42c36241d8
Ivan Subotic <400790+subotic@users.noreply.github.com>
2023-03-24 23:24:23 +0100

    fix copyright notice

--- libs/jdnconvertiblecalendardateadapter/src/lib/jdnconvertible-calendar-date-formats.ts
+++ libs/jdnconvertiblecalendardateadapter/src/lib/jdnconvertible-calendar-date-formats.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { MatDateFormats } from '@angular/material/core';
 
 export const MAT_JDN_DATE_FORMATS: MatDateFormats = {
--- libs/jdnconvertiblecalendardateadapter/src/public_api.ts
+++ libs/jdnconvertiblecalendardateadapter/src/public_api.ts
@@ -1,4 +1,9 @@
 /*
+ * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
  * Public API Surface of jdnconvertible-calendar-date-adapter
  */
 
