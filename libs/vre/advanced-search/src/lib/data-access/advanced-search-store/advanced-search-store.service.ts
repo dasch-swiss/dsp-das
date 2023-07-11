b34d3e348c0fde95eb56ede643c4c075eabdc976
Mike Delez <60604010+mdelez@users.noreply.github.com>
2023-07-11 11:49:17 +0200

    fix search button disabled logic

--- libs/vre/advanced-search/src/lib/data-access/advanced-search-store/advanced-search-store.service.ts
+++ libs/vre/advanced-search/src/lib/data-access/advanced-search-store/advanced-search-store.service.ts
@@ -28,11 +28,39 @@ export class AdvancedSearchStoreService extends ComponentStore<AdvancedSearchSta
 
     /** combined selectors */
 
-    // search button is disabled if no ontology and no resource class is selected
+    // search button is disabled if:
+    // no ontology and no resource class is selected
+    // OR
+    // no ontology is selected and the list of property forms is empty or at least one selected property is undefined
     searchButtonDisabled$: Observable<boolean> = this.select(
         this.selectedOntology$,
+        this.selectedResourceClass$,
         this.propertyFormList$,
-        (ontology, propertyFormList) => !(ontology && propertyFormList.length && propertyFormList.every(prop => !!prop.selectedProperty))
+        (ontology, resourceClass, propertyFormList) => {
+            const isOntologyUndefined = !ontology;
+            const isResourceClassUndefined = !resourceClass;
+            const isPropertyFormListEmpty = !propertyFormList.length;
+
+            // returns true as soon as it encounters the first element where selectedProperty is undefined
+            const areSomeSelectedPropertiesUndefined =
+            propertyFormList.some((prop) => !prop.selectedProperty);
+
+            if(!isPropertyFormListEmpty) { // list not empty
+                // at least one selected property is undefined or onto is undefined
+                if(areSomeSelectedPropertiesUndefined || isOntologyUndefined)
+                    return true;
+
+                // onto is defined, list not empty and all selected properties are defined
+                return false;
+            } else { // list empty
+                // no onto or resclass selected
+                if(isOntologyUndefined || isResourceClassUndefined)
+                    return true;
+
+                // onto and resclass are selected
+                return false;
+            }
+        }
     );
 
     // add button is disabled if no ontology is selected
@@ -66,7 +94,7 @@ export class AdvancedSearchStoreService extends ComponentStore<AdvancedSearchSta
         } else {
             updatedPropertyFormList = currentPropertyFormList.filter(item => item !== property);
         }
-        console.log('updated list:', updatedPropertyFormList);
+
         this.patchState({ propertyFormList: updatedPropertyFormList });
     }
 
@@ -74,11 +102,11 @@ export class AdvancedSearchStoreService extends ComponentStore<AdvancedSearchSta
         const currentPropertyFormList = this.get((state) => state.propertyFormList);
         const index = currentPropertyFormList.indexOf(property);
 
-        if (index > -1) {  // Only update if the property is found
+        if (index > -1) {  // only update if the property is found
             const updatedPropertyFormList = [
-                ...currentPropertyFormList.slice(0, index),  // Elements before the one to update
-                property,  // The updated property
-                ...currentPropertyFormList.slice(index + 1)  // Elements after the one to update
+                ...currentPropertyFormList.slice(0, index),  // elements before the one to update
+                property,  // the updated property
+                ...currentPropertyFormList.slice(index + 1)  // elements after the one to update
             ];
 
             this.patchState({ propertyFormList: updatedPropertyFormList });
