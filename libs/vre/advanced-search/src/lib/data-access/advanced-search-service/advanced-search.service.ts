88c265bc870f586097c5e6b1483ffd2f72756737
Mike Delez <60604010+mdelez@users.noreply.github.com>
2023-09-01 11:01:42 +0200

    add some comments

--- libs/vre/advanced-search/src/lib/data-access/advanced-search-service/advanced-search.service.ts
+++ libs/vre/advanced-search/src/lib/data-access/advanced-search-service/advanced-search.service.ts
@@ -421,6 +421,9 @@ export class AdvancedSearchService {
             : '';
         console.log(propertyStrings);
         // this was in the CONSTRUCT but idk if it's necessary
+        // if it's needed, we will need a better way to handle the case when searching for a property
+        // of a linked resource where a value does not exist. look at the current implementation of
+        // the advanced search to see the issue
         //${propertyStrings.map((prop) => prop.linkString).join('\n')}
         const gravSearch = `
             PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
@@ -539,6 +542,7 @@ export class AdvancedSearchService {
                 case Operators.Equals:
                     return `?mainRes <${property.selectedProperty?.iri}> <${property.searchValue}> .`;
                 case Operators.NotEquals:
+                    // this looks wrong but it is correct
                     return `FILTER NOT EXISTS { \n
                     ?mainRes <${property.selectedProperty?.iri}> <${property.searchValue}> . }`;
                 case Operators.Matches:
