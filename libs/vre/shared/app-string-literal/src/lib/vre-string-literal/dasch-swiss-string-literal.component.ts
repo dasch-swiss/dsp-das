5d22dd5770224feacedc00e73236c75a44a04ecc
domsteinbach <dominique.steinbach@unibas.ch>
2023-09-27 10:25:58 +0200

    fix(lint): avoid standard dom events as event emitter/output variable names

--- libs/vre/shared/app-string-literal/src/lib/vre-string-literal/dasch-swiss-string-literal.component.ts
+++ libs/vre/shared/app-string-literal/src/lib/vre-string-literal/dasch-swiss-string-literal.component.ts
@@ -113,7 +113,7 @@ export class AppStringLiteralComponent implements OnInit, OnChanges {
      *
      * @emits {boolean} focus
      */
-    @Output() focus: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output() onFocus: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     @ViewChild('textInput', { static: false }) textInput: ElementRef;
 
