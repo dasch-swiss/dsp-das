c370931e73920add55587a72b9d586f1ad5f8677
domsteinbach <dominique.steinbach@unibas.ch>
2023-10-30 16:11:39 +0100

    feat(project description): order project descriptions by language

--- apps/dsp-app/src/app/project/description/description.component.ts
+++ apps/dsp-app/src/app/project/description/description.component.ts
@@ -11,6 +11,7 @@ import {
 } from '@dasch-swiss/vre/shared/app-session';
 import { ApplicationStateService } from '@dasch-swiss/vre/shared/app-state-service';
 import {RouteConstants} from "@dasch-swiss/vre/shared/app-config";
+import { StringLiteral } from '@dasch-swiss/dsp-js/src/models/admin/string-literal';
 
 @Component({
     selector: 'app-description',
@@ -62,11 +63,13 @@ export class DescriptionComponent implements OnInit {
         // get the project data
         this._applicationStateService.get(this.projectUuid).subscribe(
             (response: ReadProject) => {
-                this.project = response;
+                // sort the projectdescriptions by language
+                this.project = this.projectWithSortedDescriptions(response);
+
 
                 // if the user is not sysadmin, check if he is project admin
                 if (!this.userHasPermission) {
-                    this.userHasPermission = this.session.user.projectAdmin.some(
+                    this.userHasPermission = this.session?.user?.projectAdmin?.some(
                         (e) => e === this.project.id
                     );
                 }
@@ -76,6 +79,27 @@ export class DescriptionComponent implements OnInit {
             }
         );
         this.loading = false;
+    }
+
+    // returns the project with the descriptions sorted by language
+    projectWithSortedDescriptions(project: ReadProject): ReadProject  {
+        if (project.description && project.description.length > 1) {
+            // sort the descriptions by language
+            project.description = this.sortDescriptionsByLanguage(project.description);
+        }
+        return project;
+    }
+
+    // returns the descriptions sorted by language
+    sortDescriptionsByLanguage(descriptions: StringLiteral[]): StringLiteral[] {
+        const languageOrder = ['en', 'de', 'fr', 'it', 'rm'];
+
+        return descriptions.sort((a, b) => {
+            const indexA = languageOrder.indexOf(a.language);
+            const indexB = languageOrder.indexOf(b.language);
+
+            return indexA - indexB;
+        });
     }
 
     editProject() {
