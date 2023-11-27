c7529813f6ff0a8be0e99c139d11e70e44037762
Mike Delez <60604010+mdelez@users.noreply.github.com>
2023-11-27 09:33:12 +0100

    delete temporary tests

--- apps/dsp-app-e2e/cypress/e2e/Project_Admin/Browse_Data/Data_Models/test.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('testing project admin', () => {
-    xit('should click on create project button', () => {
-        cy.visit('http://localhost:4200/');
-        cy.get('.create-project-button').should('exist');
-    });
-
-});
--- apps/dsp-app-e2e/cypress/e2e/Project_Member/Browse_Data/Data_Models/test.cy.ts
+++
@@ -1,9 +0,0 @@
-// temporary test to test logging in as a project member
-// you will need a user with the username 'projectMember' and the password 'test1234' in your local db
-describe('testing', () => {
-    xit('should click on create project button', () => {
-        cy.visit('http://localhost:4200/');
-        cy.get('.create-project-button').should('not.exist');
-    });
-
-});
