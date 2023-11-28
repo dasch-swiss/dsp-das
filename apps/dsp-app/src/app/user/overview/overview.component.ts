3103ec9bcdcdcf43966f62c1375020d022ecd982
domsteinbach <dominique.steinbach@unibas.ch>
2023-11-28 15:38:27 +0100

    refactor(OverViewComponent): Remove unnecessary

--- apps/dsp-app/src/app/user/overview/overview.component.ts
+++ apps/dsp-app/src/app/user/overview/overview.component.ts
@@ -1,5 +1,4 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import {
@@ -7,22 +6,11 @@ import {
     ReadUser,
     User,
 } from '@dasch-swiss/dsp-js';
-import {DspApiConnectionToken, RouteConstants} from '@dasch-swiss/vre/shared/app-config';
-import { DialogComponent } from '@dsp-app/src/app/main/dialog/dialog.component';
-import { ProjectService } from '@dsp-app/src/app/workspace/resource/services/project.service';
+import {RouteConstants} from '@dasch-swiss/vre/shared/app-config';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subject, Subscription } from 'rxjs';
 import { AuthService } from '@dasch-swiss/vre/shared/app-session';
 import { LoadProjectsAction, ProjectsSelectors, UserSelectors } from '@dasch-swiss/vre/shared/app-state';
-
-// should only be used by this component and child components
-export type TileLinks = 'workspace' | 'settings';
-
-// should only be used by this component and child components
-export interface routeParams {
-    id: string;
-    path: TileLinks;
-}
 
 @Component({
     selector: 'app-overview',
@@ -31,10 +19,10 @@ export interface routeParams {
 })
 export class OverviewComponent implements OnInit, OnDestroy {
     private destroyed$ = new Subject<void>();
-    
+
     loginSuccessfulSubscription: Subscription;
     isLoggedIn$ = this._authService.isLoggedIn$;
-    
+
     @Select(UserSelectors.user) user$: Observable<ReadUser>;
     // list of projects a user is a member of
     @Select(UserSelectors.userActiveProjects) userActiveProjects$: Observable<StoredProject>;
@@ -45,71 +33,27 @@ export class OverviewComponent implements OnInit, OnDestroy {
     @Select(UserSelectors.isSysAdmin) isSysAdmin$: Observable<boolean>;
 
     constructor(
-        @Inject(DspApiConnectionToken)
-        private _dialog: MatDialog,
         private _titleService: Title,
         private _router: Router,
-        private _projectService: ProjectService,
         private _authService: AuthService,
         private store: Store,
     ) {
         this._titleService.setTitle('Projects Overview');
+
+        // reload projects after successful login
         this.loginSuccessfulSubscription = this._authService.loginSuccessfulEvent.subscribe((user: User) => {
-            if (!user.systemAdmin) {
                 this.store.dispatch(new LoadProjectsAction());
-            }
         });
     }
 
     ngOnInit() {
-        const isSysAdmin = this.store.selectSnapshot(UserSelectors.isSysAdmin);
-        // if user is a system admin or not logged in, get all the projects
-        // system admin can create new projects and edit projects
-        // users not logged in can only view projects
-        if (isSysAdmin || !this._authService.isLoggedIn()) {
-            this.store.dispatch(new LoadProjectsAction());
-        } else {
-            // logged-in user is NOT a system admin: get all projects the user is a member of. Acts on refresh/init.
-            this.store.dispatch(new LoadProjectsAction());
-        }
-    }
-
-    openDialog(mode: string, name?: string, id?: string): void {
-        const dialogConfig: MatDialogConfig = {
-            width: '560px',
-            maxHeight: '80vh',
-            position: {
-                top: '112px',
-            },
-            data: { name: name, mode: mode, project: id },
-        };
-
-        this._dialog.open(DialogComponent, dialogConfig);
-    }
-
-    navigateTo(params: routeParams) {
-        const uuid = this._projectService.iriToUuid(params.id);
-
-        switch (params.path) {
-            case 'workspace':
-                this._router.navigate([RouteConstants.project, uuid]);
-                break;
-
-            case 'settings':
-                this._router.navigate([
-                    RouteConstants.project, uuid, RouteConstants.settings, RouteConstants.collaboration
-                ]);
-                break;
-
-            default:
-                break;
-        }
+        this.store.dispatch(new LoadProjectsAction());
     }
 
     createNewProject() {
         this._router.navigate([RouteConstants.newProjectRelative]);
     }
-    
+
     ngOnDestroy(): void {
         this.loginSuccessfulSubscription.unsubscribe();
         this.destroyed$.next();
