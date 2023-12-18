bfea6cb0b818f4b42897c06b6cd4edd9c4c31609
Julien Schneider <julien.schneider.1991@gmail.com>
2023-12-18 11:04:55 +0100

    wip

--- apps/dsp-app/src/app/project/reusable-project-form/reusable-project-form.component.ts
+++ apps/dsp-app/src/app/project/reusable-project-form/reusable-project-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, UntypedFormControl, Validators } from '@angular/forms';
 import { existingNamesValidator } from '@dsp-app/src/app/main/directive/existing-name/existing-name.directive';
 import PROJECT_FORM_CONSTANTS from '@dsp-app/src/app/project/reusable-project-form/reusable-project-form.constants';
@@ -6,12 +6,13 @@ import { StringLiteral } from '@dasch-swiss/dsp-js';
 import {
     arrayLengthGreaterThanZeroValidator,
 } from '@dsp-app/src/app/project/reusable-project-form/array-length-greater-than-zero-validator';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-reusable-project-form',
     templateUrl: './reusable-project-form.component.html',
 })
-export class ReusableProjectFormComponent implements OnInit {
+export class ReusableProjectFormComponent implements OnInit, OnDestroy {
 
     @Input() formData: { shortname: string, longname: string, shortcode: string, description: StringLiteral[] };
     @Input() editable: boolean;
@@ -20,6 +21,8 @@ export class ReusableProjectFormComponent implements OnInit {
     form: FormGroup;
 
     shortcodePatternError = { errorKey: 'pattern', message: 'This field must contains letters from 0 to 9 and A to F' };
+
+    formSubscription: Subscription;
 
     constructor(
         private _fb: FormBuilder) {
@@ -54,9 +57,22 @@ export class ReusableProjectFormComponent implements OnInit {
             }),
         });
 
-        this.form.valueChanges.subscribe(() => {
+
+        this.formSubscription = this.form.valueChanges.subscribe(() => {
+            console.log(this.form);
             this.formValueChange.emit(this.form);
         });
     }
 
+    onDescriptionChange(value) {
+        this.form.patchValue({ description: value });
+        console.log(value, this.form.controls.description.untouched)
+        if (this.form.controls.description.untouched) {
+            this.form.controls.description.markAsTouched();
+        }
+    }
+
+    ngOnDestroy() {
+        this.formSubscription.unsubscribe();
+    }
 }
