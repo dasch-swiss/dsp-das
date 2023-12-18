86141fad8c709e6f886f4d6b2db77f4edea17510
Julien Schneider <julien.schneider.1991@gmail.com>
2023-12-18 16:24:08 +0100

    chore: add rule import/no-extraneous-dependencies

--- libs/jdnconvertiblecalendardateadapter/src/lib/active_calendar_token.ts
+++ libs/jdnconvertiblecalendardateadapter/src/lib/active_calendar_token.ts
@@ -4,6 +4,7 @@
  */
 
 import { InjectionToken } from '@angular/core';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { BehaviorSubject } from 'rxjs';
 
 export const ACTIVE_CALENDAR = new InjectionToken<
