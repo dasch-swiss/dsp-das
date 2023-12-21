12a4554f0c556d90460ff03b17bfdde73e425d3b
Julien Schneider <julien.schneider.1991@gmail.com>
2023-12-21 18:04:12 +0100

    feat: do not use state management

--- apps/dsp-app/src/app/project/description/description.component.ts
+++ apps/dsp-app/src/app/project/description/description.component.ts
@@ -1,13 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { ReadProject, ReadUser } from '@dasch-swiss/dsp-js';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { StringLiteral } from '@dasch-swiss/dsp-js/src/models/admin/string-literal';
+import { ProjectApiService } from '@dasch-swiss/vre/shared/app-api';
 import { RouteConstants } from '@dasch-swiss/vre/shared/app-config';
 import { ProjectService } from '@dasch-swiss/vre/shared/app-helper-services';
-import { ProjectsSelectors, UserSelectors } from '@dasch-swiss/vre/shared/app-state';
-import { Select, Store } from '@ngxs/store';
-import { Observable, combineLatest, of } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { finalize, map, switchMap } from 'rxjs/operators';
 import { AppGlobal } from '../../app-global';
 
 @Component({
@@ -17,58 +15,27 @@ import { AppGlobal } from '../../app-global';
   styleUrls: ['./description.component.scss'],
 })
 export class DescriptionComponent {
-  // project uuid coming from the route
-  projectUuid: string;
-
-  get readProject$(): Observable<ReadProject> {
-    if (!this.projectUuid) {
-      return of({} as ReadProject);
-    }
-
-    return this.readProjects$.pipe(
-      take(1),
-      map(projects => this.getCurrentProject(projects))
-    );
-  }
-
-  get userHasPermission$(): Observable<boolean> {
-    return combineLatest([this.user$, this.readProject$, this.store.select(UserSelectors.userProjectAdminGroups)]).pipe(
-      map(([user, readProject, userProjectGroups]: [ReadUser, ReadProject, string[]]) => {
-        if (readProject == null) {
-          return false;
-        }
-
-        return ProjectService.IsProjectAdminOrSysAdmin(user, userProjectGroups, readProject.id);
-      })
-    );
-  }
-
-  @Select(UserSelectors.user) user$: Observable<ReadUser>;
-  @Select(ProjectsSelectors.readProjects) readProjects$: Observable<ReadProject[]>;
-  @Select(ProjectsSelectors.isProjectsLoading)
-  isProjectsLoading$: Observable<boolean>;
+  loading = true;
+  readProject$ = this._route.paramMap.pipe(
+    switchMap(params =>
+      this._projectApiService.get(this._projectService.uuidToIri(params.get(RouteConstants.uuidParameter)))
+    ),
+    map(response => response.project),
+    finalize(() => {
+      this.loading = false;
+    })
+  );
+  sortedDescriptions$ = this.readProject$.pipe(map(({ description }) => this._sortDescriptionsByLanguage(description)));
+  userHasPermission$ = of(true);
+  RouteConstants = RouteConstants;
 
   constructor(
     private _route: ActivatedRoute,
-    private _router: Router,
-    private store: Store
-  ) {
-    // get the uuid of the current project
-    this._route.parent.paramMap.subscribe((params: Params) => {
-      this.projectUuid = params.get('uuid');
-    });
-  }
+    private _projectService: ProjectService,
+    private _projectApiService: ProjectApiService
+  ) {}
 
-  editProject() {
-    this._router.navigate([RouteConstants.project, this.projectUuid, RouteConstants.edit]);
-  }
-
-  trackByFn = (index: number, item: string) => `${index}-${item}`;
-
-  trackByStringLiteralFn = (index: number, item: StringLiteral) => `${index}-${item.value}`;
-
-  // returns the descriptions sorted by language
-  private sortDescriptionsByLanguage(descriptions: StringLiteral[]): StringLiteral[] {
+  private _sortDescriptionsByLanguage(descriptions: StringLiteral[]): StringLiteral[] {
     const languageOrder = AppGlobal.languagesList.map(l => l.language);
 
     return descriptions.sort((a, b) => {
@@ -77,23 +44,5 @@ export class DescriptionComponent {
 
       return indexA - indexB;
     });
-  }
-
-  private getCurrentProject(projects: ReadProject[]): ReadProject {
-    if (!projects) {
-      return null;
-    }
-
-    const project = projects.find(x => x.id.split('/').pop() === this.projectUuid);
-    return project ? this.projectWithSortedDescriptions(project) : null;
-  }
-
-  // returns the project with the descriptions sorted by language
-  private projectWithSortedDescriptions(project: ReadProject): ReadProject {
-    if (project.description && project.description.length > 1) {
-      // sort the descriptions by language
-      project.description = this.sortDescriptionsByLanguage(project.description);
-    }
-    return project;
   }
 }
