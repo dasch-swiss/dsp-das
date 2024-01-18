1adcfe6e3e4a170f974651ed5c7060874d16d625
IrKa <irmastnt@gmail.com>
2024-01-18 09:50:34 +0100

    linting fix

--- libs/vre/shared/app-state/src/store.module.ts
+++ libs/vre/shared/app-state/src/store.module.ts
@@ -3,10 +3,10 @@ import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsRouterPluginModule, RouterStateSerializer } from '@ngxs/router-plugin';
 import { NgxsModule } from '@ngxs/store';
-import { ListsState, OntologiesState, OntologyClassState, ProjectsState, UserState } from './index';
 import { CurrentPageState } from './lib/current-page/current-page.state';
 import { CustomRouterStateSerializer } from './lib/router/router-state.serializer';
 import { DEVTOOLS_REDUX_CONFIG, LOGGER_CONFIG, OPTIONS_CONFIG } from './store.config';
+import { ListsState, OntologiesState, OntologyClassState, ProjectsState, UserState } from './index';
 
 const STATE_MODULES = [UserState, ProjectsState, CurrentPageState, OntologiesState, ListsState, OntologyClassState];
 
