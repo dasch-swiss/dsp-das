a13b985a85659954efbc75084ba2c6dbeb3c5084
Julien Schneider <julien.schneider.1991@gmail.com>
2024-01-24 16:57:46 +0100

    feat: AppErrorHandler manages JSLibErrors

--- libs/vre/shared/app-error-handler/src/lib/app-error-handler.service.ts
+++ libs/vre/shared/app-error-handler/src/lib/app-error-handler.service.ts
@@ -1,6 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable, ErrorHandler } from '@angular/core';
+import { ApiResponseError } from '@dasch-swiss/dsp-js';
 import { NotificationService } from '@dasch-swiss/vre/shared/app-notification';
+import { AjaxError } from 'rxjs/ajax';
 
 @Injectable({
   providedIn: 'root',
@@ -8,12 +10,16 @@ import { NotificationService } from '@dasch-swiss/vre/shared/app-notification';
 export class AppErrorHandler implements ErrorHandler {
   constructor(private _notification: NotificationService) {}
   handleError(error: any): void {
-    if (error instanceof HttpErrorResponse) {
-      this.handleHttpError(error);
+    if (error instanceof ApiResponseError && error.error instanceof AjaxError) {
+      // JS-LIB
+      this.handleHttpError(error.error, error.url);
+    } else if (error instanceof HttpErrorResponse) {
+      // ApiServices
+      this.handleHttpError(error, error.url);
     }
   }
 
-  private handleHttpError(error: HttpErrorResponse): void {
+  private handleHttpError(error: HttpErrorResponse | AjaxError, url: string | null): void {
     let message: string;
 
     if (error.message.includes('knora.json: 0 Unknown Error')) {
@@ -23,7 +29,7 @@ export class AppErrorHandler implements ErrorHandler {
     } else if (error.status === 504) {
       message = `There was a timeout issue with one or several requests.
             The resource(s) or a part of it cannot be displayed correctly.
-            Failed on ${error.url}`;
+            Failed on ${url}`;
     } else {
       message = 'There is an error on our side. Our team is notified!';
     }
