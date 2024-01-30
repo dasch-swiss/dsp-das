579c3373a0962bec53624184a2536e58e9c40470
Julien Schneider <julien.schneider.1991@gmail.com>
2024-01-30 15:36:35 +0100

    fix: edit and delete list e2e test

--- apps/dsp-app/cypress/e2e/system-admin/lists.cy.ts
+++ apps/dsp-app/cypress/e2e/system-admin/lists.cy.ts
@@ -1,7 +1,8 @@
 import { faker } from '@faker-js/faker';
 
 describe('Lists', () => {
-  it.skip('user can create/edit/delete a list', () => {
+  it('user can create/edit/delete a list', () => {
+    // create
     cy.intercept('POST', '/admin/lists').as('submitRequest');
 
     cy.visit('/project/0803/add-list');
@@ -12,18 +13,28 @@ describe('Lists', () => {
     cy.wait('@submitRequest');
     cy.url().should('match', /\/project\/0803\/list\/(.+)/);
 
-    cy.intercept('PATCH', '/admin/lists').as('editRequest');
+    // edit
+    cy.intercept('PUT', '/admin/lists/*').as('editRequest');
 
     const data = {
       label: faker.lorem.words(2),
       comment: faker.lorem.sentence(),
     };
     cy.get('[data-cy=edit-button]').click();
-    cy.get('[data-cy=labels-input]').type(data.label);
+
+    cy.get('[data-cy=labels-input] input').type(data.label, { force: true });
     cy.get('[data-cy=comments-input]').type(data.comment);
     cy.get('[data-cy=submit-button]').click();
 
     cy.wait('@editRequest');
     cy.url().should('match', /\/project\/0803\/list\/(.+)/);
+
+    // delete
+    cy.intercept('DELETE', '/admin/lists/*').as('deleteRequest');
+    cy.get('[data-cy=delete-button]').click();
+    cy.get('[data-cy=confirmation-button]').click();
+    cy.wait('@deleteRequest');
+
+    cy.url().should('match', /\/project\/0803\/data-models$/);
   });
 });
