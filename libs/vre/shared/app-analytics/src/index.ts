67534abce0fec994358b1e9de049349e12cc36a6
Julien Schneider <julien.schneider.1991@gmail.com>
2024-01-30 09:49:01 +0100

    feat: add Grafana faro (#1407)

--- libs/vre/shared/app-analytics/src/index.ts
+++ libs/vre/shared/app-analytics/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/datadog-rum/datadog-rum.service';
 export * from './lib/pendo-analytics/pendo-analytics.service';
+export * from './lib/grafana-faro.service';
---
+++ libs/vre/shared/app-analytics/src/lib/grafana-faro.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AppConfigService } from '@dasch-swiss/vre/shared/app-config';
+import { Faro, initializeFaro } from '@grafana/faro-web-sdk';
+
+@Injectable({ providedIn: 'root' })
+export class GrafanaFaroService {
+  faro: Faro;
+
+  constructor(private readonly _appConfig: AppConfigService) {}
+
+  setup() {
+    this.faro = initializeFaro({
+      url: 'https://faro-collector-prod-eu-west-2.grafana.net/collect/66166d1b81448a1cca47cde470d9ec98',
+      app: {
+        name: 'DSP-APP',
+        version: this._appConfig.dspConfig.release,
+        environment: this._appConfig.dspInstrumentationConfig.environment,
+      },
+    });
+  }
+}
