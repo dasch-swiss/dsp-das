90a001343a35a749aabbd7de01eff8713c3a092f
Julien Schneider <julien.schneider.1991@gmail.com>
2024-02-02 13:59:20 +0100

    fix: improve

--- apps/dsp-app/src/app/user/user-menu/user-menu.component.ts
+++ apps/dsp-app/src/app/user/user-menu/user-menu.component.ts
@@ -1,34 +1,28 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
-import { Router } from '@angular/router';
 import { User } from '@dasch-swiss/dsp-js';
 import { RouteConstants } from '@dasch-swiss/vre/shared/app-config';
 import { AuthService } from '@dasch-swiss/vre/shared/app-session';
 import { UserSelectors } from '@dasch-swiss/vre/shared/app-state';
 import { Select, Store } from '@ngxs/store';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { MenuItem } from '../../main/declarations/menu-item';
 
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
   styleUrls: ['./user-menu.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserMenuComponent implements OnInit, OnDestroy {
   @ViewChild(MatMenuTrigger) menuTrigger: MatMenuTrigger;
 
-  routeConstants = RouteConstants;
   navigation: MenuItem[];
-
-  private ngUnsubscribe: Subject<void> = new Subject<void>();
-
+  subscription: Subscription;
+  systemLink = RouteConstants.system;
   isLoggedIn$ = this._store.select(UserSelectors.isLoggedIn);
 
   @Select(UserSelectors.user) user$: Observable<User>;
   @Select(UserSelectors.isSysAdmin) isSysAdmin$: Observable<User>;
-
-  systemLink = RouteConstants.system;
 
   constructor(
     private _authService: AuthService,
@@ -36,41 +30,33 @@ export class UserMenuComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
+    this.subscription = this.isLoggedIn$.subscribe(isLoggedIn => {
+      if (isLoggedIn && this.menuTrigger.menuOpen) {
+        this.menuTrigger.closeMenu();
+      }
+    });
+
     this.navigation = [
       {
         label: 'DSP-App Home Page',
         shortLabel: 'home',
-        route: this.routeConstants.homeRelative,
+        route: RouteConstants.homeRelative,
         icon: '',
       },
       {
         label: 'My Account',
         shortLabel: 'Account',
-        route: this.routeConstants.userAccountRelative,
+        route: RouteConstants.userAccountRelative,
         icon: '',
       },
     ];
   }
 
   ngOnDestroy() {
-    this.ngUnsubscribe.next();
-    this.ngUnsubscribe.complete();
+    this.subscription.unsubscribe();
   }
 
-  /**
-   * logout and destroy session
-   *
-   */
   logout() {
     this._authService.logout();
-  }
-
-  /**
-   * closes menu in case of submitting login form
-   */
-  closeMenu(loginSuccess: boolean) {
-    if (loginSuccess) {
-      this.menuTrigger.closeMenu();
-    }
   }
 }
