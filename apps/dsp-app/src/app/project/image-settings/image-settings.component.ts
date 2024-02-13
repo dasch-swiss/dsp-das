99d0ef0da954083bd98b3c9ac81f2a4b32b085fb
Julien Schneider <julien.schneider.1991@gmail.com>
2024-02-13 13:44:22 +0100

    refactor: first refactor

--- apps/dsp-app/src/app/project/image-settings/image-settings.component.ts
+++ apps/dsp-app/src/app/project/image-settings/image-settings.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectRestrictedViewSettings } from '@dasch-swiss/dsp-js';
@@ -13,14 +13,13 @@ import {
   UpdateProjectRestrictedViewSettingsAction,
 } from '@dasch-swiss/vre/shared/app-state';
 import { TranslateService } from '@ngx-translate/core';
-import { Actions, Select, Store, ofActionSuccessful } from '@ngxs/store';
-import { Observable, combineLatest } from 'rxjs';
-import { map, switchMap, take, takeWhile } from 'rxjs/operators';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { switchMap, takeWhile } from 'rxjs/operators';
 import { ReplaceAnimation } from '../../main/animations/replace-animation';
 import { ProjectImageSettings } from './project-image-settings';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-image-settings',
   styleUrls: ['./image-settings.component.scss'],
   templateUrl: './image-settings.component.html',
@@ -29,15 +28,7 @@ import { ProjectImageSettings } from './project-image-settings';
 export class ImageSettingsComponent implements OnInit {
   projectImageSettings = new ProjectImageSettings();
   form: FormGroup;
-  projectUuid: string;
-  project$ = this.route.parent.parent.paramMap.pipe(
-    map(params => params.get(RouteConstants.uuidParameter)),
-    map(uuid => this._projectService.uuidToIri(uuid)),
-    switchMap(iri => this._projectApiService.get(iri)),
-    map(project => project.project)
-  );
-
-  @Output() formValueChange = new EventEmitter<FormGroup>();
+  projectUuid = this.route.parent.parent.snapshot.paramMap.get(RouteConstants.uuidParameter);
 
   @Select(ProjectsSelectors.isProjectsLoading) isProjectsLoading$: Observable<boolean>;
   @Select(ProjectsSelectors.projectRestrictedViewSettings)
@@ -49,46 +40,32 @@ export class ImageSettingsComponent implements OnInit {
     private route: ActivatedRoute,
     private _store: Store,
     private _notification: NotificationService,
-    private _actions: Actions,
     private _fb: FormBuilder,
     private translateService: TranslateService
   ) {}
 
   ngOnInit() {
-    this._buildForm();
-    this.setFormData();
+    this._store
+      .dispatch(new LoadProjectRestrictedViewSettingsAction(this._projectService.uuidToIri(this.projectUuid)))
+      .pipe(
+        switchMap(() => this.projectRestrictedViewSettings$.pipe(takeWhile(settings => settings?.watermark !== null)))
+      )
+      .subscribe(settings => {
+        this.projectImageSettings = ProjectImageSettings.GetProjectImageSettings(settings.size);
+        this.form = this._fb.group({
+          restrictImageSize: this.projectImageSettings.restrictImageSize,
+          isWatermark: settings.watermark,
+          aspect: this.projectImageSettings.aspect,
+          percentage: this.projectImageSettings.percentage,
+          absoluteWidthIndex: this.absoluteWidthIndex(this.projectImageSettings.absoluteWidth),
+        });
+        console.log('GOT FORM!', this.form);
+      });
   }
 
   formatPercentageLabel = (value: number): string => `${value}%`;
 
   formatAbsoluteLabel = (value: number): string => `${ProjectImageSettings.AbsoluteWidthSteps[value]}px`;
-
-  setFormData() {
-    this.projectUuid = this.route.parent.parent.snapshot.paramMap.get(RouteConstants.uuidParameter);
-    this._store.dispatch(new LoadProjectRestrictedViewSettingsAction(this._projectService.uuidToIri(this.projectUuid)));
-    combineLatest([
-      this._actions.pipe(ofActionSuccessful(LoadProjectRestrictedViewSettingsAction), take(1)),
-      this.projectRestrictedViewSettings$.pipe(takeWhile(settings => settings?.watermark !== null)),
-    ])
-      .pipe(
-        take(1),
-        map(([action, settings]) => {
-          if (!settings?.size) {
-            return;
-          }
-
-          this.projectImageSettings = ProjectImageSettings.GetProjectImageSettings(settings.size);
-          this.form.patchValue({
-            restrictImageSize: this.projectImageSettings.restrictImageSize,
-            isWatermark: settings.watermark,
-            aspect: this.projectImageSettings.aspect,
-            percentage: this.projectImageSettings.percentage,
-            absoluteWidthIndex: this.absoluteWidthIndex(this.projectImageSettings.absoluteWidth),
-          });
-        })
-      )
-      .subscribe();
-  }
 
   absoluteWidthIndex = (value: number): number =>
     ProjectImageSettings.AbsoluteWidthSteps.findIndex(step => step == value);
@@ -108,21 +85,10 @@ export class ImageSettingsComponent implements OnInit {
       watermark: this.form.value.isWatermark,
     };
 
-    this._store.dispatch(new UpdateProjectRestrictedViewSettingsAction(this.projectUuid, request));
-    this._actions.pipe(ofActionSuccessful(UpdateProjectRestrictedViewSettingsAction), take(1)).subscribe(() => {
+    this._store.dispatch(new UpdateProjectRestrictedViewSettingsAction(this.projectUuid, request)).subscribe(() => {
       this._notification.openSnackBar(
         this.translateService.instant('appLabels.form.project.imageSettings.updateConfirmation')
       );
-    });
-  }
-
-  private _buildForm() {
-    this.form = this._fb.group({
-      isWatermark: [false, []],
-      restrictImageSize: [false, []],
-      aspect: [false, []],
-      absoluteWidthIndex: [0, []],
-      percentage: [1, []],
     });
   }
 }
