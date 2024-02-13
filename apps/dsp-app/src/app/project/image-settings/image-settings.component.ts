7ab836ef5c8497d7031a5499e708c584c38305dc
Julien Schneider <julien.schneider.1991@gmail.com>
2024-02-13 15:08:50 +0100

    refactor: fifth refactor

--- apps/dsp-app/src/app/project/image-settings/image-settings.component.ts
+++ apps/dsp-app/src/app/project/image-settings/image-settings.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectRestrictedViewSettings } from '@dasch-swiss/dsp-js';
 import { SetRestrictedViewRequest } from '@dasch-swiss/vre/open-api';
@@ -16,7 +15,6 @@ import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { switchMap, takeWhile } from 'rxjs/operators';
 import { ReplaceAnimation } from '../../main/animations/replace-animation';
-import { ProjectImageSettings } from './project-image-settings';
 
 @Component({
   selector: 'app-image-settings',
@@ -25,72 +23,26 @@ import { ProjectImageSettings } from './project-image-settings';
   animations: [ReplaceAnimation.animation],
 })
 export class ImageSettingsComponent implements OnInit {
-  static AbsoluteWidthSteps: number[] = [64, 128, 256, 512, 1024];
-
-  static GetProjectImageSettings(size: string): ProjectImageSettings {
-    /*
-                                                                                              !d,d The returned image is scaled so that the width and height of the returned image are not greater than d, while maintaining the aspect ratio.
-                                                                                              pct:n The width and height of the returned image is scaled to n percent of the width and height of the extracted region. 1<= n <= 100.
-                                                                                            */
-    const isPercentage = size.startsWith('pct');
-    console.log('size', size);
-    if (isPercentage) {
-      return {
-        restrictImageSize: false,
-        isWatermark: true,
-        aspect: true,
-        absoluteWidth: parseInt(size.substring(1).split(',').pop(), 0),
-        percentage: parseInt(size.split('pct:').pop()),
-      };
-    } else {
-      return {
-        restrictImageSize: size.split('pct:').pop() === '100',
-        isWatermark: true,
-        aspect: false,
-        absoluteWidth: ImageSettingsComponent.AbsoluteWidthSteps[0],
-        percentage: 1,
-      };
-    }
-  }
-
-  static FormatToIiifSize(restrictImageSize: boolean, aspect: boolean, percentage: number, width: number): string {
-    if (!restrictImageSize) {
-      return 'pct:100';
-    }
-
-    return aspect ? `pct:${percentage}` : `!${width},${width}`;
-  }
-
-  projectImageSettings: ProjectImageSettings | undefined;
-  form = this._fb.group({ isWatermark: false, size: 'pct:100' });
+  readonly absoluteWidthSteps: number[] = [64, 128, 256, 512, 1024];
   projectUuid = this.route.parent.parent.snapshot.paramMap.get(RouteConstants.uuidParameter);
 
   @Select(ProjectsSelectors.isProjectsLoading) isProjectsLoading$: Observable<boolean>;
   @Select(ProjectsSelectors.projectRestrictedViewSettings)
   projectRestrictedViewSettings$: Observable<ProjectRestrictedViewSettings>;
 
-  get restricted() {
-    return this.form.value.size !== 'pct:100';
-  }
-
-  get isPercentage() {
-    return true;
-  }
-
-  get percentage() {
-    return 5; // this.form.value.size
-  }
-
-  settingsPercentage = true;
-  restrict = true;
+  percentage = 80;
+  fixedWidth = this.absoluteWidthSteps[3];
+  isWatermark = false;
+  allowRestriction = false;
+  isPercentageSize = true;
 
   constructor(
     private _projectService: ProjectService,
     private route: ActivatedRoute,
     private _store: Store,
     private _notification: NotificationService,
-    private _fb: FormBuilder,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private _cd: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
@@ -100,29 +52,38 @@ export class ImageSettingsComponent implements OnInit {
         switchMap(() => this.projectRestrictedViewSettings$.pipe(takeWhile(settings => settings?.watermark !== null)))
       )
       .subscribe(settings => {
-        this.projectImageSettings = ImageSettingsComponent.GetProjectImageSettings(settings.size);
-        this.form = this._fb.group({
-          isWatermark: settings.watermark as boolean,
-          size: settings.size,
-        });
+        this.isWatermark = settings.watermark as boolean; // TODO
+
+        if (settings.size === 'pct:100') {
+          this.allowRestriction = false;
+          return;
+        }
+
+        this.allowRestriction = true;
+        this.isPercentageSize = settings.size.startsWith('pct');
+        if (this.isPercentageSize) {
+          this.percentage = parseInt(settings.size.split(':')[1], 0);
+        } else {
+          this.fixedWidth = parseInt(settings.size.split(',')[1], 0);
+        }
+        this._cd.detectChanges();
       });
   }
 
   formatPercentageLabel = (value: number): string => `${value}%`;
 
-  formatAbsoluteLabel = (value: number): string => `${ImageSettingsComponent.AbsoluteWidthSteps[value]}px`;
+  formatAbsoluteLabel = (value: number): string => `${this.absoluteWidthSteps[value]}px`;
 
-  absoluteWidthIndex = (value: number): number =>
-    ImageSettingsComponent.AbsoluteWidthSteps.findIndex(step => step == value);
+  absoluteWidthIndex = (value: number): number => this.absoluteWidthSteps.findIndex(step => step == value);
 
   absoluteSliderChange(value: number) {
-    this.projectImageSettings.absoluteWidth = ImageSettingsComponent.AbsoluteWidthSteps[value];
+    this.fixedWidth = this.absoluteWidthSteps[value];
   }
 
   onSubmit() {
     const request: SetRestrictedViewRequest = {
-      size: 'pct:20',
-      watermark: this.form.value.isWatermark,
+      size: this.getSizeForRequest(),
+      watermark: this.isWatermark,
     };
 
     this._store.dispatch(new UpdateProjectRestrictedViewSettingsAction(this.projectUuid, request)).subscribe(() => {
@@ -130,5 +91,13 @@ export class ImageSettingsComponent implements OnInit {
         this.translateService.instant('appLabels.form.project.imageSettings.updateConfirmation')
       );
     });
+  }
+
+  private getSizeForRequest() {
+    if (this.allowRestriction) {
+      return this.isPercentageSize ? `pct:${this.percentage}` : `!${this.fixedWidth},${this.fixedWidth}`;
+    } else {
+      return 'pct:100';
+    }
   }
 }
