3e0bc1b2e11b9a7f2683a03214cb4f6e062c30f5
Julien Schneider <julien.schneider.1991@gmail.com>
2024-03-05 14:09:08 +0100

    fix

--- apps/dsp-app/src/app/workspace/resource/resource-instance-form/select-properties/property-item.component.ts
+++ apps/dsp-app/src/app/workspace/resource/resource-instance-form/select-properties/property-item.component.ts
@@ -4,16 +4,15 @@ import { CardinalityUtil, ResourcePropertyDefinition } from '@dasch-swiss/dsp-js
 
 @Component({
   selector: 'app-property-item',
-  template: `<div class="property">
+  template: `<div>
     <h3 class="mat-subtitle-2" [class.label-info]="prop.comment" [matTooltip]="prop.comment" matTooltipPosition="above">
       {{ prop.label }}
       <span *ngIf="propertyValuesKeyValuePair[prop.id + '-cardinality'][0] === 1">*</span>
     </h3>
-    <div class="property-value large-field">
+    <div>
       <ng-container *ngFor="let val of propertyValuesKeyValuePair[prop.id]; let i = index">
         <ng-container *ngIf="val !== undefined">
           <app-switch-properties-2
-            class="value"
             [property]="prop"
             [control]="myControl(prop.label + '_' + i)"></app-switch-properties-2>
           <!--
@@ -34,7 +33,6 @@ import { CardinalityUtil, ResourcePropertyDefinition } from '@dasch-swiss/dsp-js
             mat-icon-button
             *ngIf="propertyValuesKeyValuePair[prop.id + '-filtered'].length !== 1"
             type="button"
-            class="value-action delete"
             title="Delete this value"
             (click)="deleteValue(prop, i, $event)">
             <mat-icon>delete</mat-icon>
@@ -44,12 +42,7 @@ import { CardinalityUtil, ResourcePropertyDefinition } from '@dasch-swiss/dsp-js
 
       <!-- Add button -->
       <div *ngIf="addValueIsAllowed(prop)">
-        <button
-          mat-icon-button
-          type="button"
-          class="value-action create"
-          title="Add a new value"
-          (click)="addNewValueFormToProperty(prop, $event)">
+        <button mat-icon-button type="button" title="Add a new value" (click)="addNewValueFormToProperty(prop, $event)">
           <mat-icon>add_box</mat-icon>
         </button>
       </div>
