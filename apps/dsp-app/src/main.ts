b5609981ec332d06ddd5a9358bc30e0651664ddd
Julien Schneider <julien.schneider.1991@gmail.com>
2024-03-07 11:29:10 +0100

    feat: add release and version

--- apps/dsp-app/src/main.ts
+++ apps/dsp-app/src/main.ts
@@ -5,25 +5,11 @@ import * as Sentry from '@sentry/angular-ivy';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-function configListener() {
-  try {
-    const configuration = JSON.parse(this.responseText);
-
-    initSentry();
-    // pass config to bootstrap process using an injection token
-    // which will make the encapsulated value available inside
-    // services that inject this token
-    platformBrowserDynamic([{ provide: AppConfigToken, useValue: configuration }])
-      .bootstrapModule(AppModule)
-      .catch(err => console.error(err));
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function initSentry() {
+function initSentry(environmentName: string) {
   Sentry.init({
     dsn: 'https://20bfc69ec9e457239886c1128cc17928@o4506122165747712.ingest.us.sentry.io/4506122171252736',
+    environment: environmentName,
+    release: environment.version,
     integrations: [
       Sentry.browserTracingIntegration(),
       Sentry.replayIntegration({
@@ -39,6 +25,21 @@ function initSentry() {
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
+}
+
+function configListener() {
+  try {
+    const configuration = JSON.parse(this.responseText);
+    initSentry(configuration.instrumentation.environment);
+    // pass config to bootstrap process using an injection token
+    // which will make the encapsulated value available inside
+    // services that inject this token
+    platformBrowserDynamic([{ provide: AppConfigToken, useValue: configuration }])
+      .bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  } catch (error) {
+    console.error(error);
+  }
 }
 function configFailed() {
   console.error('Error: retrieving config.json');
