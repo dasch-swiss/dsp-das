6fa1023646a1083c67902e481e8facd1378ffb8e
Julien Schneider <julien.schneider.1991@gmail.com>
2024-03-11 11:49:13 +0100

    fix

--- apps/dsp-app/src/app/workspace/resource/resource-instance-form/select-properties/switch-properties/switch-properties-3.component.ts
+++ apps/dsp-app/src/app/workspace/resource/resource-instance-form/select-properties/switch-properties/switch-properties-3.component.ts
@@ -8,10 +8,28 @@ import { Constants, ResourcePropertyDefinition } from '@dasch-swiss/dsp-js';
     <app-nu-list [itemTpl]="itemTpl" [newControl]="newControl" [formArray]="formArray"></app-nu-list>
 
     <ng-template let-item #intTpl>
-      <mat-form-field style="width: 100%">
+      <mat-form-field>
         <input matInput [formControl]="item" type="number" />
       </mat-form-field>
     </ng-template>
+
+    <ng-template let-item #decimalTpl>
+      <mat-form-field>
+        <input matInput [formControl]="item" type="number" />
+      </mat-form-field>
+    </ng-template>
+
+    <ng-template let-item #booleanTpl>
+      <mat-form-field>
+        <mat-slide-toggle [formControl]="item"></mat-slide-toggle>
+      </mat-form-field>
+    </ng-template>
+
+    <ng-template let-item #colorTpl>
+      <app-color-value-2 [control]="item"></app-color-value-2>
+    </ng-template>
+
+    <ng-template #defaultTpl>Nothing to show</ng-template>
   `,
 })
 export class SwitchProperties3Component implements AfterViewInit {
@@ -19,6 +37,10 @@ export class SwitchProperties3Component implements AfterViewInit {
   @Input() formArray: FormArray;
 
   @ViewChild('intTpl') intTpl: TemplateRef<any>;
+  @ViewChild('decimalTpl') decimalTpl: TemplateRef<any>;
+  @ViewChild('booleanTpl') booleanTpl: TemplateRef<any>;
+  @ViewChild('colorTpl') colorTpl: TemplateRef<any>;
+  @ViewChild('defaultTpl') defaultTpl: TemplateRef<any>;
 
   itemTpl: TemplateRef<any>;
   newControl: FormControl<any>;
@@ -32,10 +54,18 @@ export class SwitchProperties3Component implements AfterViewInit {
     this._cd.detectChanges();
   }
 
-  private getTemplate() {
+  private getTemplate(): { template: TemplateRef<any>; control: FormControl<any> } {
     switch (this.property.objectType) {
       case Constants.IntValue:
         return { template: this.intTpl, control: new FormControl(0) };
+      case Constants.DecimalValue:
+        return { template: this.decimalTpl, control: new FormControl(0) };
+      case Constants.BooleanValue:
+        return { template: this.booleanTpl, control: new FormControl(false) };
+      case Constants.ColorValue:
+        return { template: this.colorTpl, control: new FormControl('#000000') };
+      default:
+        return { template: this.defaultTpl, control: new FormControl(null) };
     }
   }
 }
