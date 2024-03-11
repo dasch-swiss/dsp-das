a5bfc44a6e0f096cf5fe334f2d6ebc6353eb457a
Julien Schneider <julien.schneider.1991@gmail.com>
2024-03-11 12:37:39 +0100

    fix

--- apps/dsp-app/src/app/workspace/resource/resource-instance-form/select-properties/switch-properties/nu-list.component.ts
+++ apps/dsp-app/src/app/workspace/resource/resource-instance-form/select-properties/switch-properties/nu-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, TemplateRef } from '@angular/core';
 import { FormArray, FormControl } from '@angular/forms';
+import { Cardinality } from '@dasch-swiss/dsp-js';
 
 @Component({
   selector: 'app-nu-list',
   template: ` <div *ngFor="let control of formArray.controls; let index = index" style="display: flex">
       <ng-container *ngTemplateOutlet="itemTpl; context: { $implicit: control }"></ng-container>
-      <button (click)="formArray.removeAt(index)" mat-icon-button>
+      <button (click)="formArray.removeAt(index)" mat-icon-button *ngIf="Cardinality.cardinality">
         <mat-icon>delete</mat-icon>
       </button>
     </div>
@@ -15,8 +16,11 @@ export class NuListComponent {
   @Input() itemTpl: TemplateRef<any>;
   @Input() formArray: FormArray;
   @Input() newValue: any;
+  @Input() cardinality: Cardinality;
 
   add() {
     this.formArray.push(new FormControl(this.newValue));
   }
+
+  protected readonly Cardinality = Cardinality;
 }
