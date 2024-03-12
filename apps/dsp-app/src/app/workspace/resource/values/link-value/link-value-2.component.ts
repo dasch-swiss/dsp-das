d4b21123a2ec262409bf468dde4e8bc9bc766da6
Julien Schneider <julien.schneider.1991@gmail.com>
2024-03-12 17:22:42 +0100

    fix

--- apps/dsp-app/src/app/workspace/resource/values/link-value/link-value-2.component.ts
+++ apps/dsp-app/src/app/workspace/resource/values/link-value/link-value-2.component.ts
@@ -3,14 +3,14 @@ import { FormControl } from '@angular/forms';
 import {
   Constants,
   KnoraApiConnection,
-  ReadOntology,
   ReadResource,
+  ReadResourceSequence,
   ResourceClassAndPropertyDefinitions,
   ResourceClassDefinition,
   ResourcePropertyDefinition,
 } from '@dasch-swiss/dsp-js';
 import { DspApiConnectionToken } from '@dasch-swiss/vre/shared/app-config';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-link-value-2',
@@ -32,8 +32,8 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
       <mat-autocomplete #auto="matAutocomplete" [displayWith]="displayResource">
         <mat-option *ngIf="resourceClasses.length === 0" disabled="true"> No results were found.</mat-option>
         <!--<mat-option *ngFor="let rc of resourceClasses" (click)="openDialog('createLinkResource', $event, propIri, rc)">
-                                  Create New: {{ rc?.label }}
-                                </mat-option>-->
+                                                                                                                                                          Create New: {{ rc?.label }}
+                                                                                                                                                        </mat-option>-->
         <mat-option *ngFor="let res of resources" [value]="res"> {{ res?.label }}</mat-option>
       </mat-autocomplete>
     </mat-form-field>
@@ -41,12 +41,19 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 })
 export class LinkValue2Component implements OnInit {
   @Input() control: FormControl<any>;
+  @Input() propIri: string;
   @Input() currentOntoIri: string;
   @Input() parentResource: ReadResource;
 
   loadingResults = false;
 
   resourceClasses: ResourceClassDefinition[];
+  subClasses: ResourceClassDefinition[] = [];
+  restrictToResourceClass: string;
+  resourceClassLabel: string;
+
+  resources: ReadResource[] = [];
+  properties: ResourcePropertyDefinition[];
 
   constructor(
     @Inject(DspApiConnectionToken)
@@ -56,10 +63,10 @@ export class LinkValue2Component implements OnInit {
   ngOnInit() {
     // in case the resource is referencing itself, assign the parent resource to linkedResource
     /*
-                          if (this.displayValue && this.displayValue.linkedResourceIri === this.parentResource.id) {
-                            this.displayValue.linkedResource = this.parentResource;
-                          }
-                             */
+                                                                                      if (this.displayValue && this.displayValue.linkedResourceIri === this.parentResource.id) {
+                                                                                        this.displayValue.linkedResource = this.parentResource;
+                                                                                      }
+                                                                                         */
 
     const linkType = this.parentResource.getLinkPropertyIriFromLinkValuePropertyIri(this.propIri);
     this.restrictToResourceClass = this.parentResource.entityInfo.properties[linkType].objectType;
@@ -71,11 +78,10 @@ export class LinkValue2Component implements OnInit {
         this.resourceClassLabel = onto.classes[this.restrictToResourceClass].label;
       });
 
-    this._dspApiConnection.v2.ontologyCache.getOntology(this.currentOntoIri).subscribe(
-      (ontoMap: Map<string, ReadOntology>) => {
-        // filter out knorabase ontology
-        const filteredOntoMap = new Map(Array.from(ontoMap).filter(([key]) => key !== Constants.KnoraApiV2));
-
+    this._dspApiConnection.v2.ontologyCache
+      .getOntology(this.currentOntoIri)
+      .pipe(map(ontoMap => new Map(Array.from(ontoMap).filter(([key]) => key !== Constants.KnoraApiV2))))
+      .subscribe(filteredOntoMap => {
         let resClasses = [];
 
         // loop through each ontology in the project and create an array of ResourceClassDefinitions
@@ -86,36 +92,60 @@ export class LinkValue2Component implements OnInit {
         });
 
         // add the superclass to the list of resource classes
-        this.resourceClasses = resClasses.filter(
-          (resClassDef: ResourceClassDefinition) => resClassDef.id === this.restrictToResourceClass
-        );
-
         // recursively loop through all of the superclass's subclasses, including nested subclasses
         // and add them to the list of resource classes
-        this.resourceClasses = this.resourceClasses.concat(
-          this._getSubclasses(resClasses, this.restrictToResourceClass)
-        );
+        this.resourceClasses = resClasses
+          .filter((resClassDef: ResourceClassDefinition) => resClassDef.id === this.restrictToResourceClass)
+          .concat(this._getSubclasses(resClasses, this.restrictToResourceClass));
 
         this.properties = filteredOntoMap
           .get(this.currentOntoIri)
           .getPropertyDefinitionsByType(ResourcePropertyDefinition);
-      },
-      error => {
-        console.error(error);
-      }
-    );
+      });
 
-    this.valueFormControl.valueChanges
+    this._onFormChange();
+  }
+
+  private _onFormChange() {
+    this.control.valueChanges
       .pipe(
-        debounceTime(400), // only proceed if value has not changed for this amount of milliseconds
-        distinctUntilChanged()
-      ) // only proceed if value has changed to a different value
-      .subscribe(data => this.searchByLabel(data));
+        filter(searchTerm => searchTerm.length >= 3),
+        switchMap((searchTerm: string) =>
+          this._dspApiConnection.v2.search.doSearchByLabel(searchTerm, 0, {
+            limitToResourceClass: this.restrictToResourceClass,
+          })
+        )
+      )
+      .subscribe((response: ReadResourceSequence) => {
+        this.resources = response.resources;
+      });
   }
 
   displayResource(resource: ReadResource | null): string {
     if (resource instanceof ReadResource) {
       return resource.label;
     }
+  }
+
+  private _getSubclasses(
+    resClasses: ResourceClassDefinition[],
+    resClassRestrictionIri: string
+  ): ResourceClassDefinition[] {
+    // filter list by the provided IRI to find all subclasses of the provided IRI
+    const subclasses = resClasses.filter(
+      (resClassDef: ResourceClassDefinition) => resClassDef.subClassOf.indexOf(resClassRestrictionIri) > -1
+    );
+
+    // add the filtered list to the list of all subclasses
+    this.subClasses = this.subClasses.concat(subclasses);
+
+    // if the provided IRI has subclasses, recursively call this function to find any subclasses of those subclasses
+    if (subclasses.length) {
+      subclasses.forEach(sub => {
+        this._getSubclasses(resClasses, sub.id);
+      });
+    }
+
+    return this.subClasses;
   }
 }
