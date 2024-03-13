398cb17ac80d729b6e6a04eaac7933a3bb3dbed4
Julien Schneider <julien.schneider.1991@gmail.com>
2024-03-13 13:58:23 +0100

    fix: remove resourceClass

--- apps/dsp-app/src/app/workspace/resource/values/link-value/link-value-2.component.ts
+++ apps/dsp-app/src/app/workspace/resource/values/link-value/link-value-2.component.ts
@@ -1,15 +1,9 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import {
-  Constants,
-  KnoraApiConnection,
-  ReadResource,
-  ReadResourceSequence,
-  ResourceClassDefinition,
-} from '@dasch-swiss/dsp-js';
+import { KnoraApiConnection, ReadResource, ReadResourceSequence, ResourceClassDefinition } from '@dasch-swiss/dsp-js';
 import { DspApiConnectionToken } from '@dasch-swiss/vre/shared/app-config';
 import { TempLinkValueService } from '@dsp-app/src/app/workspace/resource/values/link-value/temp-link-value.service';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-link-value-2',
@@ -24,8 +18,8 @@ import { filter, map, switchMap } from 'rxjs/operators';
       <mat-autocomplete #auto="matAutocomplete" [displayWith]="displayResource">
         <mat-option *ngIf="resources.length === 0" disabled="true"> No results were found.</mat-option>
         <!--<mat-option *ngFor="let rc of resourceClasses" (click)="openDialog('createLinkResource', $event, propIri, rc)">
-                                                                                                                                                                                                                                                                                                                                                          Create New: {{ rc?.label }}
-                                                                                                                                                                                                                                                                                                                                                        </mat-option>-->
+                                                                                                                                                                                                                                                                                                                                                                          Create New: {{ rc?.label }}
+                                                                                                                                                                                                                                                                                                                                                                        </mat-option>-->
         <mat-option *ngFor="let res of resources" [value]="res.id"> {{ res.label }}</mat-option>
       </mat-autocomplete>
     </mat-form-field>
@@ -35,15 +29,10 @@ export class LinkValue2Component implements OnInit {
   @Input() control: FormControl<any>;
   @Input() propIri: string;
 
-  get currentOntoIri() {
-    return this._tempLinkValueService.currentOntoIri;
-  }
-
   get parentResource() {
     return this._tempLinkValueService.parentResource;
   }
 
-  resourceClasses: ResourceClassDefinition[];
   subClasses: ResourceClassDefinition[] = [];
   restrictToResourceClass: string;
 
@@ -58,21 +47,6 @@ export class LinkValue2Component implements OnInit {
   ngOnInit() {
     const linkType = this.parentResource.getLinkPropertyIriFromLinkValuePropertyIri(this.propIri);
     this.restrictToResourceClass = this.parentResource.entityInfo.properties[linkType].objectType;
-
-    this._dspApiConnection.v2.ontologyCache
-      .getOntology(this.currentOntoIri)
-      .pipe(map(ontoMap => new Map(Array.from(ontoMap).filter(([key]) => key !== Constants.KnoraApiV2))))
-      .subscribe(filteredOntoMap => {
-        const resClasses = Array.from(filteredOntoMap.values()).reduce((prev, onto) => {
-          return prev.concat(filteredOntoMap.get(onto.id).getClassDefinitionsByType(ResourceClassDefinition));
-        }, []);
-        // add the superclass to the list of resource classes
-        // recursively loop through all of the superclass's subclasses, including nested subclasses
-        // and add them to the list of resource classes
-        this.resourceClasses = resClasses
-          .filter((resClassDef: ResourceClassDefinition) => resClassDef.id === this.restrictToResourceClass)
-          .concat(this._getSubclasses(resClasses, this.restrictToResourceClass));
-      });
 
     this._onFormChange();
   }
@@ -95,27 +69,5 @@ export class LinkValue2Component implements OnInit {
   displayResource(resId: string | undefined): string {
     if (!resId || !this.resources || this.resources.length === 0) return '';
     return this.resources.find(res => res.id === resId).label;
-  }
-
-  private _getSubclasses(
-    resClasses: ResourceClassDefinition[],
-    resClassRestrictionIri: string
-  ): ResourceClassDefinition[] {
-    // filter list by the provided IRI to find all subclasses of the provided IRI
-    const subclasses = resClasses.filter(
-      (resClassDef: ResourceClassDefinition) => resClassDef.subClassOf.indexOf(resClassRestrictionIri) > -1
-    );
-
-    // add the filtered list to the list of all subclasses
-    this.subClasses = this.subClasses.concat(subclasses);
-
-    // if the provided IRI has subclasses, recursively call this function to find any subclasses of those subclasses
-    if (subclasses.length) {
-      subclasses.forEach(sub => {
-        this._getSubclasses(resClasses, sub.id);
-      });
-    }
-
-    return this.subClasses;
   }
 }
