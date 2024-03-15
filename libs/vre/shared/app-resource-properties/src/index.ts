d4eb718b61c2c3257b4c2da3133a579551dfc13d
Julien Schneider <julien.schneider.1991@gmail.com>
2024-03-15 14:37:06 +0100

    fix

---
+++ libs/vre/shared/app-resource-properties/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/resource-properties.components;
+export * from './lib/switch-properties-3.component';
+export * from './lib/switch-properties-3.component';
---
+++ libs/vre/shared/app-resource-properties/src/lib/resource-properties.components.ts
@@ -0,0 +1,15 @@
+import { BooleanValue2Component } from './boolean-value-2.component';
+import { IntervalValue2Component } from './interval-value-2.component';
+import { NuListComponent } from './nu-list.component';
+import { SwitchProperties3Component } from './switch-properties-3.component';
+import { TimeValue2Component } from './time-value-2.component';
+import { UriValue2Component } from './uri-value-2.component';
+
+export const ResourcePropertiesComponents = [
+  SwitchProperties3Component,
+  BooleanValue2Component,
+  IntervalValue2Component,
+  NuListComponent,
+  TimeValue2Component,
+  UriValue2Component,
+];
