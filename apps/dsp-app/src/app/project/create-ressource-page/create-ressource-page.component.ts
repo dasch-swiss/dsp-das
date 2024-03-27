30c3246e09ca38bd351e8d61162db70d184a2bdc
Julien Schneider <julien.schneider.1991@gmail.com>
2024-03-27 15:32:02 +0100

    fix

--- apps/dsp-app/src/app/project/create-ressource-page/create-ressource-page.component.ts
+++ apps/dsp-app/src/app/project/create-ressource-page/create-ressource-page.component.ts
@@ -4,28 +4,23 @@ import { ResourceClassDefinition } from '@dasch-swiss/dsp-js';
 import { getAllEntityDefinitionsAsArray } from '@dasch-swiss/vre/shared/app-api';
 import { RouteConstants } from '@dasch-swiss/vre/shared/app-config';
 import { OntologyService, ProjectService } from '@dasch-swiss/vre/shared/app-helper-services';
-import {
-  IProjectOntologiesKeyValuePairs,
-  OntologiesSelectors,
-  ProjectsSelectors,
-} from '@dasch-swiss/vre/shared/app-state';
+import { OntologiesSelectors, ProjectsSelectors } from '@dasch-swiss/vre/shared/app-state';
 import { ResourceService } from '@dsp-app/src/app/workspace/resource/services/resource.service';
-import { Select, Store } from '@ngxs/store';
-import { combineLatest, Observable } from 'rxjs';
+import { Store } from '@ngxs/store';
+import { combineLatest } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-ressource-page',
   template: ` <app-create-ressource-form
+    *ngIf="classIri$ | async as classIri"
     [resourceType]="(resClass$ | async)?.label"
-    [resourceClassIri]="classIri$ | async"
+    [resourceClassIri]="classIri"
     [projectIri]="projectIri"
     (createdResourceIri)="afterCreation($event)"></app-create-ressource-form>`,
 })
 export class CreateRessourcePageComponent {
-  @Select(OntologiesSelectors.projectOntologies)
-  projectOntologies$: Observable<IProjectOntologiesKeyValuePairs>;
-
+  projectOntologies$ = this._store.select(OntologiesSelectors.projectOntologies);
   project$ = this._store.select(ProjectsSelectors.currentProject);
   projectUuid = this._route.snapshot.params.uuid ?? this._route.parent.snapshot.params.uuid;
 
@@ -58,18 +53,20 @@ export class CreateRessourcePageComponent {
     return this._projectService.uuidToIri(this.projectUuid);
   }
 
-  get resClass$(): Observable<ResourceClassDefinition> {
-    return combineLatest([this.projectOntologies$, this.classIri$, this.ontoId$]).pipe(
-      map(([projectOntologies, classId, ontoId]) => {
-        const ontology = projectOntologies[this.projectIri].readOntologies.find(onto => onto.id === ontoId);
-        if (ontology) {
-          // find ontology of current resource class to get the class label
-          const classes = getAllEntityDefinitionsAsArray(ontology.classes);
-          return <ResourceClassDefinition>classes[classes.findIndex(res => res.id === classId)];
-        }
-      })
-    );
-  }
+  resClass$ = combineLatest([
+    this.projectOntologies$.pipe(filter(v => Object.keys(v).length !== 0)),
+    this.classIri$,
+    this.ontoId$,
+  ]).pipe(
+    map(([projectOntologies, classId, ontoId]) => {
+      const ontology = projectOntologies[this.projectIri].readOntologies.find(onto => onto.id === ontoId);
+      if (ontology) {
+        // find ontology of current resource class to get the class label
+        const classes = getAllEntityDefinitionsAsArray(ontology.classes);
+        return <ResourceClassDefinition>classes[classes.findIndex(res => res.id === classId)];
+      }
+    })
+  );
 
   afterCreation(resourceIri: string) {
     const uuid = this._resourceService.getResourceUuid(resourceIri);
