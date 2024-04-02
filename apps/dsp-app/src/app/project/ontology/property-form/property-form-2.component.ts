1cbc26965f4f42f236205e4268e082b5210c0a2f
Julien Schneider <julien.schneider.1991@gmail.com>
2024-04-02 08:44:06 +0200

    fix

--- apps/dsp-app/src/app/project/ontology/property-form/property-form-2.component.ts
+++ apps/dsp-app/src/app/project/ontology/property-form/property-form-2.component.ts
@@ -8,59 +8,58 @@ import { PropertyForm } from '@dsp-app/src/app/project/ontology/property-form/pr
 
 @Component({
   selector: 'app-property-form-2',
-  template: ` <form [formGroup]="form">
-    <mat-form-field style="width: 100%">
+  template: `
+        <form [formGroup]="form">
+            <mat-form-field style="width: 100%">
       <span matPrefix>
         <mat-icon>{{ selectedProperty.icon }}</mat-icon>
       </span>
-      <mat-label>Property type</mat-label>
-      <!-- in case of edit property, the user can change the property type within the same group -->
-      <mat-select [formControl]="form.controls.propType">
-        <mat-select-trigger>
-          {{ selectedProperty.group }}
-          :&nbsp; {{ selectedProperty.label }}
-        </mat-select-trigger>
+                <mat-label>Property type</mat-label>
+                <mat-select [formControl]="form.controls.propType">
+                    <mat-select-trigger>
+                        {{ selectedProperty.group }}
+                        :&nbsp; {{ selectedProperty.label }}
+                    </mat-select-trigger>
 
-        <mat-optgroup *ngFor="let type of filteredProperties" [label]="type.group">
-          <mat-option *ngFor="let ele of type.elements" [value]="ele.guiEle">
-            <mat-icon>{{ ele.icon }}</mat-icon>
-            {{ ele.label }}
-          </mat-option>
-        </mat-optgroup>
-      </mat-select>
-      <!--TODO <mat-hint *ngIf="unsupportedPropertyType" class="ontology-warning-with-prefix">
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      {{ propertyForm.controls['propType'].value.description }}
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    </mat-hint>-->
-    </mat-form-field>
-    <app-common-input
-      placeholder="Property name *"
-      prefixIcon="fingerprint"
-      [control]="form.controls.name"></app-common-input>
-    <dasch-swiss-multi-language-input
-      [formArray]="form.controls.labels"
-      placeholder="Property label*"></dasch-swiss-multi-language-input>
-    <dasch-swiss-multi-language-textarea
-      [formArray]="form.controls.comments"
-      placeholder="Comment"></dasch-swiss-multi-language-textarea>
+                    <mat-optgroup *ngFor="let type of filteredProperties" [label]="type.group">
+                        <mat-option *ngFor="let ele of type.elements" [value]="ele.guiEle">
+                            <mat-icon>{{ ele.icon }}</mat-icon>
+                            {{ ele.label }}
+                        </mat-option>
+                    </mat-optgroup>
+                </mat-select>
+                {{ propertyForm.controls['propType'].value.description }}
+                </mat-hint>-->
+            </mat-form-field>
+            <app-common-input
+                placeholder="Property name *"
+                prefixIcon="fingerprint"
+                [control]="form.controls.name"></app-common-input>
+            <dasch-swiss-multi-language-input
+                [formArray]="form.controls.labels"
+                placeholder="Property label*"></dasch-swiss-multi-language-input>
+            <dasch-swiss-multi-language-textarea
+                [formArray]="form.controls.comments"
+                placeholder="Comment"></dasch-swiss-multi-language-textarea>
 
-    <app-gui-attr-list
-      *ngIf="formData.property.propType.objectType === Constants.ListValue"
-      [control]="form.controls.guiAttr"></app-gui-attr-list>
+            <app-gui-attr-list
+                *ngIf="formData.property.propType.objectType === Constants.ListValue"
+                [control]="form.controls.guiAttr"></app-gui-attr-list>
 
-    <app-gui-attr-link
-      *ngIf="formData.property.propType.objectType === Constants.LinkValue"
-      [control]="form.controls.guiAttr"></app-gui-attr-link>
+            <app-gui-attr-link
+                *ngIf="formData.property.propType.objectType === Constants.LinkValue"
+                [control]="form.controls.guiAttr"></app-gui-attr-link>
 
-    <div style="display: flex; justify-content: center; gap: 16px">
-      <app-multiple-slide-toggle
-        [control]="form.controls.cardinality"
-        [label]="'Multiple values ?'"></app-multiple-slide-toggle>
+            <div style="display: flex; justify-content: center; gap: 16px">
+                <app-multiple-slide-toggle
+                    [control]="form.controls.cardinality"
+                    [label]="'Multiple values ?'"></app-multiple-slide-toggle>
 
-      <app-required-slide-toggle
-        [control]="form.controls.cardinality"
-        [label]="'Required value ?'"></app-required-slide-toggle>
-    </div>
-  </form>`,
+                <app-required-slide-toggle
+                    [control]="form.controls.cardinality"
+                    [label]="'Required value ?'"></app-required-slide-toggle>
+            </div>
+        </form>`,
 })
 export class PropertyForm2Component implements OnInit {
   @Input() formData: {
@@ -116,7 +115,13 @@ export class PropertyForm2Component implements OnInit {
         ],
         [Validators.required]
       ),
-      guiAttr: this._fb.control<string>(this.formData.guiAttribute, [Validators.required]),
+      guiAttr: this._fb.control<string>(
+        {
+          value: this.formData.guiAttribute,
+          disabled: true,
+        },
+        [Validators.required]
+      ),
       cardinality: this._fb.control(this.formData.cardinality ?? Cardinality._0_1),
     });
 
