01a09d1438c8c61d0f976c49d8bec74edf05f08f
Julien Schneider <julien.schneider.1991@gmail.com>
2024-04-03 13:57:09 +0200

    fix

--- libs/vre/shared/app-property-form/src/lib/components/gui-attr-list.component.ts
+++ libs/vre/shared/app-property-form/src/lib/components/gui-attr-list.component.ts
@@ -10,11 +10,13 @@ import { PropertyForm } from '../property-form.type';
       <span matPrefix> <mat-icon>tune</mat-icon>&nbsp; </span>
       <mat-label>Select a list</mat-label>
       <mat-select [formControl]="control">
-        <mat-option *ngFor="let item of lists$ | async" [value]="'hlist=<' + item.id + '>'">
+        <mat-option *ngFor="let item of lists$ | async" [value]="item.id">
           {{ item.labels[0].value }}
         </mat-option>
       </mat-select>
-      <mat-error *ngIf="control.invalid && control.touched"> {{ control.errors[0] | humanReadableError }}</mat-error>
+      <mat-error *ngIf="control.invalid && control.touched && control.errors as errors">
+        {{ errors[0] | humanReadableError }}
+      </mat-error>
     </mat-form-field>
   `,
   styles: ['.large-field {width: 100%}'],
@@ -22,6 +24,7 @@ import { PropertyForm } from '../property-form.type';
 export class GuiAttrListComponent {
   @Input() control: PropertyForm['controls']['guiAttr'];
   lists$ = this._store.select(ListsSelectors.listsInProject);
+
   // gui attribute value for lists looks as follows: hlist=<http://rdfh.ch/lists/00FF/73d0ec0302>
 
   constructor(private _store: Store) {}
