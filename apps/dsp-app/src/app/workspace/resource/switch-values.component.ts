6f2272c559de7964dc5f630669db7c7e3886be9f
Julien Schneider <julien.schneider.1991@gmail.com>
2024-04-03 12:15:09 +0200

    fix

--- apps/dsp-app/src/app/workspace/resource/switch-values.component.ts
+++ apps/dsp-app/src/app/workspace/resource/switch-values.component.ts
@@ -1,14 +1,12 @@
-import { Component, Input } from '@angular/core';
-import { Constants, ReadValue } from '@dasch-swiss/dsp-js';
+import { Component, Input, OnInit } from '@angular/core';
+import { Constants, ReadBooleanValue, ReadValue } from '@dasch-swiss/dsp-js';
 
 @Component({
   selector: 'app-switch-values',
   template: ` <ng-container [ngSwitch]="value.type">
     <ng-container *ngSwitchCase="constants.IntValue">{{ value.strval }}</ng-container>
     <ng-container *ngSwitchCase="constants.DecimalValue">{{ value.strval }}</ng-container>
-    <ng-container *ngSwitchCase="constants.BooleanValue">
-      <mat-slide-toggle [checked]="true"></mat-slide-toggle>
-    </ng-container>
+    <mat-icon *ngSwitchCase="constants.BooleanValue">{{ booleanValue.bool ? 'toggle_on' : 'toggle_off' }} </mat-icon>
     <ng-container *ngSwitchCase="constants.ColorValue">{{ value.strval }}</ng-container>
     <ng-container *ngSwitchCase="constants.TextValue">{{ value.strval }}</ng-container>
     <ng-container *ngSwitchCase="constants.DateValue">{{ value.strval }}</ng-container>
@@ -20,8 +18,16 @@ import { Constants, ReadValue } from '@dasch-swiss/dsp-js';
     <ng-container *ngSwitchCase="constants.UriValue">{{ value.strval }}</ng-container>
   </ng-container>`,
 })
-export class SwitchValuesComponent {
+export class SwitchValuesComponent implements OnInit {
   @Input() value: ReadValue;
 
   readonly constants = Constants;
+
+  get booleanValue() {
+    return this.value as ReadBooleanValue;
+  }
+
+  ngOnInit() {
+    console.log(this.value);
+  }
 }
