8ad57cbc2a6e511106fc1efa2e80773613f0df40
domsteinbach <dominique.steinbach@unibas.ch>
2024-04-10 10:21:34 +0200

    refactor: implement dialog config in app config

---
+++ libs/vre/shared/app-config/src/lib/app-config/dsp-dialog-config.ts
@@ -0,0 +1,17 @@
+import { MatDialogConfig } from '@angular/material/dialog';
+
+export class DspDialogConfig {
+  static dialogDrawerConfig<T = any>(dialogData?: T): MatDialogConfig<T> {
+    return {
+      height: 'calc(100vh - 72px)', // 72px == height of the header
+      position: {
+        top: '72px', // same as the header
+        right: '0px',
+      },
+      autoFocus: true,
+      enterAnimationDuration: 0,
+      exitAnimationDuration: 0,
+      data: dialogData,
+    };
+  }
+}
