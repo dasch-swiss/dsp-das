3fa1d81b4f512465b6446e77fabfbffe0b243adb
Julien Schneider <julien.schneider.1991@gmail.com>
2024-04-23 12:12:28 +0200

    fix

--- apps/dsp-app/src/app/workspace/resource/resource.component.ts
+++ apps/dsp-app/src/app/workspace/resource/resource.component.ts
@@ -315,7 +315,6 @@ export class ResourceComponent implements OnChanges, OnDestroy {
   private _initResource(iri) {
     this.oldResourceIri = this.resourceIri;
     this._getResource(iri).subscribe(dspResource => {
-      return;
       this._renderResource(dspResource);
       this._getResourceAttachedData(dspResource);
     });
@@ -462,17 +461,17 @@ export class ResourceComponent implements OnChanges, OnDestroy {
             };
 
             /*
-                                                            TODO Julien: I removed this part
-                                                            const stillImageRepresentations = [
-                                                              new FileRepresentation(
-                                                                resource.getValuesAs(Constants.HasStillImageFileValue, ReadStillImageFileValue)[0],
-                                                                []
-                                                              ),
-                                                            ];
+                                                                        TODO Julien: I removed this part
+                                                                        const stillImageRepresentations = [
+                                                                          new FileRepresentation(
+                                                                            resource.getValuesAs(Constants.HasStillImageFileValue, ReadStillImageFileValue)[0],
+                                                                            []
+                                                                          ),
+                                                                        ];
 
-                                                            this.representationsToDisplay = stillImageRepresentations;
+                                                                        this.representationsToDisplay = stillImageRepresentations;
 
-                                                            */
+                                                                        */
             // --> TODO: get regions here
 
             break;
