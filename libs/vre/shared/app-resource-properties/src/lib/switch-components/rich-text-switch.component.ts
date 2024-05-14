2494a604c2144193f84689019cf0556a51f7b9ea
Julien Schneider <julien.schneider.1991@gmail.com>
2024-05-14 18:15:39 +0200

    fix: standoff links are clickable (#1585)

--- libs/vre/shared/app-resource-properties/src/lib/switch-components/rich-text-switch.component.ts
+++ libs/vre/shared/app-resource-properties/src/lib/switch-components/rich-text-switch.component.ts
@@ -1,10 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ReadLinkValue } from '@dasch-swiss/dsp-js';
+import { ResourceService } from '@dasch-swiss/vre/shared/app-common';
 import { IsSwitchComponent } from './is-switch-component.interface';
 
 @Component({
   selector: 'app-rich-text-switch',
-  template: ` <div *ngIf="displayMode; else editMode" [innerHTML]="control.value" appHtmlLink></div>
+  template: ` <div
+      *ngIf="displayMode; else editMode"
+      [innerHTML]="control.value"
+      appHtmlLink
+      (internalLinkClicked)="_openResource($event)"></div>
     <ng-template #editMode>
       <app-rich-text-value [control]="myControl"></app-rich-text-value>
     </ng-template>`,
@@ -15,5 +21,13 @@ export class RichTextSwitchComponent implements IsSwitchComponent {
 
   get myControl() {
     return this.control as FormControl<string>;
+  }
+
+  constructor(private _resourceService: ResourceService) {}
+
+  _openResource(linkValue: ReadLinkValue | string) {
+    const iri = typeof linkValue == 'string' ? linkValue : linkValue.linkedResourceIri;
+    const path = this._resourceService.getResourcePath(iri);
+    window.open(`/resource${path}`, '_blank');
   }
 }
