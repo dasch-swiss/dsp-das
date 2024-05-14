a55ae010b85fc291119962fe76226ada623ba355
Julien Schneider <julien.schneider.1991@gmail.com>
2024-05-14 18:05:57 +0200

    fix

--- libs/vre/shared/app-resource-properties/src/lib/switch-components/rich-text-switch.component.ts
+++ libs/vre/shared/app-resource-properties/src/lib/switch-components/rich-text-switch.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Constants, ReadLinkValue, ReadValue } from '@dasch-swiss/dsp-js';
-import { PropertyInfoValues, ResourceService } from '@dasch-swiss/vre/shared/app-common';
+import { ReadLinkValue } from '@dasch-swiss/dsp-js';
+import { ResourceService } from '@dasch-swiss/vre/shared/app-common';
 import { IsSwitchComponent } from './is-switch-component.interface';
 
 @Component({
@@ -19,50 +19,15 @@ export class RichTextSwitchComponent implements IsSwitchComponent {
   @Input() control!: FormControl<string | null>;
   @Input() displayMode = true;
 
-  propArray: PropertyInfoValues[] = [];
-
   get myControl() {
     return this.control as FormControl<string>;
   }
 
   constructor(private _resourceService: ResourceService) {}
 
-  standoffLinkClicked(resIri: string): void {
-    // find the corresponding standoff link value
-    const referredResStandoffLinkVal: ReadLinkValue[] = this._getStandoffLinkValueForResource(resIri);
-
-    // only emit an event if the corresponding standoff link value could be found
-    if (referredResStandoffLinkVal.length === 1) {
-      this._openResource(referredResStandoffLinkVal[0]);
-    }
-  }
-
   _openResource(linkValue: ReadLinkValue | string) {
     const iri = typeof linkValue == 'string' ? linkValue : linkValue.linkedResourceIri;
     const path = this._resourceService.getResourcePath(iri);
     window.open(`/resource${path}`, '_blank');
-  }
-
-  private _getStandoffLinkValueForResource(resIri: string): ReadLinkValue[] {
-    // find the PropertyInfoValues for the standoff link value
-    const standoffLinkPropInfoVals: PropertyInfoValues[] = this.propArray.filter(
-      resPropInfoVal => resPropInfoVal.propDef.id === Constants.HasStandoffLinkToValue
-    );
-
-    if (standoffLinkPropInfoVals.length === 1) {
-      // find the corresponding standoff link value
-      const referredResStandoffLinkVal: ReadValue[] = standoffLinkPropInfoVals[0].values.filter(
-        (standoffLinkVal: ReadValue) =>
-          standoffLinkVal instanceof ReadLinkValue && (standoffLinkVal as ReadLinkValue).linkedResourceIri === resIri
-      );
-
-      // if no corresponding standoff link value was found,
-      // this array is empty
-      return referredResStandoffLinkVal as ReadLinkValue[];
-    } else {
-      // this should actually never happen
-      // because all resource types have a cardinality for a standoff link value
-      return [];
-    }
   }
 }
