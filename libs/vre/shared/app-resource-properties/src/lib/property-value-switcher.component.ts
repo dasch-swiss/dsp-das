d806de67d1cfcb4d5e76091ada3a939043781430
Julien Schneider <julien.schneider.1991@gmail.com>
2024-05-14 14:20:06 +0200

    fix: change how text values are handled (html) (#1579)
    Co-authored-by: domsteinbach <36757218+domsteinbach@users.noreply.github.com>

--- libs/vre/shared/app-resource-properties/src/lib/property-value-switcher.component.ts
+++ libs/vre/shared/app-resource-properties/src/lib/property-value-switcher.component.ts
@@ -10,7 +10,14 @@ import {
   ViewChild,
 } from '@angular/core';
 import { ValidatorFn } from '@angular/forms';
-import { Constants, ReadResource, ReadValue } from '@dasch-swiss/dsp-js';
+import {
+  Constants,
+  ReadResource,
+  ReadTextValueAsHtml,
+  ReadTextValueAsString,
+  ReadTextValueAsXml,
+  ReadValue,
+} from '@dasch-swiss/dsp-js';
 import { PropertyInfoValues } from '@dasch-swiss/vre/shared/app-common';
 import { JsLibPotentialError } from './JsLibPotentialError';
 import { FormValueArray } from './form-value-array.type';
@@ -68,6 +75,10 @@ import { PropertyValueService } from './property-value.service';
       <app-text-switch [control]="item" [displayMode]="displayMode"></app-text-switch>
     </ng-template>
 
+    <ng-template #textHtmlTpl let-item="item" let-displayMode="displayMode">
+      <app-text-html-switch [control]="item" [displayMode]="displayMode"></app-text-html-switch>
+    </ng-template>
+
     <ng-template #paragraphTpl let-item="item" let-displayMode="displayMode">
       <app-base-switch [control]="item" [displayMode]="displayMode" style="white-space: pre-line">
         <mat-form-field style="width: 100%">
@@ -122,10 +133,6 @@ export class PropertyValueSwitcherComponent implements OnInit, OnChanges, AfterV
 
   @Input({ required: true }) myProperty!: PropertyInfoValues;
 
-  get property() {
-    return this.myProperty.guiDef;
-  }
-
   get cardinality() {
     return this.myProperty.guiDef.cardinality;
   }
@@ -139,6 +146,7 @@ export class PropertyValueSwitcherComponent implements OnInit, OnChanges, AfterV
   @ViewChild('booleanTpl') booleanTpl!: TemplateRef<any>;
   @ViewChild('colorTpl') colorTpl!: TemplateRef<any>;
   @ViewChild('textTpl') textTpl!: TemplateRef<any>;
+  @ViewChild('textHtmlTpl') textHtmlTpl!: TemplateRef<any>;
   @ViewChild('paragraphTpl') paragraphTpl!: TemplateRef<any>;
   @ViewChild('richTextTpl') richTextTpl!: TemplateRef<any>;
   @ViewChild('dateTpl') dateTpl!: TemplateRef<any>;
@@ -191,14 +199,7 @@ export class PropertyValueSwitcherComponent implements OnInit, OnChanges, AfterV
       case Constants.ColorValue:
         return this.colorTpl;
       case Constants.TextValue:
-        switch (this.propertyDefinition.guiElement) {
-          case Constants.GuiRichText:
-            return this.richTextTpl;
-          case Constants.GuiTextarea:
-            return this.paragraphTpl;
-          default:
-            return this.textTpl;
-        }
+        return this._manageTextValue();
       case Constants.DateValue:
         return this.dateTpl;
       case Constants.TimeValue:
@@ -216,6 +217,39 @@ export class PropertyValueSwitcherComponent implements OnInit, OnChanges, AfterV
       default: {
         throw Error(`Unrecognized property ${this.propertyDefinition.objectType}`);
       }
+    }
+  }
+
+  private _manageTextValue() {
+    if (this.myProperty.values.length === 0) {
+      return this._defaultTextBehavior();
+    }
+
+    const value = this.myProperty.values[0] as ReadTextValueAsString | ReadTextValueAsXml | ReadTextValueAsHtml;
+
+    if (value instanceof ReadTextValueAsString) {
+      return this._defaultTextBehavior();
+    }
+
+    if (value instanceof ReadTextValueAsXml && value.mapping === Constants.StandardMapping) {
+      return this.richTextTpl;
+    }
+
+    if (value instanceof ReadTextValueAsHtml) {
+      return this.textHtmlTpl;
+    }
+
+    throw new Error('The text value is not supported');
+  }
+
+  private _defaultTextBehavior() {
+    switch (this.propertyDefinition.guiElement) {
+      case Constants.GuiRichText:
+        return this.richTextTpl;
+      case Constants.GuiTextarea:
+        return this.paragraphTpl;
+      default:
+        return this.textTpl;
     }
   }
 }
