a6568a600f7328dbfd371017d43f64256903a4ef
Julien Schneider <julien.schneider.1991@gmail.com>
2024-05-21 15:14:11 +0200

    fix: improve compound viewer

--- libs/vre/shared/app-resource-page/src/lib/compound/compound-viewer.component.ts
+++ libs/vre/shared/app-resource-page/src/lib/compound/compound-viewer.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
-import { FileRepresentation } from '@dasch-swiss/vre/shared/app-representations';
 import { IncomingRepresentationsService } from '../incoming-representations.service';
 
 @Component({
   selector: 'app-compound-viewer',
   template: ` <app-still-image
-    class="dsp-representation stillimage"
-    *ngIf="incomingRepresentationsService.representationsToDisplay.length > 0"
-    [image]="incomingRepresentationsService.representationsToDisplay[0]"></app-still-image>`,
+      class="dsp-representation stillimage"
+      *ngIf="incomingRepresentationsService.representationsToDisplay.length > 0"
+      [image]="incomingRepresentationsService.representationsToDisplay[0]"></app-still-image>
+
+    <h5>(DEV TEAM) The following elements will be integrated to the still image navigation soon</h5>
+    <div style="display: flex">
+      <app-compound-arrow-navigation [forwardNavigation]="false"></app-compound-arrow-navigation>
+      <app-compound-arrow-navigation [forwardNavigation]="true"></app-compound-arrow-navigation>
+      <app-compound-slider></app-compound-slider>
+      <app-compound-navigation></app-compound-navigation>
+    </div>`,
 })
 export class CompoundViewerComponent {
-  images: FileRepresentation[];
-
   constructor(public incomingRepresentationsService: IncomingRepresentationsService) {}
 }
