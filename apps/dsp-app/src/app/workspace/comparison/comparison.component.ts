4bc38ac9ffb77efb26e5b753fc4107f6d85cbb63
Julien Schneider <julien.schneider.1991@gmail.com>
2024-05-24 09:23:21 +0200

    fix

--- apps/dsp-app/src/app/workspace/comparison/comparison.component.ts
+++ apps/dsp-app/src/app/workspace/comparison/comparison.component.ts
@@ -1,33 +1,48 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ShortResInfo } from '../results/list-view/list-view.component';
 import { SplitSize } from '../results/results.component';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-comparison',
-  templateUrl: './comparison.component.html',
-  styleUrls: ['./comparison.component.scss'],
+  template: `
+    <div class="content">
+      <as-split direction="vertical">
+        <as-split-area>
+          <!-- note: This part is repeating twice (not added as component) because angular-split
+                                                              library does not support addition div inside as-split -->
+          <as-split direction="horizontal" (dragEnd)="splitSizeChanged = $event">
+            <as-split-area *ngFor="let res of topRow">
+              <app-resource-fetcher [resourceIri]="res" />
+            </as-split-area>
+          </as-split>
+        </as-split-area>
+        <as-split-area *ngIf="resourcesNumber > 3">
+          <as-split direction="horizontal" (dragEnd)="splitSizeChanged = $event">
+            <as-split-area *ngFor="let res of bottomRow">
+              <app-resource-fetcher [resourceIri]="res" />
+            </as-split-area>
+          </as-split>
+        </as-split-area>
+      </as-split>
+    </div>
+  `,
+  styles: [
+    `
+      .content {
+        width: 100%;
+        height: 1400px;
+      }
+    `,
+  ],
 })
 export class ComparisonComponent implements OnChanges {
-  /**
-   * number of resources
-   */
-  @Input() noOfResources?: number;
-
-  /**
-   * resource ids
-   */
+  @Input() resourcesNumber?: number;
   @Input() resourceIds?: string[];
-
-  /**
-   * list of resources with id and label
-   */
   @Input() resources?: ShortResInfo[];
 
   // parent (or own) split size changed
   @Input() splitSizeChanged: SplitSize;
 
-  // if number of selected resources > 3, divide them into 2 rows
   topRow: string[] = [];
   bottomRow: string[] = [];
 
@@ -36,18 +51,18 @@ export class ComparisonComponent implements OnChanges {
       this.resourceIds = this.resources.map(res => res.id);
     }
 
-    if (!this.noOfResources) {
-      this.noOfResources =
+    if (!this.resourcesNumber) {
+      this.resourcesNumber =
         this.resourceIds && this.resourceIds.length ? this.resourceIds.length : this.resources.length;
     }
 
     // if number of resources are more than 3, divide it into 2 rows
     // otherwise display then in 1 row only
-    if (this.noOfResources < 4) {
+    if (this.resourcesNumber < 4) {
       this.topRow = this.resourceIds;
     } else {
-      this.topRow = this.resourceIds.slice(0, this.noOfResources / 2);
-      this.bottomRow = this.resourceIds.slice(this.noOfResources / 2);
+      this.topRow = this.resourceIds.slice(0, this.resourcesNumber / 2);
+      this.bottomRow = this.resourceIds.slice(this.resourcesNumber / 2);
     }
   }
 }
