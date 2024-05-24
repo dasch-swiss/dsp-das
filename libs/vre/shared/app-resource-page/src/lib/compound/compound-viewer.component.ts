5d3d9c8e86c92b1f11947122222fbe6d08f6bfb1
Julien Schneider <julien.schneider.1991@gmail.com>
2024-05-24 13:01:53 +0200

    fix

--- libs/vre/shared/app-resource-page/src/lib/compound/compound-viewer.component.ts
+++ libs/vre/shared/app-resource-page/src/lib/compound/compound-viewer.component.ts
@@ -6,21 +6,16 @@ import { CompoundService } from './compound.service';
   selector: 'app-compound-viewer',
   template: `
     <ng-container *ngIf="compoundService.compoundPosition">
-      <app-still-image
-        class="dsp-representation stillimage"
-        *ngIf="
-          compoundService.incomingResource &&
-          fileRepresentation.fileValue &&
-          compoundService.incomingResource as incomingResource
-        "
-        [resource]="incomingResource">
-        <div navigationArrows class="arrows">
-          <app-compound-arrow-navigation [forwardNavigation]="false" class="arrow" />
-          <app-compound-arrow-navigation [forwardNavigation]="true" class="arrow" />
-        </div>
-        <app-compound-navigation navigation />
-        <app-compound-slider slider />
-      </app-still-image>
+      <ng-container *ngIf="compoundService.incomingResource as incomingResource">
+        <app-still-image class="dsp-representation stillimage" *ngIf="imageIsAccessible" [resource]="incomingResource">
+          <div navigationArrows class="arrows">
+            <app-compound-arrow-navigation [forwardNavigation]="false" class="arrow" />
+            <app-compound-arrow-navigation [forwardNavigation]="true" class="arrow" />
+          </div>
+          <app-compound-navigation navigation />
+          <app-compound-slider slider />
+        </app-still-image>
+      </ng-container>
     </ng-container>
   `,
   styles: [
@@ -44,6 +39,10 @@ import { CompoundService } from './compound.service';
 export class CompoundViewerComponent {
   get fileRepresentation() {
     return new FileRepresentation(getFileValue(this.compoundService.incomingResource!)!);
+  }
+
+  get imageIsAccessible() {
+    return this.fileRepresentation.fileValue;
   }
 
   constructor(public compoundService: CompoundService) {}
