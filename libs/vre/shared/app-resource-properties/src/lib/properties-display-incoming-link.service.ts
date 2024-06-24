b54fb06d6c790c0feb965683a231f1a22f29e6d9
Irmantas Kaukas <irmastnt@gmail.com>
2024-06-24 14:24:21 +0200

    getIncomingLinksRecursively

--- libs/vre/shared/app-resource-properties/src/lib/properties-display-incoming-link.service.ts
+++ libs/vre/shared/app-resource-properties/src/lib/properties-display-incoming-link.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ReadResourceSequence } from '@dasch-swiss/dsp-js';
+import { ReadResource, ReadResourceSequence } from '@dasch-swiss/dsp-js';
 import { IncomingService } from '@dasch-swiss/vre/shared/app-common-to-move';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { expand, map, reduce, takeWhile } from 'rxjs/operators';
 import { IncomingOrStandoffLink } from './incoming-link.interface';
 import { sortByKeys } from './sortByKeys';
 
@@ -11,18 +11,59 @@ export class PropertiesDisplayIncomingLinkService {
   constructor(private _incomingService: IncomingService) {}
 
   getIncomingLinks$(resourceId: string, offset: number): Observable<IncomingOrStandoffLink[]> {
+    return this._getIncomingLinks$(resourceId, offset);
+  }
+
+  getIncomingLinksRecursively$(resourceId: string, offset: number): Observable<IncomingOrStandoffLink[]> {
+    const incomingLinks$ = this._incomingService.getIncomingLinksForResource(resourceId, offset).pipe(
+      expand(response =>
+        this._incomingService.getIncomingLinksForResource(resourceId, offset++).pipe(
+          map(incomingResources => {
+            const array = (incomingResources as ReadResourceSequence).resources.map(resource => {
+              return this._createIncomingOrStandoffLink(resource);
+            });
+
+            return sortByKeys(array, ['project', 'label']);
+          })
+        )
+      ),
+      takeWhile(response => response.mayHaveMoreResults, true),
+      reduce((all, { data }) => all.concat(data), [])
+    );
+
     return this._incomingService.getIncomingLinksForResource(resourceId, offset).pipe(
       map(incomingResources => {
         const array = (incomingResources as ReadResourceSequence).resources.map(resource => {
-          return {
-            label: resource.label,
-            uri: `/resource/${resource.id.match(/[^\/]*\/[^\/]*$/)[0]}`,
-            project: resource.resourceClassLabel,
-          };
+          return this._createIncomingOrStandoffLink(resource);
         });
 
         return sortByKeys(array, ['project', 'label']);
       })
     );
+  }
+
+  private _getIncomingLinks$(resourceId: string, offset: number): Observable<IncomingOrStandoffLink[]> {
+    return this._incomingService.getIncomingLinksForResource(resourceId, offset).pipe(
+      map(incomingResources => {
+        const array = (incomingResources as ReadResourceSequence).resources.map(resource => {
+          return this._createIncomingOrStandoffLink(resource);
+        });
+
+        return sortByKeys(array, ['project', 'label']);
+      })
+    );
+  }
+
+  private _createIncomingOrStandoffLink(resource: ReadResource): IncomingOrStandoffLink {
+    const resourceIdPathOnly = resource.id.match(/[^\/]*\/[^\/]*$/);
+    if (!resourceIdPathOnly) {
+      throw new Error('Resource id is not in the expected format');
+    }
+
+    return {
+      label: resource.label,
+      uri: `/resource/${resourceIdPathOnly[0]}`,
+      project: resource.resourceClassLabel ? resource.resourceClassLabel : '',
+    };
   }
 }
