dd5786ab08eb27f08738a52124030d23451ea209
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-06-25 18:46:43 +0200

    extract options

--- k6/first-page-load.js
+++ k6/first-page-load.js
@@ -1,46 +1,9 @@
 import { browser } from 'k6/experimental/browser';
 import { check } from 'k6';
 import { Homepage } from './pages/homepage.js';
+import { defaultOptions } from './options/options.js';
 
-export const options = {
-  cloud: {
-    distribution: {
-      distributionLabel1: { loadZone: 'amazon:de:frankfurt', percent: 100 },
-    },
-  },
-  scenarios: {
-    smokeTest: {
-      // a small test that checks for major issues before spending more time and resources
-      executor: 'shared-iterations',
-      tags: { type: 'smoke-test' },
-      vus: 1,
-      iterations: 5,
-      maxDuration: '10s',
-
-      options: {
-        browser: {
-          type: 'chromium',
-        },
-      },
-    },
-    averageLoad: {
-      startTime: '10s',
-      executor: 'ramping-vus',
-      tags: { type: 'average-load' },
-      startVUs: 0,
-      stages: [
-        { duration: '20s', target: 9 },
-        { duration: '10s', target: 0 },
-      ],
-      gracefulRampDown: '0s',
-      options: {
-        browser: {
-          type: 'chromium',
-        },
-      },
-    },
-  },
-};
+export const options = defaultOptions;
 
 export default async function () {
   const page = browser.newPage();
