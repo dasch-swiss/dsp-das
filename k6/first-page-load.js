dfff15431f1e119c74d1048b6269dcdd722ab982
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-06-25 10:27:07 +0200

    add more options and check

--- k6/first-page-load.js
+++ k6/first-page-load.js
@@ -1,9 +1,24 @@
 import { browser } from 'k6/experimental/browser';
+import { check } from 'k6';
+import { HomePage } from './pages/homepage.js';
 
 export const options = {
+  vus: 10,
+  cloud: {
+    distribution: {
+      distributionLabel1: { loadZone: 'amazon:de:frankfurt', percent: 100 },
+    },
+  },
   scenarios: {
-    ui: {
+    browser: {
       executor: 'shared-iterations',
+
+      // executor-specific configuration
+      vus: 1,
+      iterations: 200,
+      maxDuration: '5s',
+      tags: { example_tag: 'testing' },
+
       options: {
         browser: {
           type: 'chromium',
@@ -19,9 +34,8 @@ export default async function () {
   try {
     await page.goto('https://app.dev.dasch.swiss/');
     page.screenshot({ path: 'screenshots/screenshot.png' });
-
     check(page, {
-      title: p => p.locator('/html[1]/head[1]/title[1]').textContent() == 'Projects overview',
+      title: p => p.locator('html>head>title').textContent() == 'Projects Overview',
     });
   } finally {
     page.close();
