aabb7c64ed32ab6d47b3d9cff4792b9d3504a71e
Irmantas Kaukas <irmastnt@gmail.com>
2024-06-26 15:27:18 +0200

    feat: incoming links paging and pager component (DEV-3792) (#1668)

--- libs/vre/shared/app-resource-properties/src/lib/properties-display-incoming-link.service.ts
+++ libs/vre/shared/app-resource-properties/src/lib/properties-display-incoming-link.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ReadResourceSequence } from '@dasch-swiss/dsp-js';
+import { ReadResource, ReadResourceSequence } from '@dasch-swiss/dsp-js';
 import { IncomingService } from '@dasch-swiss/vre/shared/app-common-to-move';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { IncomingOrStandoffLink } from './incoming-link.interface';
 import { sortByKeys } from './sortByKeys';
 
@@ -10,19 +10,34 @@ import { sortByKeys } from './sortByKeys';
 export class PropertiesDisplayIncomingLinkService {
   constructor(private _incomingService: IncomingService) {}
 
-  getIncomingLinks$(resourceId: string, offset: number): Observable<IncomingOrStandoffLink[]> {
-    return this._incomingService.getIncomingLinksForResource(resourceId, offset).pipe(
+  searchIncomingLinks$ = (resourceId: string, offset: number) =>
+    this._incomingService.getIncomingLinksForResource(resourceId, offset).pipe(shareReplay());
+
+  getIncomingLinks$(searchResults$: Observable<ReadResourceSequence>): Observable<IncomingOrStandoffLink[]> {
+    return searchResults$.pipe(
       map(incomingResources => {
         const array = (incomingResources as ReadResourceSequence).resources.map(resource => {
-          return {
-            label: resource.label,
-            uri: `/resource/${resource.id.match(/[^\/]*\/[^\/]*$/)[0]}`,
-            project: resource.resourceClassLabel,
-          };
+          return this._createIncomingOrStandoffLink(resource);
         });
 
         return sortByKeys(array, ['project', 'label']);
       })
     );
+  }
+
+  mayHaveMoreResults$ = (searchResults$: Observable<ReadResourceSequence>): Observable<boolean> =>
+    searchResults$.pipe(map(incomingResources => (incomingResources as ReadResourceSequence).mayHaveMoreResults));
+
+  private _createIncomingOrStandoffLink(resource: ReadResource): IncomingOrStandoffLink {
+    const resourceIdPathOnly = resource.id.match(/[^\/]*\/[^\/]*$/);
+    if (!resourceIdPathOnly) {
+      throw new Error('Resource id is not in the expected format');
+    }
+
+    return {
+      label: resource.label,
+      uri: `/resource/${resourceIdPathOnly[0]}`,
+      project: resource.resourceClassLabel ? resource.resourceClassLabel : '',
+    };
   }
 }
