e32ec60d40e997bbe54600f00e3599b8a3ed87cb
Julien Schneider <julien.schneider.1991@gmail.com>
2024-06-27 15:57:58 +0200

    fix

--- libs/vre/shared/app-resource-properties/src/lib/property-value-comment.component.ts
+++ libs/vre/shared/app-resource-properties/src/lib/property-value-comment.component.ts
@@ -16,7 +16,7 @@ import { PropertiesDisplayService } from './properties-display.service';
 
     <ng-template #editMode>
       <mat-form-field style="flex: 1; width: 100%; margin: 10px 0" subscriptSizing="dynamic" *ngIf="!displayMode">
-        <mat-label>Comment</mat-label>
+        <mat-label>{{ 'appLabels.resource.comment' | translate }}</mat-label>
         <mat-icon matPrefix style="color: #808080" *ngIf="control.disabled">lock</mat-icon>
         <textarea matInput rows="1" [formControl]="control"></textarea>
       </mat-form-field>
