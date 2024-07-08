dd209b638f3e8111faa66113e4e7fbe5b9cbb4b5
Julien Schneider <julien.schneider.1991@gmail.com>
2024-07-08 10:36:28 +0200

    fix: remove closed bracket in href link

--- libs/vre/shared/app-resource-properties/src/lib/properties-display.component.ts
+++ libs/vre/shared/app-resource-properties/src/lib/properties-display.component.ts
@@ -224,7 +224,7 @@ export class PropertiesDisplayComponent implements OnChanges, OnDestroy {
 
       return {
         label: link.strval ?? '',
-        uri: `/resource/${resourceIdPathOnly[0]}}`,
+        uri: `/resource/${resourceIdPathOnly[0]}`,
         project: link.linkedResource?.resourceClassLabel ?? '',
       };
     });
