7c044bd163e0a4a1593efbdfe1d350ae83ce7210
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-07-16 08:56:53 +0200

    test: Migrate tests to k6/browser which left experimental stage, run cloud test in dedicated project

--- k6/pages/resource-page.js
+++ k6/pages/resource-page.js
@@ -1,7 +1,7 @@
 export class ResourcePage {
   constructor(page) {
     this.page = page;
-    this.rsourcelabel = page.locator('.resource-header > .resource-label > h4');
+    this.resourcelabel = page.locator('.resource-header > .resource-label > h4');
   }
   async goto() {
     await this.page.goto(__ENV.APP_URL + '/project/yTerZGyxjZVqFMNNKXCDPF/ontology/beol/basicLetter');
--- k6/tests/load-resource.js
+++ k6/tests/load-resource.js
@@ -1,4 +1,4 @@
-import { browser } from 'k6/experimental/browser';
+import { browser } from 'k6/browser';
 import { check } from 'k6';
 import { ResourcePage } from '../pages/resource-page.js';
 import { defaultOptions } from '../options/options.js';
@@ -8,18 +8,22 @@ export const options = defaultOptions;
 export const errorCounter = new Counter('errors');
 
 export default async function () {
-  const page = browser.newPage();
+  const context = await browser.newContext();
+  const page = await context.newPage();
   const homepage = new ResourcePage(page);
 
   try {
     await homepage.goto();
+
+    await page.waitForNavigation();
+    const resourceLabel = await page.resourcelabel.textContent();
     const success = check(homepage, {
-      resourcelabel: p => p.rsourcelabel.textContent() == '1723-02-06_Scheuchzer_Johann_Jakob-Bernoulli_Johann_I',
+      resourcelabel: resourcelabel => resourcelabel == '1723-02-06_Scheuchzer_Johann_Jakob-Bernoulli_Johann_I',
     });
     if (!success) {
       errorCounter.add(1);
     }
-    page.screenshot({ path: 'screenshots/resource-page.png' });
+    await page.screenshot({ path: 'screenshots/resource-page.png' });
   } finally {
     page.close();
   }
