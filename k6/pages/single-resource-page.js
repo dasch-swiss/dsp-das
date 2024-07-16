a9aba96be0d2263f09b1b6e2bdb86a8e7457ce1b
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-07-16 17:11:16 +0200

    add a test for a resource on a single page without databrowser

---
+++ k6/pages/single-resource-page.js
@@ -0,0 +1,9 @@
+export class SingleResourcePage {
+  constructor(page) {
+    this.page = page;
+    this.resourcelabel = page.locator('.resource-header > .resource-label > h4');
+  }
+  async goto() {
+    await this.page.goto(__ENV.APP_URL + '/project/yTerZGyxjZVqFMNNKXCDPF/ontology/beol/basicLetter');
+  }
+}
