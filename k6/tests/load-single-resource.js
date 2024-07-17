427cb0342275391a04d8074ca0c0dae0f40841d1
Christian Kleinbölting <seakayone@users.noreply.github.com>
2024-07-17 16:18:47 +0200

    test: Migrate tests to k6/browser which left experimental stage, run cloud test in dedicated project (#1694)

--- k6/tests/load-single-resource.js
+++ k6/tests/load-single-resource.js
@@ -1,8 +1,8 @@
 import { browser } from 'k6/browser';
 import { check } from 'k6';
-import { ResourcePage } from '../pages/resource-page.js';
 import { defaultOptions } from '../options/options.js';
 import { Counter } from 'k6/metrics';
+import { SingleResourcePage } from '../pages/single-resource-page.js';
 
 export const options = defaultOptions;
 export const errorCounter = new Counter('errors');
@@ -10,7 +10,7 @@ export const errorCounter = new Counter('errors');
 export default async function () {
   const context = await browser.newContext();
   const page = await context.newPage();
-  const homepage = new ResourcePage(page);
+  const homepage = new SingleResourcePage(page);
 
   try {
     await homepage.goto();
@@ -18,12 +18,12 @@ export default async function () {
     await page.waitForNavigation();
     const resourceLabel = await page.resourcelabel.textContent();
     const success = check(homepage, {
-      resourceLabel: resourceLabel => resourceLabel == '1723-02-06_Scheuchzer_Johann_Jakob-Bernoulli_Johann_I',
+      resourceLabel: resourceLabel => resourceLabel == 'transcription of M087-10-TN',
     });
     if (!success) {
       errorCounter.add(1);
     }
-    await page.screenshot({ path: 'screenshots/resource-page.png' });
+    await page.screenshot({ path: 'screenshots/single-resource-page.png' });
   } finally {
     await page.close();
   }
