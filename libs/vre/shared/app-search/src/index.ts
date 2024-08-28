2103404366dab9a71e29beaf91732b7603dc4121
Julien Schneider <julien.schneider.1991@gmail.com>
2024-08-28 16:35:52 +0200

    wip

--- libs/vre/shared/app-search/src/index.ts
+++ libs/vre/shared/app-search/src/index.ts
@@ -1,1 +1,5 @@
-export * from './lib/vre-shared-app-search.module';
+export * from './lib/advanced-search/advanced-search-container.component';
+export * from './lib/expert-search/expert-search.component';
+export * from './lib/fulltext-search/fulltext-search.component';
+export * from './lib/search-panel/search-panel.component';
+export * from './lib/services/search-params.service';
---
+++ libs/vre/shared/app-search/src/search.components.ts
@@ -0,0 +1,11 @@
+import { AdvancedSearchContainerComponent } from './lib/advanced-search/advanced-search-container.component';
+import { ExpertSearchComponent } from './lib/expert-search/expert-search.component';
+import { FulltextSearchComponent } from './lib/fulltext-search/fulltext-search.component';
+import { SearchPanelComponent } from './lib/search-panel/search-panel.component';
+
+export const SearchComponents = [
+  AdvancedSearchContainerComponent,
+  ExpertSearchComponent,
+  FulltextSearchComponent,
+  SearchPanelComponent,
+];
