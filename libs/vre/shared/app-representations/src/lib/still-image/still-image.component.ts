1f2cc9ea49bb9a3c8dde2d128566cf1a4abfc323
domsteinbach <dominique.steinbach@unibas.ch>
2024-09-02 11:11:59 +0200

    fix: pass projectUuid via input

--- libs/vre/shared/app-representations/src/lib/still-image/still-image.component.ts
+++ libs/vre/shared/app-representations/src/lib/still-image/still-image.component.ts
@@ -30,6 +30,7 @@ import { ReadStillImageExternalFileValue } from '@dasch-swiss/dsp-js/src/models/
 import { ResourceUtil } from '@dasch-swiss/vre/shared/app-common';
 import { DialogComponent } from '@dasch-swiss/vre/shared/app-common-to-move';
 import { DspApiConnectionToken } from '@dasch-swiss/vre/shared/app-config';
+import { ProjectService } from '@dasch-swiss/vre/shared/app-helper-services';
 import { NotificationService } from '@dasch-swiss/vre/shared/app-notification';
 import { Store } from '@ngxs/store';
 import * as OpenSeadragon from 'openseadragon';
@@ -185,6 +186,7 @@ export class StillImageComponent implements OnInit, OnChanges, OnDestroy {
 
   openReplaceFileDialog() {
     const propId = this.resource.properties[Constants.HasStillImageFileValue][0].id;
+    const projectUuid = ProjectService.IriToUuid(this.resource.attachedToProject);
 
     const dialogConfig: MatDialogConfig = {
       width: '800px',
@@ -194,6 +196,7 @@ export class StillImageComponent implements OnInit, OnChanges, OnDestroy {
       },
       data: {
         mode: 'replaceFile',
+        projectUuid,
         title: '2D Image (Still Image)',
         subtitle: 'Update image of the resource',
         representation: 'stillImage',
