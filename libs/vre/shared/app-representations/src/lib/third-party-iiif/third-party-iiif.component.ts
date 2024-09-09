31e1ee06907ab5b221977d68020674ade19c5ffc
domsteinbach <dominique.steinbach@unibas.ch>
2024-09-09 11:29:01 +0200

    refactor: changes for updaing values

--- libs/vre/shared/app-representations/src/lib/third-party-iiif/third-party-iiif.component.ts
+++ libs/vre/shared/app-representations/src/lib/third-party-iiif/third-party-iiif.component.ts
@@ -1,13 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  forwardRef,
-  Input,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef, OnDestroy, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormControl, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
 import { ReadStillImageExternalFileValue } from '@dasch-swiss/dsp-js';
 import { CreateStillImageExternalFileValue } from '@dasch-swiss/dsp-js/src/models/v2/resources/values/create/create-file-value';
 import { UpdateExternalStillImageFileValue } from '@dasch-swiss/dsp-js/src/models/v2/resources/values/update/update-file-value';
@@ -33,7 +25,13 @@ export class ThirdPartyIiifComponent implements ControlValueAccessor, OnInit, On
   onChange!: (value: any) => void;
   onTouched!: () => void;
 
-  writeValue(value: null): void {}
+  writeValue(value: ReadStillImageExternalFileValue | null): void {
+    if (value) {
+      this.iiifUrlControl.patchValue(value?.externalUrl || '');
+      this._initialFileValue = value;
+      this.iiifUrlControl.updateValueAndValidity();
+    }
+  }
 
   registerOnChange(fn: any): void {
     this.onChange = fn;
@@ -43,11 +41,11 @@ export class ThirdPartyIiifComponent implements ControlValueAccessor, OnInit, On
     this.onTouched = fn;
   }
 
-  @Input() fileValue: ReadStillImageExternalFileValue; // if editing an existing file value
+  private _initialFileValue: ReadStillImageExternalFileValue | null = null; // if editing an existing file value
 
-  iiifUrlControl: FormControl<string>;
+  iiifUrlControl: FormControl<string | null>;
 
-  previewImageUrl: string | null;
+  previewImageUrl: string | undefined;
   formStatus: 'VALIDATING' | 'LOADING' | 'IDLE' = 'IDLE';
 
   private _destroy$ = new Subject<void>();
@@ -60,41 +58,37 @@ export class ThirdPartyIiifComponent implements ControlValueAccessor, OnInit, On
 
   readonly exampleString = 'https://example.org/image-service/abcd1234/full/max/0/default.jpg';
 
-  constructor(private _cdr: ChangeDetectorRef) {}
-
-  ngOnInit() {
-    this.iiifUrlControl = new FormControl(this.fileValue?.externalUrl || '', {
+  constructor(
+    private _cdr: ChangeDetectorRef,
+    private _fb: FormBuilder
+  ) {
+    this.iiifUrlControl = this._fb.control('', {
       validators: [Validators.required, iiifUrlValidator()],
       asyncValidators: [previewImageUrlValidatorAsync(), infoJsonUrlValidatorAsync()],
     });
+  }
 
-    // trigger validation if an existing url is passed
-    if (this.fileValue) {
-      this.iiifUrlControl.markAsPending();
-    }
-
+  ngOnInit() {
     this.iiifUrlControl.valueChanges.pipe(takeUntil(this._destroy$)).subscribe(urlStr => {
-      const iiifUrl = IIIFUrl.createUrl(urlStr);
+      const iiifUrl = IIIFUrl.createUrl(urlStr || '');
       this.previewImageUrl = iiifUrl?.previewImageUrl;
     });
 
     this.iiifUrlControl.statusChanges.pipe(takeUntil(this._destroy$)).subscribe(state => {
       this.formStatus = state === 'PENDING' ? 'VALIDATING' : 'IDLE';
-      if (this.formStatus === 'IDLE' && this.iiifUrlControl.valid) {
+      if (this.formStatus === 'IDLE' && this.iiifUrlControl.value && this.iiifUrlControl.valid) {
         const fileValue = this._getValue();
         fileValue.externalUrl = this.iiifUrlControl.value;
         this.onChange(fileValue);
-      } else {
-        this.onChange(null);
       }
       this._cdr.detectChanges();
     });
   }
 
   private _getValue(): CreateStillImageExternalFileValue | UpdateExternalStillImageFileValue {
-    if (this.fileValue) {
+    if (this._initialFileValue) {
       const updateValue: UpdateExternalStillImageFileValue = new UpdateExternalStillImageFileValue();
-      updateValue.id = this.fileValue.id;
+      updateValue.id = this._initialFileValue.id;
       return updateValue;
     } else {
       return new CreateStillImageExternalFileValue();
