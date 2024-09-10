8dc3b3350606948fd4d64cdd63b3ed945541b07b
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-09-10 17:25:04 +0200

    Update existing test, use k6chaijs for assertions

--- k6/tests/first-page-load.js
+++ k6/tests/first-page-load.js
@@ -1,30 +1,17 @@
 import { browser } from 'k6/browser';
-import { check } from 'k6';
 import { HomePage } from '../pages/home-page.js';
 import { defaultOptions } from '../options/options.js';
-import { Counter } from 'k6/metrics';
-
-const errorCounter = new Counter('errors');
+import { expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.0/index.js';
 
 export const options = defaultOptions;
 
 export default async function () {
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  const page = await browser.newPage();
   const homepage = new HomePage(page);
 
   try {
     await homepage.goto();
-
-    await page.waitForNavigation();
-    const title = await page.title.textContent();
-    let success = check(homepage, {
-      title: title => title == 'Projects Overview',
-    });
-
-    if (!success) {
-      errorCounter.add(1);
-    }
+    expect(await homepage.title()).to.equal('Projects Overview');
     await page.screenshot({ path: 'screenshots/homepage.png' });
   } finally {
     await page.close();
