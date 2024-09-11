765b103051b143a8be017e3d92fb9e60b3af9071
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-09-11 17:31:54 +0200

    rename test

--- k6/tests/workflow-login-success.js
+++ k6/tests/workflow-login-success.js
@@ -3,33 +3,18 @@ import { defaultOptions } from '../options/options.js';
 import { HomePage } from '../pages/home-page.js';
 import { OntologyEditorPage } from '../pages/ontology-editor-page.js';
 import { LOGIN_DATA, BEOL } from '../options/constants.js';
-import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
+import { expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
 
-//export const options = defaultOptions;
-export const options = {
-  scenarios: {
-    ui: {
-      executor: 'shared-iterations',
-      options: {
-        browser: {
-          type: 'chromium',
-        },
-      },
-    },
-  },
-  thresholds: {
-    checks: ['rate==1.0'],
-  },
-};
+export const options = defaultOptions;
 
 export default async function () {
   const page = await browser.newPage();
   const homePage = new HomePage(page);
   try {
-    console.log('loading home page');
+    console.log('Loading home page');
     await homePage.goto();
     await homePage.login();
-    console.log('logged in');
+    console.log('Logged in');
     await homePage.openUserMenu();
     console.log('Opened user menu');
     expect(await homePage.loggedInUser.textContent()).to.equal(LOGIN_DATA.fullname);
