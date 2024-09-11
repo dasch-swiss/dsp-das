292e3eee15f70ce7379c90ca237717ee65c7d34a
Christian Kleinbölting <seakayone@users.noreply.github.com>
2024-09-11 15:06:13 +0200

    refactor: Use k6chaijs for assertions (#1773)

--- k6/pages/resource-page.js
+++ k6/pages/resource-page.js
@@ -1,9 +1,13 @@
 export class ResourcePage {
   constructor(page) {
     this.page = page;
-    this.resourcelabel = page.locator('.resource-header > .resource-label > h4');
   }
+
   async goto() {
     await this.page.goto(__ENV.APP_URL + '/project/yTerZGyxjZVqFMNNKXCDPF/ontology/beol/basicLetter');
+  }
+
+  async resourceLabel() {
+    return await this.page.locator('.resource-header > .resource-label > h4').textContent();
   }
 }
