807fd704eeb9645eba2c224884bd80073466b410
domsteinbach <36757218+domsteinbach@users.noreply.github.com>
2024-10-07 09:23:36 +0200

    feat: implement download original functionality (DEV-4102) (#1803)

--- libs/vre/shared/app-common/src/lib/resource.util.ts
+++ libs/vre/shared/app-common/src/lib/resource.util.ts
@@ -1,6 +1,11 @@
 import { PermissionUtil, ReadResource, ReadValue } from '@dasch-swiss/dsp-js';
 
 export class ResourceUtil {
+  public static userCanView(instance: ReadResource | ReadValue) {
+    const permissions = PermissionUtil.allUserPermissions(instance.userHasPermission as 'RV' | 'V' | 'M' | 'D' | 'CR');
+    return permissions.includes(PermissionUtil.Permissions.V);
+  }
+
   public static userCanEdit(instance: ReadResource | ReadValue) {
     const permissions = PermissionUtil.allUserPermissions(instance.userHasPermission as 'RV' | 'V' | 'M' | 'D' | 'CR');
     return permissions.includes(PermissionUtil.Permissions.M);
--- libs/vre/shared/app-representations/src/lib/still-image/still-image.component.ts
+++ libs/vre/shared/app-representations/src/lib/still-image/still-image.component.ts
@@ -41,6 +41,7 @@ import { EditThirdPartyIiifFormComponent } from '../edit-third-party-iiif-form/e
 import { ThirdPartyIiifProps } from '../edit-third-party-iiif-form/edit-third-party-iiif-types';
 import { RegionService } from '../region.service';
 import { RepresentationService } from '../representation.service';
+import { FileInfo } from '../representation.types';
 import { ResourceFetcherService } from '../resource-fetcher.service';
 import { IIIFUrl } from '../third-party-iiif/third-party-iiif';
 import { osdViewerConfig } from './osd-viewer.config';
@@ -74,8 +75,14 @@ export class StillImageComponent implements OnInit, OnChanges, OnDestroy {
 
   isPng: boolean = false;
 
+  imageFileInfo: FileInfo | undefined = undefined;
+
   get isReadStillImageExternalFileValue(): boolean {
     return !!this.imageFileValue && this.imageFileValue.type === Constants.StillImageExternalFileValue;
+  }
+
+  get userCanView() {
+    return this.imageFileValue && ResourceUtil.userCanView(this.imageFileValue);
   }
 
   get usercanEdit() {
@@ -189,9 +196,9 @@ export class StillImageComponent implements OnInit, OnChanges, OnDestroy {
     this._notification.openSnackBar(message);
   }
 
-  download(url: string) {
-    if (this.imageFileValue instanceof ReadStillImageFileValue) {
-      this._rs.downloadFile(url, this.imageFileValue.filename);
+  download() {
+    if (this.imageFileValue?.fileUrl) {
+      this._rs.downloadFile(this.imageFileValue?.fileUrl, this.imageFileInfo?.originalFilename);
     }
   }
 
@@ -513,22 +520,29 @@ export class StillImageComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   private _loadInternalImages() {
-    if (this.imageFileValue instanceof ReadStillImageFileValue) {
-      this._rs
-        .getFileInfo(this.imageFileValue?.fileUrl || '', this.imageFileValue?.filename)
-        .pipe(take(1))
-        .subscribe(
-          () => {
-            if (this.failedToLoad) {
-              this._onSuccessAfterFailedImageLoad();
-            }
-            this._openInternalImages();
-          },
-          () => {
-            this._onFailedImageLoad();
-          }
-        );
+    const projectShort = this.resource.attachedToProject.split('/').pop();
+    const assetId = this.imageFileValue?.filename.split('.')[0] || '';
+
+    if (!projectShort || !assetId) {
+      this._onFailedImageLoad();
+      return;
     }
+
+    this._rs
+      .getIngestFileInfo(projectShort, assetId)
+      .pipe(take(1))
+      .subscribe(
+        (fileInfo: FileInfo) => {
+          this.imageFileInfo = fileInfo;
+          if (this.failedToLoad) {
+            this._onSuccessAfterFailedImageLoad();
+          }
+          this._openInternalImages();
+        },
+        () => {
+          this._onFailedImageLoad();
+        }
+      );
   }
 
   private _loadExternalIIIF() {
