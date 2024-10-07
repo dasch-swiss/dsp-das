63edf10f6b726ea1d3b5423de1dcb5f6f445131c
Julien Schneider <julien.schneider.1991@gmail.com>
2024-10-07 13:32:44 +0200

    fix: remove dialog component

--- libs/vre/shared/app-common-to-move/src/index.ts
+++ libs/vre/shared/app-common-to-move/src/index.ts
@@ -1,5 +1,4 @@
 export * from './common-to-move.components';
-export * from './lib/dialog/dialog.component';
 export * from './lib/dialog/dialog-header/dialog-header.component';
 export * from './lib/filtered-resources.interface';
 export * from './lib/gui-cardinality.interface';
