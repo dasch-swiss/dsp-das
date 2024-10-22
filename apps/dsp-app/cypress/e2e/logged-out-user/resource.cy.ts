429bfd0a769be43bd2498b16eef15532e8cf95c0
Irmantas Kaukas <irmastnt@gmail.com>
2024-10-22 13:11:23 +0200

    screenshot path fix

--- apps/dsp-app/cypress/e2e/logged-out-user/resource.cy.ts
+++ apps/dsp-app/cypress/e2e/logged-out-user/resource.cy.ts
@@ -71,7 +71,7 @@ describe('View Existing Resource', () => {
   };
 
   const headless = Cypress.browser.isHeadless ? 'headless' : '';
-  const screenshotsPath = Cypress.browser.isHeadless ? 'screenshots/resource.cy.ts' : 'screenshots';
+  const screenshotsPath = Cypress.browser.isHeadless ? 'screenshots/logged-out-user/resource.cy.ts' : 'screenshots';
   const uploadedImageFilePath = '/uploads/Fingerprint_Logo_coloured.png';
   const fullUploadedImageFilePathScaled = `cypress/uploads/Fingerprint_Logo_coloured1024x768${headless}.png`;
   const uploadedVideoFilePath = '/uploads/dasch-short.mp4';
