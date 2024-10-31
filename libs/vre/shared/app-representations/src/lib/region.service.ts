a15d8d48d28e10205a63f3c72cea9f6b3f97095f
Julien Schneider <julien.schneider.1991@gmail.com>
2024-10-31 10:54:44 +0100

    fix

--- libs/vre/shared/app-representations/src/lib/region.service.ts
+++ libs/vre/shared/app-representations/src/lib/region.service.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectorRef, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ReadResourceSequence } from '@dasch-swiss/dsp-js';
 import { DspResource, GenerateProperty } from '@dasch-swiss/vre/shared/app-common';
 import { IncomingService } from '@dasch-swiss/vre/shared/app-common-to-move';
 import { BehaviorSubject, of } from 'rxjs';
-import { map, switchMap, take } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 /**
  * Regions, also called annotations, are used to mark specific areas on an image.
@@ -30,13 +30,7 @@ export class RegionService {
   private _imageIsLoadedSubject = new BehaviorSubject(false);
   imageIsLoaded$ = this._imageIsLoadedSubject.asObservable();
 
-  private _getIncomingRegions$ =
-  );
-
-  constructor(
-    private _incomingService: IncomingService,
-    private _cd: ChangeDetectorRef
-  ) {}
+  constructor(private _incomingService: IncomingService) {}
 
   onInit(resource: DspResource) {
     this._resource = resource;
@@ -48,22 +42,24 @@ export class RegionService {
   }
 
   updateRegions() {
-      this._incomingService.getIncomingRegions(this._resource.res.id, 0).pipe(
-          map(regions =>
-              (regions as ReadResourceSequence).resources.map(_resource => {
-                  const z = new DspResource(_resource);
-                  z.resProps = GenerateProperty.regionProperty(_resource);
-                  return z;
-              })
-          )).subscribe(res => {
-      this._regionsSubject.next(res);
-      this._cd.markForCheck();
-    });
+    this._incomingService
+      .getIncomingRegions(this._resource.res.id, 0)
+      .pipe(
+        map(regions =>
+          (regions as ReadResourceSequence).resources.map(_resource => {
+            const z = new DspResource(_resource);
+            z.resProps = GenerateProperty.regionProperty(_resource);
+            return z;
+          })
+        )
+      )
+      .subscribe(res => {
+        this._regionsSubject.next(res);
+      });
   }
 
   highlightRegion(regionIri: string) {
     this._highlightRegion.next(regionIri);
-    this._cd.markForCheck();
   }
 
   imageIsLoaded() {
