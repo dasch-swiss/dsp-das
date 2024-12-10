898e26930091557fdc1916f127997f1e8ff115c2
domsteinbach <dominique.steinbach@unibas.ch>
2024-12-10 10:03:27 +0100

    feat: introduce page input, first page, last page, design

--- libs/vre/shared/app-ui/src/lib/pager/pager.component.ts
+++ libs/vre/shared/app-ui/src/lib/pager/pager.component.ts
@@ -1,59 +1,81 @@
 import { NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { TranslateModule } from '@ngx-translate/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'dasch-swiss-app-pager',
-  imports: [NgIf, MatIconModule, MatButtonModule, TranslateModule],
+  imports: [MatIconModule, MatButtonModule, TranslateModule, MatInputModule, MatTooltipModule, NgIf],
   templateUrl: './pager.component.html',
   styleUrls: ['./pager.component.scss'],
 })
-export class PagerComponent {
+export class PagerComponent implements OnChanges {
+  @Input() numberOfAllResults = 0;
   @Input() pageSize = 25;
-  @Input() numberOfAllResults: number | undefined = undefined;
-  @Input() lastItemOfPage: number | undefined = undefined;
+
   @Output() pageChanged: EventEmitter<number> = new EventEmitter<number>();
 
-  currentIndex = 0;
+  @ViewChild('pageInput') private _pageInput!: ElementRef<HTMLInputElement>;
 
-  get isLastOnPage(): boolean {
-    if (this.numberOfAllResults) {
-      return this.pageSize * (this.currentIndex + 1) >= this.numberOfAllResults;
-    } else {
-      return this.lastItemOfPage !== undefined && this.lastItemOfPage < this.pageSize;
-    }
+  private _pageIndex = 0;
+
+  get pageIndex(): number {
+    return this._pageIndex;
   }
 
-  get itemRangeStart(): number {
-    return this.pageSize * this.currentIndex + 1;
-  }
-
-  get itemRangeEnd(): number {
-    if (this.lastItemOfPage) {
-      return this.lastItemOfPage;
-    }
-    if (this.numberOfAllResults !== undefined) {
-      return this.pageSize * (this.currentIndex + 1) > this.numberOfAllResults
-        ? this.numberOfAllResults
-        : this.pageSize * (this.currentIndex + 1);
-    }
-    return 0;
+  set pageIndex(val: number) {
+    this._pageIndex = val;
+    this.pageChanged.emit(val);
   }
 
   get lastPageIndex(): number {
     return this.numberOfAllResults ? Math.ceil(this.numberOfAllResults / this.pageSize) - 1 : 0;
   }
 
-  get pages(): number[] {
-    return Array.from({ length: this.lastPageIndex + 1 }, (_, i) => i + 1);
+  get itemRange(): string {
+    const itemRangeStart = this.numberOfAllResults ? this._pageIndex * this.pageSize + 1 : 0;
+    const itemRangeEnd = Math.min((this._pageIndex + 1) * this.pageSize, this.numberOfAllResults);
+    return `${itemRangeStart} - ${itemRangeEnd} ${this._translate.instant('paginator.rangeLabelOf')} ${this.numberOfAllResults}`;
   }
 
-  goToPage(pageIndex: number) {
-    this.currentIndex = pageIndex;
-    this.pageChanged.emit(pageIndex);
+  constructor(private _translate: TranslateService) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['numberOfAllResults']) {
+      this._pageIndex = 0;
+    }
+  }
+
+  onPageInput(page: string) {
+    if (!page) {
+      return;
+    }
+    const pageNumber = parseInt(page, 10);
+    // if the user inputs a far off number set to the closest available page
+    const clampedPage = Math.max(1, Math.min(pageNumber, this.lastPageIndex + 1));
+    if (clampedPage !== pageNumber) {
+      this._pageInput.nativeElement.value = clampedPage.toString();
+    }
+    this.pageIndex = clampedPage - 1;
+  }
+
+  selectTextOnFocus(event: FocusEvent) {
+    const input = event.target as HTMLInputElement;
+    input.select();
   }
 }
