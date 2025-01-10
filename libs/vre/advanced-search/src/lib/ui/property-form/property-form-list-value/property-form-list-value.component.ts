313098d7ceb16c07097b77a23a98d4367d7b85b4
Irmantas Kaukas <irmastnt@gmail.com>
2025-01-10 18:01:13 +0100

    fix: property form list value item is focused by keypress when mat-menu is open in advanced search (DEV-4498)

--- libs/vre/advanced-search/src/lib/ui/property-form/property-form-list-value/property-form-list-value.component.ts
+++ libs/vre/advanced-search/src/lib/ui/property-form/property-form-list-value/property-form-list-value.component.ts
@@ -1,9 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { ListNodeV2, Constants } from '@dasch-swiss/dsp-js';
+import { MatMenuItem, MatMenuModule } from '@angular/material/menu';
+import { Constants, ListNodeV2 } from '@dasch-swiss/dsp-js';
 import { PropertyFormItem } from '../../../data-access/advanced-search-store/advanced-search-store.service';
 import { ListItemComponent } from './list-item/list-item.component';
 
@@ -15,14 +24,29 @@ import { ListItemComponent } from './list-item/list-item.component';
   styleUrls: ['./property-form-list-value.component.scss'],
 })
 export class PropertyFormListValueComponent implements AfterViewInit {
+  @HostListener('document:keydown', ['$event'])
+  onDocumentKeydown(event: KeyboardEvent) {
+    const isMenuOpen = this.menuItems && this.menuItems.length > 0;
+    if (isMenuOpen) {
+      this.onKeydown(event);
+    }
+  }
+
   @Input() list: ListNodeV2 | undefined = undefined;
   @Input() value: string | PropertyFormItem[] | undefined = undefined;
 
   @Output() emitValueChanged = new EventEmitter<string>();
+  @ViewChildren(MatMenuItem) menuItems!: QueryList<MatMenuItem>;
+  lastKeyPressTime: number = 0;
+  buffer: string = '';
 
   constants = Constants;
 
   selectedItem: string | undefined = undefined;
+
+  get sortedLabelList(): ListNodeV2[] | undefined {
+    return this.list?.children.sort((a, b) => a.label.localeCompare(b.label));
+  }
 
   ngAfterViewInit(): void {
     if (this.list && this.value && typeof this.value === 'string') {
@@ -48,5 +72,21 @@ export class PropertyFormListValueComponent implements AfterViewInit {
     }
 
     return undefined;
+  }
+
+  onKeydown(event: KeyboardEvent) {
+    const currentTime = new Date().getTime();
+    if (currentTime - this.lastKeyPressTime > 500) {
+      this.buffer = '';
+    }
+    this.lastKeyPressTime = currentTime;
+    this.buffer += event.key.toLowerCase();
+    const matchingIndex = this.sortedLabelList!.findIndex(item => {
+      return item.label.toLowerCase().startsWith(this.buffer);
+    });
+    if (matchingIndex !== -1) {
+      const matchingMenuItem = this.menuItems.toArray()[matchingIndex];
+      matchingMenuItem.focus();
+    }
   }
 }
