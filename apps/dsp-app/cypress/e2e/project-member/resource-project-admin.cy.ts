65c98a7857ea1d6f250f4acb6f6305dd9d9dbe56
Irmantas Kaukas <irmastnt@gmail.com>
2025-01-16 13:57:04 +0100

    fix: resource project admin e2e tests with populated data (DEV-4438)

--- apps/dsp-app/cypress/e2e/project-member/resource-project-admin.cy.ts
+++ apps/dsp-app/cypress/e2e/project-member/resource-project-admin.cy.ts
@@ -91,11 +91,10 @@ describe('Check project admin existing resource functionality', () => {
     cy.get('[data-cy="show-all-properties"]').click();
     cy.get('[data-cy=add-property-value-button]').scrollIntoView();
     cy.get('[data-cy="add-property-value-button"]').click();
-    cy.get('[data-cy="add-property-value-button"]').should('not.exist', { timeout: 500 });
 
     cy.intercept('GET', `**/resources/**`).as('resourcesRequest');
     const newLabel = faker.lorem.word();
-    cy.get('[data-cy=common-input-text]').should('be.visible').type(newLabel);
+    cy.get('[data-cy=common-input-text]').should('be.visible', { timeout: 500 }).type(newLabel);
     const firstComment = faker.lorem.word();
     cy.get('[data-cy=comment-textarea]').should('be.visible').type(firstComment);
     cy.get('[data-cy="save-button"]').click();
