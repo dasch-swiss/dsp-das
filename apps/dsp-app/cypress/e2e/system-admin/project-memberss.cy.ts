1b5a762117c5786511e4b183478dc858c75b3448
Irmantas Kaukas <irmastnt@gmail.com>
2025-01-28 12:22:39 +0100

    test: remove user from a project (DEV-2328)

---
+++ apps/dsp-app/cypress/e2e/system-admin/project-memberss.cy.ts
@@ -0,0 +1,34 @@
+import { Project0001Page } from '../../support/pages/existing-ontology-class-page';
+
+describe('Project members', () => {
+  let project0001Page: Project0001Page;
+
+  before(() => {
+    cy.resetDatabase();
+    project0001Page = new Project0001Page();
+  });
+
+  it('admin can remove user from a project', () => {
+    cy.intercept('DELETE', '/admin/users/**').as('deleteRequest');
+    cy.visit(`/project/${Project0001Page.projectShortCode}/settings/collaboration`);
+    let memberCount = 0;
+    cy.get('[data-cy=member-count]')
+      .should('be.visible')
+      .invoke('text')
+      .then(text => {
+        memberCount = parseInt(text.match(/\d+/)?.[0]);
+        expect(memberCount).to.be.greaterThan(0);
+      });
+
+    cy.get('[data-cy=user-menu]').should('exist').first().click();
+    cy.get('[data-cy=remove-member-button]').should('be.visible').click();
+    cy.get('[data-cy=confirmation-button]').should('be.visible').click();
+    cy.wait('@deleteRequest').its('response.statusCode').should('eq', 200);
+    cy.get('[data-cy=member-count]')
+      .invoke('text')
+      .then(text => {
+        const currentMemberCount = parseInt(text.match(/\d+/)?.[0]);
+        expect(currentMemberCount).to.be.eq(memberCount - 1);
+      });
+  });
+});
