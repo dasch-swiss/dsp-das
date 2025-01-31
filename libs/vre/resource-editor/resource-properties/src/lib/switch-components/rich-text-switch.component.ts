9d89854af89639f498ddb5c460e05f50f16608b7
Julien Schneider <julien.schneider.1991@gmail.com>
2025-01-31 14:07:03 +0100

    fix

--- libs/vre/resource-editor/resource-properties/src/lib/switch-components/rich-text-switch.component.ts
+++ libs/vre/resource-editor/resource-properties/src/lib/switch-components/rich-text-switch.component.ts
@@ -89,7 +89,7 @@ export class RichTextSwitchComponent implements IsSwitchComponent, OnChanges {
     let newValue = controlValue;
     if (matches) {
       Array.from(matches).forEach(matchArray => {
-        const uuid = Math.random().toString(36).substring(7);
+        const uuid = window.crypto.getRandomValues(new Uint32Array(1))[0].toString();
         const parsedFootnote = `<footnote content="${matchArray[1]}" id="${uuid}">${this._footnoteService.footnotes.length + 1}</footnote>`;
         newValue = newValue.replace(matchArray[0], parsedFootnote);
         this._footnoteService.addFootnote(
