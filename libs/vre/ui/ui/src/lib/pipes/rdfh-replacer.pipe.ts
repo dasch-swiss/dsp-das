ebbe53f7b3bde1fdb6803ff4c7db611f0b2a01a4
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-04 18:10:38 +0100

    fix

--- libs/vre/ui/ui/src/lib/pipes/rdfh-replacer.pipe.ts
+++ libs/vre/ui/ui/src/lib/pipes/rdfh-replacer.pipe.ts
@@ -15,10 +15,13 @@ export class HrefReplacerPipe implements PipeTransform {
     let htmlString = typeof value === 'string' ? value : value['changingThisBreaksApplicationSecurity'];
 
     // Replace href="http://rdfh/..." with href="/..."
-    htmlString = htmlString.replace(/<a\s+href="http:\/\/rdfh\.ch\/([^"]+)".*>([^<]+)<\/a>/g, (_, path, content) => {
-      const newHref = `href="/resource/${path}"`; // Replace 'rdfh/' with '/'
-      return `<a ${newHref} target="_blank">${content}</a>`;
-    });
+    htmlString = htmlString.replace(
+      /<a\s+href="http:\/\/rdfh\.ch\/([^"]+)".*>([^<]+)<\/a>/g,
+      (_: any, path: string, content: string) => {
+        const newHref = `href="/resource/${path}"`; // Replace 'rdfh/' with '/'
+        return `<a ${newHref} target="_blank">${content}</a>`;
+      }
+    );
 
     // Sanitize the modified HTML string back to SafeHtml
     return this.sanitizer.bypassSecurityTrustHtml(htmlString);
