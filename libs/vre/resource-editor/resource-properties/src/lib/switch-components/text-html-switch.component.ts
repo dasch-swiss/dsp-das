743b8c27f1963ddb794d5cf8291c026e1fd255b7
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-05 15:00:53 +0100

    fix

--- libs/vre/resource-editor/resource-properties/src/lib/switch-components/text-html-switch.component.ts
+++ libs/vre/resource-editor/resource-properties/src/lib/switch-components/text-html-switch.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ReadLinkValue } from '@dasch-swiss/dsp-js';
-import { ResourceService } from '@dasch-swiss/vre/shared/app-common';
 import { IsSwitchComponent } from './is-switch-component.interface';
 
 @Component({
@@ -16,12 +14,4 @@ import { IsSwitchComponent } from './is-switch-component.interface';
 export class TextHtmlSwitchComponent implements IsSwitchComponent {
   @Input() control!: FormControl<string | null>;
   @Input() displayMode = true;
-
-  constructor(private _resourceService: ResourceService) {}
-
-  _openResource(linkValue: ReadLinkValue | string) {
-    const iri = typeof linkValue == 'string' ? linkValue : linkValue.linkedResourceIri;
-    const path = this._resourceService.getResourcePath(iri);
-    window.open(`/resource${path}`, '_blank');
-  }
 }
