8077473f4827e7a0d177ea66ab6e0b99050dbbb8
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-07 17:36:34 +0100

    wip

--- libs/vre/ui/ui/src/lib/ck-editor/ck-editor.component.ts
+++ libs/vre/ui/ui/src/lib/ck-editor/ck-editor.component.ts
@@ -1,16 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import * as Editor from 'ckeditor5-custom-build/build/ckeditor';
 import { ckEditor } from './ck-editor';
 
 @Component({
   selector: 'app-ck-editor',
-  template: ` <ckeditor [formControl]="control" [config]="ckEditor.config" [editor]="editor" />
+  template: ` <ckeditor [formControl]="footnoteControl" [config]="ckEditor.config" [editor]="editor" />
     <mat-error *ngIf="control.touched && control.errors as errors">{{ errors | humanReadableError }}</mat-error>`,
 })
-export class CkEditorComponent {
+export class CkEditorComponent implements OnInit {
   @Input({ required: true }) control!: FormControl<string>;
-
+  footnoteControl = new FormControl<string>('');
   readonly editor = Editor;
   protected readonly ckEditor = ckEditor;
+
+  ngOnInit() {
+    this.footnoteControl.setValue(this.control.value);
+    this.footnoteControl.valueChanges.subscribe(value => {
+      this.control.setValue(this.test(value));
+    });
+  }
+
+  test(rawHtml: string) {
+    const _footnoteRegExp = /<footnote content="([^>]+)">([^<]*)<\/footnote>/g;
+    return rawHtml.replace(_footnoteRegExp, (match, content) => {
+      const escapedContent = this._unescapeHtml(content);
+      return `<footnote content="${escapedContent}"></footnote>`;
+    });
+  }
+
+  private _unescapeHtml(str: string) {
+    const unescapeMap = {
+      '&amp;': '&',
+      '&lt;': '<',
+      '&gt;': '>',
+      '&quot;': '"',
+      '&#039;': "'",
+    };
+    return str.replace(/&(amp|lt|gt|quot|#039);/g, match => unescapeMap[match]);
+  }
 }
