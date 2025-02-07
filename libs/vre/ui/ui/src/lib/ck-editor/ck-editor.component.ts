c05fbe55ecfdf07cbd456a01f2c446fa13b63423
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-07 18:07:57 +0100

    fix

--- libs/vre/ui/ui/src/lib/ck-editor/ck-editor.component.ts
+++ libs/vre/ui/ui/src/lib/ck-editor/ck-editor.component.ts
@@ -11,24 +11,26 @@ import { ckEditor } from './ck-editor';
 export class CkEditorComponent implements OnInit {
   @Input({ required: true }) control!: FormControl<string>;
   footnoteControl = new FormControl<string>('');
+
   readonly editor = Editor;
   protected readonly ckEditor = ckEditor;
 
   ngOnInit() {
-    this.footnoteControl.setValue(this.control.value ? this.reverse(this.control.value) : null);
+    this.footnoteControl.setValue(this.control.value ? this._parseToFootnote(this.control.value) : null);
+
     this.footnoteControl.valueChanges.subscribe(value => {
-      this.control.setValue(this.test(value));
+      this.control.setValue(this._parseFromFootnote(value));
     });
   }
 
-  reverse(rawHtml: string) {
+  private _parseToFootnote(rawHtml: string) {
     const _footnoteRegExp2 = /<footnote content="([^>]+)"\/>/g;
     return rawHtml.replace(_footnoteRegExp2, (match, content) => {
       return `<footnote content="${content}">[Footnote]</footnote>`;
     });
   }
 
-  test(rawHtml: string) {
+  private _parseFromFootnote(rawHtml: string) {
     const _footnoteRegExp = /<footnote content="([^>]+)">([^<]*)<\/footnote>/g;
     return rawHtml.replace(_footnoteRegExp, (match, content) => {
       const escapedContent = this._unescapeHtml(content);
