ab0912f79905335393ef416be208e12406771aa6
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-12 14:15:53 +0100

    fix

--- libs/vre/pages/user-settings/settings/src/index.ts
+++ libs/vre/pages/user-settings/settings/src/index.ts
@@ -1,5 +1,5 @@
 export * from './settings-page.components';
-export * from './lib/settings-page.component';
+export * from './lib/image-settings/settings-page.component';
 export * from './lib/image-settings/image-display-absolute.component';
 export * from './lib/image-settings/image-display-ratio.component';
 export * from './lib/image-settings/image-settings.component';
