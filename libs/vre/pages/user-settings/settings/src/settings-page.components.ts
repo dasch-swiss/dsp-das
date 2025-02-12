b9febdd193e87c9dc2050b34952fe4aa625e05c0
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-12 14:19:03 +0100

    fix

--- libs/vre/pages/user-settings/settings/src/settings-page.components.ts
+++ libs/vre/pages/user-settings/settings/src/settings-page.components.ts
@@ -1,7 +1,7 @@
-import { ImageDisplayAbsoluteComponent } from './lib/image-settings/image-display-absolute.component';
-import { ImageDisplayRatioComponent } from './lib/image-settings/image-display-ratio.component';
-import { ImageSettingsComponent } from './lib/image-settings/image-settings.component';
-import { SettingsPageComponent } from './lib/image-settings/settings-page.component';
+import { ImageDisplayAbsoluteComponent } from '../../../project/project/src/lib/image-settings/image-display-absolute.component';
+import { ImageDisplayRatioComponent } from '../../../project/project/src/lib/image-settings/image-display-ratio.component';
+import { ImageSettingsComponent } from '../../../project/project/src/lib/image-settings/image-settings.component';
+import { SettingsPageComponent } from '../../../project/project/src/lib/image-settings/settings-page.component';
 
 export const SettingsPageComponents = [
   SettingsPageComponent,
