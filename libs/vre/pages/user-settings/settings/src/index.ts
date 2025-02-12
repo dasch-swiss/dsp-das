37ce8fe15b682c341a0e3c2e0e79b8142640cacd
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-12 14:29:44 +0100

    fix

--- libs/vre/pages/user-settings/settings/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './settings-page.components';
-export * from '../../../project/project/src/lib/image-settings/settings-page.component';
-export * from '../../../project/project/src/lib/image-settings/image-display-absolute.component';
-export * from '../../../project/project/src/lib/image-settings/image-display-ratio.component';
-export * from '../../../project/project/src/lib/image-settings/image-settings.component';
