da3d57681497d6ea602b18fb1dca8a216cfd2926
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-13 14:27:22 +0100

    fix

--- libs/vre/ui/ui/src/lib/incoming-resource-pager/incoming-resource-pager.component.ts
+++ libs/vre/ui/ui/src/lib/incoming-resource-pager/incoming-resource-pager.component.ts
@@ -1,14 +1,31 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-incoming-resource-pager',
-  imports: [MatIconModule, MatButtonModule, TranslateModule],
-  templateUrl: './incoming-resource-pager.component.html',
+  template: `
+    <div class="paging-container">
+      <div class="navigation">
+        <button mat-button class="pagination-button previous" [disabled]="pageIndex === 0" (click)="changePage(-1)">
+          <mat-icon>west</mat-icon>
+          <span>{{ 'uiControls.pager.previous' | translate }}</span>
+        </button>
+        <span class="fill-remaining-space"></span>
+        <div class="range">
+          <span>{{ itemRangeStart }} - {{ itemRangeEnd }} of {{ itemsNumber }}</span>
+        </div>
+        <span class="fill-remaining-space"></span>
+        <button
+          mat-button
+          class="pagination-button next"
+          [disabled]="itemRangeEnd === itemsNumber"
+          (click)="changePage(1)">
+          <span>{{ 'uiControls.pager.next' | translate }}</span>
+          <mat-icon>east</mat-icon>
+        </button>
+      </div>
+    </div>
+  `,
   styleUrls: ['./incoming-resource-pager.component.scss'],
 })
 export class IncomingResourcePagerComponent {
