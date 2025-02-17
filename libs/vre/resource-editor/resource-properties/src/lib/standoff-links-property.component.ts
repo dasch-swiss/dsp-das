3e26477cc1d06f5fb5326138030a64d954e3392f
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-17 15:25:13 +0100

    fix: paging of incoming links works again (#2108)
    Co-authored-by: domsteinbach <dominique.steinbach@unibas.ch>
    Co-authored-by: domsteinbach <36757218+domsteinbach@users.noreply.github.com>

---
+++ libs/vre/resource-editor/resource-properties/src/lib/standoff-links-property.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, OnChanges } from '@angular/core';
+import { Constants, ReadLinkValue } from '@dasch-swiss/dsp-js';
+import { AppError } from '@dasch-swiss/vre/core/error-handler';
+import { DspResource } from '@dasch-swiss/vre/shared/app-common';
+import { IncomingOrStandoffLink } from './incoming-link.interface';
+import { sortByKeys } from './sortByKeys';
+
+@Component({
+  selector: 'app-standoff-links-property',
+  template: ` <app-property-row
+    [tooltip]="'resource.standoffLink.tooltip' | translate"
+    [label]="'resource.standoffLink.label' | translate"
+    [isEmptyRow]="standoffLinks.length > 0"
+    [borderBottom]="true">
+    <app-incoming-standoff-link-value [links]="standoffLinks" />
+  </app-property-row>`,
+})
+export class StandoffLinksPropertyComponent implements OnChanges {
+  @Input({ required: true }) resource!: DspResource;
+
+  standoffLinks: IncomingOrStandoffLink[] = [];
+
+  ngOnChanges() {
+    this.standoffLinks = (
+      (this.resource.resProps.find(prop => prop.propDef.id === Constants.HasStandoffLinkToValue)
+        ?.values as ReadLinkValue[]) ?? []
+    ).map(link => {
+      const parts = link.linkedResourceIri.split('/');
+      if (parts.length < 2) {
+        throw new AppError('Linked resource IRI is not in the expected format');
+      }
+
+      const resourceIdPathOnly = parts.slice(-2).join('/');
+
+      return {
+        label: link.strval ?? '',
+        uri: `/resource/${resourceIdPathOnly}`,
+        project: link.linkedResource?.resourceClassLabel ?? '',
+      };
+    });
+    this.standoffLinks = sortByKeys(this.standoffLinks, ['project', 'label']);
+  }
+}
