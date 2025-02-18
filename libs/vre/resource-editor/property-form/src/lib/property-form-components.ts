325d1653dd59e32072c5d37cfdde79e28b9e0999
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-18 16:43:49 +0100

    fix: remove multiple slide toggle

--- libs/vre/resource-editor/property-form/src/lib/property-form-components.ts
+++ libs/vre/resource-editor/property-form/src/lib/property-form-components.ts
@@ -2,7 +2,6 @@ import { CreatePropertyFormDialogComponent } from './components/create-property-
 import { EditPropertyFormDialogComponent } from './components/edit-property-form-dialog.component';
 import { GuiAttrLinkComponent } from './components/gui-attr-link.component';
 import { GuiAttrListComponent } from './components/gui-attr-list.component';
-import { MultipleSlideToggleComponent } from './components/multiple-slide-toggle.component';
 import { PropertyFormComponent } from './components/property-form.component';
 
 export const PropertyFormComponents = [
@@ -10,6 +9,5 @@ export const PropertyFormComponents = [
   EditPropertyFormDialogComponent,
   GuiAttrLinkComponent,
   GuiAttrListComponent,
-  MultipleSlideToggleComponent,
   PropertyFormComponent,
 ];
