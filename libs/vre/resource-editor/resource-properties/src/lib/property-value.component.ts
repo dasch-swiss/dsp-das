6b3942ff3c8d100d4adcaac8c7b1f6f949126c1c
Julien Schneider <julien.schneider.1991@gmail.com>
2025-02-24 16:36:52 +0100

    fix

--- libs/vre/resource-editor/resource-properties/src/lib/property-value.component.ts
+++ libs/vre/resource-editor/resource-properties/src/lib/property-value.component.ts
@@ -38,7 +38,9 @@ import { propertiesTypeMapping } from './resource-payloads-mapping';
     (mouseenter)="showBubble = true"
     (mouseleave)="showBubble = false">
     <app-property-value-action-bubble
-      *ngIf="!propertyValueService.keepEditMode && showBubble"
+      *ngIf="
+        !propertyValueService.keepEditMode && showBubble && (propertyValueService.lastOpenedItem$ | async) !== index
+      "
       [date]="propertyValueService.editModeData?.values[index]?.valueCreationDate"
       [showDelete]="index > 0 || [Cardinality._0_1, Cardinality._0_n].includes(propertyValueService.cardinality)"
       (editAction)="propertyValueService.toggleOpenedValue(index)"
