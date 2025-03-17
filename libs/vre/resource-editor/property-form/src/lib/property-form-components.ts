3edce46c378a2d7fb005f07675c4b16271a8c95a
domsteinbach <36757218+domsteinbach@users.noreply.github.com>
2025-03-17 11:04:11 +0100

    fix(ontology editor): create page number as seqnum subproperty (#2157)
    Co-authored-by: Julien Schneider <julien.schneider.1991@gmail.com>

--- libs/vre/resource-editor/property-form/src/lib/property-form-components.ts
+++ libs/vre/resource-editor/property-form/src/lib/property-form-components.ts
@@ -1,11 +1,9 @@
-import { CreatePropertyFormDialogComponent } from './components/create-property-form-dialog.component';
 import { EditPropertyFormDialogComponent } from './components/edit-property-form-dialog.component';
 import { GuiAttrLinkComponent } from './components/gui-attr-link.component';
 import { GuiAttrListComponent } from './components/gui-attr-list.component';
 import { PropertyFormComponent } from './components/property-form.component';
 
 export const PropertyFormComponents = [
-  CreatePropertyFormDialogComponent,
   EditPropertyFormDialogComponent,
   GuiAttrLinkComponent,
   GuiAttrListComponent,
