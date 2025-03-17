f34a326338b9c96fb06116e5b30ea0c643c0473f
Julien Schneider <julien.schneider.1991@gmail.com>
2025-03-17 12:01:47 +0100

    fix: standoff

--- libs/vre/ui/ui/src/lib/ck-editor/ck-editor.ts
+++ libs/vre/ui/ui/src/lib/ck-editor/ck-editor.ts
@@ -1,3 +1,5 @@
+import { Constants } from '@dasch-swiss/dsp-js';
+
 export class ckEditor {
   static config = {
     entities: false,
@@ -57,6 +59,21 @@ export class ckEditor {
       languages: [{ language: 'plaintext', label: 'Plain text', class: '' }],
     },
     language: 'en',
+    link: {
+      addTargetToExternalLinks: false,
+      decorators: {
+        isInternal: {
+          // label: 'internal link to a Knora resource',
+          mode: 'automatic', // automatic requires callback -> but the callback is async and the user could save the text before the check ...
+          callback: (
+            url: string // console.log(url, url.startsWith( 'http://rdfh.ch/' ));
+          ) => !!url && url.startsWith('http://rdfh.ch/'), // --> TODO: get this from config via AppInitService
+          attributes: {
+            class: Constants.SalsahLink,
+          },
+        },
+      },
+    },
     table: {
       contentToolbar: ['tableColumn', 'tableRow'], // mergeTableCells is not supported by the backend due to colspan html tag mapping.
     },
