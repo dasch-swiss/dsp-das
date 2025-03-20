f410f00ec36661b6b5c887b71e9e558e7724c712
Julien Schneider <julien.schneider.1991@gmail.com>
2025-03-20 11:20:32 +0100

    fix

--- libs/vre/resource-editor/resource-properties/src/lib/create-resource-form-legal.component.ts
+++ libs/vre/resource-editor/resource-properties/src/lib/create-resource-form-legal.component.ts
@@ -1,24 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { ReadProject } from '@dasch-swiss/dsp-js';
 import { AdminProjectsLegalInfoApiService } from '@dasch-swiss/vre/3rd-party-services/open-api';
 import { ProjectsSelectors } from '@dasch-swiss/vre/core/state';
 import { Store } from '@ngxs/store';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { filter, finalize, map, switchMap, take } from 'rxjs/operators';
 import { CreateResourceFormInterface } from './create-resource-form.interface';
 
 @Component({
   selector: 'app-create-resource-form-legal',
   template: `
     <h3>Legal infos</h3>
-
+    GG
     <app-create-resource-form-row [label]="'Copyright holder'" [tooltip]="'Copyright holder'">
       <mat-form-field>
         <mat-select placeholder="Choose" [formControl]="formGroup.controls.copyrightHolder">
+          <mat-option *ngIf="copyrightHolderLoading">Loading.. {{ copyrightHolderLoading }}</mat-option>
           <mat-option *ngFor="let copyrightHolder of copyrightHolders$ | async" [value]="copyrightHolder"
             >{{ copyrightHolder }}
           </mat-option>
         </mat-select>
       </mat-form-field>
     </app-create-resource-form-row>
+    A{{ copyrightHolderLoading }}B
 
     <app-create-resource-form-row [label]="'License'" [tooltip]="'License'">
       <mat-form-field>
@@ -43,34 +46,41 @@ import { CreateResourceFormInterface } from './create-resource-form.interface';
 export class CreateResourceFormLegalComponent {
   @Input({ required: true }) formGroup!: CreateResourceFormInterface['legal'];
 
-  readonly project$ = this._store.select(ProjectsSelectors.currentProject);
+  readonly project$ = this._store.select(ProjectsSelectors.currentProject).pipe(
+    filter(project => project !== undefined),
+    map(project => project as ReadProject),
+    take(1)
+  );
+
+  copyrightHolderLoading = true;
 
   copyrightHolders$ = this.project$.pipe(
-    filter(project => project !== undefined),
     switchMap(project =>
-      this._copyrightApi.getAdminProjectsShortcodeProjectshortcodeLegalInfoCopyrightHolders(project!.shortcode)
+      this._copyrightApi.getAdminProjectsShortcodeProjectshortcodeLegalInfoCopyrightHolders(project.shortcode)
     ),
-    map(data => data.data)
+    map(data => data.data),
+    finalize(() => {
+      this.copyrightHolderLoading = false;
+    })
   );
 
   licenses$ = this.project$.pipe(
-    filter(project => project !== undefined),
     switchMap(project =>
-      this._copyrightApi.getAdminProjectsShortcodeProjectshortcodeLegalInfoLicenses(project!.shortcode)
+      this._copyrightApi.getAdminProjectsShortcodeProjectshortcodeLegalInfoLicenses(project.shortcode)
     ),
     map(data => data.data)
   );
 
   authorships$ = this.project$.pipe(
-    filter(project => project !== undefined),
     switchMap(project =>
-      this._copyrightApi.getAdminProjectsShortcodeProjectshortcodeLegalInfoAuthorships(project!.shortcode)
+      this._copyrightApi.getAdminProjectsShortcodeProjectshortcodeLegalInfoAuthorships(project.shortcode)
     ),
     map(data => data.data)
   );
 
   constructor(
     private _store: Store,
-    private _copyrightApi: AdminProjectsLegalInfoApiService
+    private _copyrightApi: AdminProjectsLegalInfoApiService,
+    private _cdr: ChangeDetectorRef
   ) {}
 }
