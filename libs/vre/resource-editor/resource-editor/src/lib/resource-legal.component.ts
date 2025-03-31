4812bb785300ee4345a621ae9e1e4d4dcd34cc43
Julien Schneider <julien.schneider.1991@gmail.com>
2025-03-31 14:43:33 +0200

    wip

--- libs/vre/resource-editor/resource-editor/src/lib/resource-legal.component.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/resource-legal.component.ts
@@ -1,24 +1,34 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { getFileValue } from '@dasch-swiss/vre/resource-editor/representations';
+import { ReadFileValue } from '@dasch-swiss/dsp-js';
+import { getFileValue, ResourceUtil } from '@dasch-swiss/vre/resource-editor/representations';
 import { DspResource } from '@dasch-swiss/vre/shared/app-common';
 
 @Component({
   selector: 'app-resource-legal',
-  template: ` <div>Copyright holder {{ legalInfos.copyrightHolder }}</div>
-    <div style="display: flex; justify-content: space-between">
-      <div>License {{ legalInfos.license.labelEn }}</div>
+  template: ` <div *ngIf="userCanEdit || fileValue.copyrightHolder !== ''">
+      Copyright holder {{ fileValue.copyrightHolder }}
+    </div>
+    <div style="display: flex; justify-content: space-between" *ngIf="userCanEdit || licenseExists">
+      <div>License {{ fileValue.license.labelEn }}</div>
       <div>Licensed on {{ resource.res.creationDate | humanReadableDate }}</div>
     </div>
-    <div>Authorship {{ legalInfos.authorship }}</div>`,
+    <div *ngIf="userCanEdit || fileValue.authorship.length > 0">
+      Authorship
+      <span *ngFor="let author of fileValue.authorship; let last = last">{{ author }}{{ last ? '' : ',' }}</span>
+    </div>`,
 })
 export class ResourceLegalComponent implements OnInit {
   @Input({ required: true }) resource!: DspResource;
 
-  get legalInfos() {
-    return getFileValue(this.resource);
+  get userCanEdit() {
+    return ResourceUtil.userCanEdit(this.resource.res);
   }
 
+  fileValue!: ReadFileValue;
+  licenseExists = false;
+
   ngOnInit() {
-    console.log(this.resource);
+    this.fileValue = getFileValue(this.resource);
+    this.licenseExists = this.fileValue.license.id !== '';
   }
 }
