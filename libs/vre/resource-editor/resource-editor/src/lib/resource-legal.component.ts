ed151526b6fcc9d7b7d30c8bfd1a33e9ddfadfe7
Julien Schneider <julien.schneider.1991@gmail.com>
2025-04-01 14:01:11 +0200

    styling 4

--- libs/vre/resource-editor/resource-editor/src/lib/resource-legal.component.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/resource-legal.component.ts
@@ -5,33 +5,32 @@ import { DspResource } from '@dasch-swiss/vre/shared/app-common';
 
 @Component({
   selector: 'app-resource-legal',
-  template: ` <div>Legal infos</div>
-    <div
-      class="mat-caption"
-      style="border: 1px solid gray; padding: 8px; margin-top: 8px; position: relative; top: 5px">
-      <div style="display: flex; justify-content: space-between">
-        <div>
-          <div *ngIf="true || fileValue.copyrightHolder !== ''">
-            <span class="label">Copyright holder</span> {{ fileValue.copyrightHolder }} Julien Schneider
-          </div>
-
-          <div *ngIf="true || fileValue.authorship.length > 0">
-            <span class="label">Authorship</span>
-            <span>Julien,</span>
-            <span>Dominique,</span>
-            <span>Irmantas</span>
-            <span *ngFor="let author of fileValue.authorship; let last = last">{{ author }}{{ last ? '' : ', ' }}</span>
-          </div>
+  template: ` <div
+    class="mat-caption"
+    style="border: 1px solid gray; padding: 8px; margin-top: 8px; position: relative; top: 5px">
+    <div style="display: flex; justify-content: space-between">
+      <div>
+        <div *ngIf="true || fileValue.copyrightHolder !== ''">
+          <span class="label">Copyright holder</span> {{ fileValue.copyrightHolder }} Julien Schneider
         </div>
-        <div>
-          <div>Licensed on {{ resource.res.creationDate | humanReadableDate }}</div>
 
-          <div style="display: flex; justify-content: flex-end">
-            <img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-sa.svg" alt="license" />
-          </div>
+        <div *ngIf="true || fileValue.authorship.length > 0">
+          <span class="label">Authorship</span>
+          <span>Julien,</span>
+          <span>Dominique,</span>
+          <span>Irmantas</span>
+          <span *ngFor="let author of fileValue.authorship; let last = last">{{ author }}{{ last ? '' : ', ' }}</span>
         </div>
       </div>
-    </div>`,
+      <div>
+        <div>Licensed on {{ resource.res.creationDate | humanReadableDate }}</div>
+
+        <div style="display: flex; justify-content: flex-end">
+          <img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-sa.svg" alt="license" />
+        </div>
+      </div>
+    </div>
+  </div>`,
   styles: ['.label { display: inline-block; width: 120px}'],
 })
 export class ResourceLegalComponent implements OnInit {
