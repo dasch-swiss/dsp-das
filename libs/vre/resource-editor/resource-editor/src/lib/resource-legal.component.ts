d06f49d2cd4a82ac95ef291cddfc86e88831fcd3
Julien Schneider <julien.schneider.1991@gmail.com>
2025-04-01 13:37:28 +0200

    styling

--- libs/vre/resource-editor/resource-editor/src/lib/resource-legal.component.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/resource-legal.component.ts
@@ -5,17 +5,28 @@ import { DspResource } from '@dasch-swiss/vre/shared/app-common';
 
 @Component({
   selector: 'app-resource-legal',
-  template: ` <div class="mat-caption">
-    <div *ngIf="userCanEdit || fileValue.copyrightHolder !== ''">Copyright holder {{ fileValue.copyrightHolder }}</div>
-    <div style="display: flex; justify-content: space-between" *ngIf="userCanEdit || licenseExists">
-      <div>License {{ fileValue.license.labelEn }}</div>
-      <div>Licensed on {{ resource.res.creationDate | humanReadableDate }}</div>
-    </div>
-    <div *ngIf="userCanEdit || fileValue.authorship.length > 0">
-      Authorship
-      <span *ngFor="let author of fileValue.authorship; let last = last">{{ author }}{{ last ? '' : ',' }}</span>
-    </div>
-  </div>`,
+  template: ` <div>Legal infos</div>
+    <div
+      class="mat-caption"
+      style="border: 1px solid gray; padding: 8px; margin-top: 8px; position: relative; top: 5px">
+      <div *ngIf="userCanEdit || fileValue.copyrightHolder !== ''">
+        Copyright holder {{ fileValue.copyrightHolder }} Julien Schneider
+      </div>
+      <div style="display: flex; justify-content: space-between" *ngIf="userCanEdit || licenseExists">
+        <div>
+          License {{ fileValue.license.labelEn }}
+          <img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-sa.svg" />
+        </div>
+        <div>Licensed on {{ resource.res.creationDate | humanReadableDate }}</div>
+      </div>
+      <div *ngIf="userCanEdit || fileValue.authorship.length > 0">
+        Authorship
+        <span>Julien,</span>
+        <span>Dominique,</span>
+        <span>Irmantas</span>
+        <span *ngFor="let author of fileValue.authorship; let last = last">{{ author }}{{ last ? '' : ', ' }}</span>
+      </div>
+    </div>`,
 })
 export class ResourceLegalComponent implements OnInit {
   @Input({ required: true }) resource!: DspResource;
