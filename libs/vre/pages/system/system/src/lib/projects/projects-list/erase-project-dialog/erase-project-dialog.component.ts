74f72a73642d3b3976ab8e3469cf0b2d6a07315b
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-05-21 12:45:31 +0200

    refactor EraseProjectDialogComponent

--- libs/vre/pages/system/system/src/lib/projects/projects-list/erase-project-dialog/erase-project-dialog.component.ts
+++ libs/vre/pages/system/system/src/lib/projects/projects-list/erase-project-dialog/erase-project-dialog.component.ts
@@ -4,23 +4,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { KnoraApiConnection, ReadProject } from '@dasch-swiss/dsp-js';
 import { ProjectApiService } from '@dasch-swiss/vre/3rd-party-services/api';
 import { DspApiConnectionToken } from '@dasch-swiss/vre/core/config';
+import { AppErrorHandler } from '@dasch-swiss/vre/core/error-handler';
 import { Store } from '@ngxs/store';
 import { of } from 'rxjs';
 import { catchError, finalize, switchMap } from 'rxjs/operators';
 
 export interface IEraseProjectDialogProps {
   project: ReadProject;
-}
-
-/**
- * validation of existing project short code
- *
- * @param {string} shortCode Project short code
- * @returns ValidatorFn
- */
-function projectShortCodeValidator(shortCode: string): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } =>
-    shortCode.toLowerCase() !== control.value.toLowerCase() ? { shortCode: [control.value] } : null;
 }
 
 @Component({
@@ -36,47 +26,61 @@ function projectShortCodeValidator(shortCode: string): ValidatorFn {
 })
 export class EraseProjectDialogComponent {
   eraseForm = new FormGroup({
-    shortCode: new FormControl('', [Validators.required, projectShortCodeValidator(this.data.project.shortcode)]),
+    shortCode: new FormControl('', [Validators.required, this.projectShortCodeValidator(this.data.project.shortcode)]),
     password: new FormControl('', [Validators.required]),
   });
+
   isLoading = false;
 
   constructor(
-    @Inject(DspApiConnectionToken)
-    private _dspApiConnection: KnoraApiConnection,
-    @Inject(MAT_DIALOG_DATA)
-    public data: IEraseProjectDialogProps,
+    @Inject(MAT_DIALOG_DATA) public data: IEraseProjectDialogProps,
     private _dialogRef: MatDialogRef<EraseProjectDialogComponent>,
+    @Inject(DspApiConnectionToken) private _dspApiConnection: KnoraApiConnection,
+    private _errorHandler: AppErrorHandler,
     private _projectApiService: ProjectApiService,
     private _store: Store
   ) {}
 
-  submit() {
-    if (this.eraseForm.invalid) return;
+  submit(): void {
+    if (this.eraseForm.invalid) throw new Error('Form is invalid.');
+
     this.isLoading = true;
 
     const currentUser = this._store.selectSnapshot(state => state.user).user;
-    return this._dspApiConnection.v2.auth
-      .login('username', currentUser.username, this.eraseForm.controls.password.value) // TODO [BE] should be dedicated endpoint
-      .pipe(catchError(() => of(false)))
-      .pipe(
-        switchMap((result: boolean) => {
-          if (!result) {
-            this.eraseForm.controls.password.setErrors({ password: [] });
-            return of(null);
-          }
+    const password = this.eraseForm.controls.password.value;
+    const shortCode = this.eraseForm.controls.shortCode.value;
 
-          return this._projectApiService.erase(this.eraseForm.controls.shortCode.value);
+    this._dspApiConnection.v2.auth
+      .login('username', currentUser.username, password!) // TODO [BE] should be dedicated endpoint
+      .pipe(
+        catchError(() => {
+          this.eraseForm.controls.password.setErrors({ invalidPassword: true });
+          return of(null);
         })
       )
       .pipe(
+        switchMap(loginResult => {
+          if (!loginResult) return of(null);
+
+          return this._projectApiService.erase(shortCode!);
+        }),
         finalize(() => {
           this.isLoading = false;
         })
       )
-      .subscribe(response => {
-        if (!response) return;
-        this._dialogRef.close(response.project);
+      .subscribe({
+        next: response => {
+          if (!response) return;
+          this._dialogRef.close(response.project);
+        },
+        error: err => {
+          this._errorHandler.handleError(`Unexpected error during submit: ${err}`);
+        },
       });
+  }
+
+  private projectShortCodeValidator(shortCode: string): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null =>
+      shortCode.toLowerCase() !== control.value.toLowerCase() ? { shortCode: [control.value] } : null;
   }
 }
