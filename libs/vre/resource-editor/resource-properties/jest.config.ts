2197b091e68a18b19a7bf70ac3c35b8d186f1486
Julien Schneider <julien.schneider.1991@gmail.com>
2025-06-17 12:01:15 +0200

    fix: jest config

--- libs/vre/resource-editor/resource-properties/jest.config.ts
+++ libs/vre/resource-editor/resource-properties/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'vre-shared-resource-properties',
-  preset: '../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/libs/vre-shared-resource-properties',
   transform: {
@@ -18,5 +18,5 @@ export default {
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
-  ]
+  ],
 };
