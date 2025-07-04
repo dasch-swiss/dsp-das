fa94c40977ef969446bcb97ea2405e241148bc87
Julien Schneider <julien.schneider.1991@gmail.com>
2025-07-04 15:10:00 +0200

    Revert "fix: remove template other than basic"
    This reverts commit 0cb71ecf

--- libs/vre/resource-editor/template-switcher/src/lib/template-viewer-switcher.component.ts
+++ libs/vre/resource-editor/template-switcher/src/lib/template-viewer-switcher.component.ts
@@ -22,28 +22,51 @@ import { JsLibPotentialError } from '@dasch-swiss/vre/resource-editor/resource-p
   selector: 'app-template-viewer-switcher',
   template: `
     <ng-template #basicDisplayTpl let-item="item">
-      <span>{{ item }}</span>
+      <span>{{ item.value }}</span>
     </ng-template>
 
-    <ng-template #booleanDisplayTpl let-item="item"></ng-template>
+    <ng-template #booleanDisplayTpl let-item="item">
+      <mat-slide-toggle *ngIf="item.value !== null" [checked]="item.value" [disabled]="true" />
+    </ng-template>
 
-    <ng-template #intervalDisplayTpl let-item="item"></ng-template>
+    <ng-template #intervalDisplayTpl let-item="item">
+      <app-interval-viewer [control]="item" />
+    </ng-template>
 
-    <ng-template #timeDisplayTpl let-item="item"></ng-template>
+    <ng-template #timeDisplayTpl let-item="item">
+      <app-time-viewer [control]="item" />
+    </ng-template>
 
-    <ng-template #listDisplayTpl let-item="item"></ng-template>
+    <ng-template #listDisplayTpl let-item="item">
+      <app-list-viewer [control]="item" [propertyDef]="propertyDefinition" />
+    </ng-template>
 
-    <ng-template #richTextDisplayTpl let-item="item"></ng-template>
+    <ng-template #richTextDisplayTpl let-item="item">
+      <app-rich-text-viewer [control]="item" />
+    </ng-template>
 
-    <ng-template #textHtmlDisplayTpl let-item="item"></ng-template>
+    <ng-template #textHtmlDisplayTpl let-item="item">
+      <div data-cy="text-html-switch" [innerHTML]="item.value | internalLinkReplacer | addTargetBlank" appMathjax></div>
+    </ng-template>
 
-    <ng-template #colorDisplayTpl let-item="item"></ng-template>
+    <ng-template #colorDisplayTpl let-item="item">
+      <div
+        style="width: 100px; border-radius: 4px; height: 15px; margin: 4px 0; border: 1px solid;"
+        data-cy="color-box"
+        [style.background-color]="item.value"></div>
+    </ng-template>
 
-    <ng-template #dateDisplayTpl let-item="item"></ng-template>
+    <ng-template #dateDisplayTpl let-item="item">
+      <app-date-viewer [control]="item" />
+    </ng-template>
 
-    <ng-template #geoNameDisplayTpl let-item="item"></ng-template>
+    <ng-template #geoNameDisplayTpl let-item="item">
+      <app-geoname-viewer [control]="item" />
+    </ng-template>
 
-    <ng-template #linkDisplayTpl let-item="item"></ng-template>
+    <ng-template #linkDisplayTpl let-item="item">
+      <app-link-viewer *ngIf="value" [control]="item" [value]="value" />
+    </ng-template>
 
     <ng-template #defaultDisplayTpl><span style="width: 100%">Nothing to show</span></ng-template>
   `,
