ba2d2fb9f817ff503b266c3c3586e5e282008dea
domsteinbach <dominique.steinbach@unibas.ch>
2025-07-08 09:22:34 +0200

    test: fix

--- libs/vre/pages/data-browser/jest.config.ts
+++ libs/vre/pages/data-browser/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: 'vre-pages-ontology-ontology-classes',
-  preset: '../../../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../../../coverage/libs/vre/pages/ontology/ontology-classes',
+  coverageDirectory: '../../../../../coverage/libs/vre/pages/data-browser',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
