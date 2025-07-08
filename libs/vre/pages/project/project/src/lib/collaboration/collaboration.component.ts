980365c25d5c5fe57777333579053bbab271fa21
domsteinbach <dominique.steinbach@unibas.ch>
2025-07-08 16:29:37 +0200

    refactor

--- libs/vre/pages/project/project/src/lib/collaboration/collaboration.component.ts
+++ libs/vre/pages/project/project/src/lib/collaboration/collaboration.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ReadUser } from '@dasch-swiss/dsp-js';
 import { LoadProjectMembersAction, ProjectsSelectors } from '@dasch-swiss/vre/core/state';
 import { ProjectService } from '@dasch-swiss/vre/shared/app-helper-services';
 import { Store } from '@ngxs/store';
-import { combineLatest, Observable, Subject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { combineLatest, Observable } from 'rxjs';
+import { filter, first, map, tap } from 'rxjs/operators';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,18 +13,28 @@ import { map, takeUntil } from 'rxjs/operators';
   templateUrl: './collaboration.component.html',
   styleUrls: ['./collaboration.component.scss'],
 })
-export class CollaborationComponent implements OnInit, OnDestroy {
+export class CollaborationComponent implements OnInit {
   project$ = this._store.select(ProjectsSelectors.currentProject);
   projectMembers$ = this._store.select(ProjectsSelectors.projectMembers);
   isAdmin$ = this._store.select(ProjectsSelectors.isCurrentProjectAdminOrSysAdmin);
 
-  projectUuid$!: Observable<string>;
-
-  private _destroy = new Subject<void>();
+  projectUuid$ = this.project$.pipe(
+    tap(p => {
+      this._titleService.setTitle(`Project ${p?.shortname} | Collaboration`);
+    }),
+    map(p => {
+      return ProjectService.IriToUuid(p?.id || '');
+    }),
+    filter(uuid => !!uuid),
+    first()
+  );
 
   get activeProjectMembers$(): Observable<ReadUser[]> {
     return combineLatest([this.project$, this.projectMembers$]).pipe(
       map(([currentProject, projectMembers]) => {
+        if (!currentProject || !projectMembers[currentProject.id]) {
+          return [];
+        }
         return projectMembers[currentProject.id]?.value.filter(member => member?.status === true) || [];
       })
     );
@@ -33,6 +43,9 @@ export class CollaborationComponent implements OnInit, OnDestroy {
   get inactiveProjectMembers$(): Observable<ReadUser[]> {
     return combineLatest([this.project$, this.projectMembers$]).pipe(
       map(([currentProject, projectMembers]) => {
+        if (!currentProject || !projectMembers[currentProject.id]) {
+          return [];
+        }
         return projectMembers[currentProject?.id].value.filter(member => member?.status === false) || [];
       })
     );
@@ -44,22 +57,10 @@ export class CollaborationComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.projectUuid$ = this.project$.pipe(
-      map(p => {
-        return ProjectService.IriToUuid(p?.id);
-      })
-    );
-    this.project$.pipe(takeUntil(this._destroy)).subscribe(p => {
-      const projectUuid = ProjectService.IriToUuid(p?.id);
+    this.projectUuid$.subscribe(projectUuid => {
       if (projectUuid) {
         this._store.dispatch(new LoadProjectMembersAction(projectUuid));
       }
-      this._titleService.setTitle(`Project ${p?.shortname} | Collaboration`);
     });
-  }
-
-  ngOnDestroy() {
-    this._destroy.next();
-    this._destroy.complete();
   }
 }
