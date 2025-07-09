ac470ecb26573d40a9dea21fedcd5cd43c6ab1bb
Julien Schneider <julien.schneider.1991@gmail.com>
2025-07-09 13:04:09 +0200

    wip

--- libs/vre/pages/ontology/ontology/src/lib/forms/resource-class-form/create-resource-class-dialog.component.ts
+++ libs/vre/pages/ontology/ontology/src/lib/forms/resource-class-form/create-resource-class-dialog.component.ts
@@ -1,16 +1,14 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { StringLiteralV2 } from '@dasch-swiss/vre/3rd-party-services/open-api';
-import { DefaultClass, OntologyService } from '@dasch-swiss/vre/shared/app-helper-services';
-import { MultiLanguages } from '@dasch-swiss/vre/ui/string-literal';
-import { take } from 'rxjs/operators';
+import { DefaultClass } from '@dasch-swiss/vre/shared/app-helper-services';
 import { OntologyEditService } from '../../services/ontology-edit.service';
-import { ResourceClassForm, ResourceClassFormData, UpdateResourceClassData } from './resource-class-form.type';
+import { ResourceClassForm } from './resource-class-form.type';
 
 @Component({
   selector: 'app-create-resource-class-dialog',
   template: `
-    <app-dialog-header [title]="''" subtitle="Customize resource class" />
+    <app-dialog-header [title]="data.label" subtitle="Create resource class" />
     <div mat-dialog-content>
       <app-resource-class-form [formData]="formData" (afterFormInit)="afterFormInit($event)" />
     </div>
@@ -30,79 +28,39 @@ import { ResourceClassForm, ResourceClassFormData, UpdateResourceClassData } fro
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CreateResourceClassDialogComponent implements OnInit {
+export class CreateResourceClassDialogComponent {
   loading = false;
-  form: ResourceClassForm | undefined;
-  formData!: ResourceClassFormData;
+  form!: ResourceClassForm;
+  formData = {
+    name: '',
+    labels: [],
+    comments: [],
+  };
+
   lastModificationDate!: string;
 
-  isUpdateResourceClassData(data: UpdateResourceClassData | DefaultClass): data is UpdateResourceClassData {
-    return 'id' in data;
-  }
-
   constructor(
-    @Inject(MAT_DIALOG_DATA)
-    public data: UpdateResourceClassData | DefaultClass,
-    public dialogRef: MatDialogRef<CreateResourceClassDialogComponent, ResourceClassFormData | UpdateResourceClassData>,
-    private _ontologyService: OntologyService,
+    @Inject(MAT_DIALOG_DATA) public data: DefaultClass,
+    public dialogRef: MatDialogRef<CreateResourceClassDialogComponent>,
     private _oes: OntologyEditService
   ) {}
 
-  ngOnInit() {
-    this.dialogRef.updateSize('800px', '');
-    if (this.isUpdateResourceClassData(this.data)) {
-      this.formData = {
-        name: this._ontologyService.getNameFromIri(this.data.id),
-        labels: this.data.labels ? (this.data.labels as MultiLanguages) : [],
-        comments: this.data.comments ? (this.data.comments as MultiLanguages) : [],
-      };
-    } else {
-      this.formData = {
-        name: '',
-        labels: [],
-        comments: [],
-      };
-    }
-  }
-
   afterFormInit(form: ResourceClassForm) {
     this.form = form;
-    if (this.isUpdateResourceClassData(this.data)) {
-      this.form.controls.name.disable();
-    }
   }
 
   onSubmit() {
     this.loading = true;
-    const labels = this.form?.controls.labels.touched
-      ? (this.form?.controls.labels.value as StringLiteralV2[])
-      : undefined; // by leaving the labels undefined if not touched, they are not updated unnecessarily by the ontology service
-    const comments = this.form?.controls.comments.touched
-      ? (this.form?.controls.comments.value as StringLiteralV2[])
-      : undefined; // by leaving the comments undefined if not touched, they are not updated unnecessarily by the ontology service
 
-    if (this.isUpdateResourceClassData(this.data)) {
-      const upd = { id: this.data.id, labels, comments } as UpdateResourceClassData;
-      this._oes
-        .updateResourceClass$(upd)
-        .pipe(take(1))
-        .subscribe(res => {
-          this.loading = false;
-          this.dialogRef.close();
-        });
-    } else {
-      const createData = {
-        name: this.form?.controls.name.value,
-        labels,
-        comments,
-      } as unknown as ResourceClassFormData;
-      this._oes
-        .createResourceClass$(createData)
-        .pipe(take(1))
-        .subscribe(res => {
-          this.loading = false;
-          this.dialogRef.close();
-        });
-    }
+    this._oes
+      .createResourceClass$({
+        name: this.form.controls.name.value,
+        labels: this.form.controls.labels.value as StringLiteralV2[],
+        comments: this.form.controls.comments.value as StringLiteralV2[],
+      })
+      .subscribe(() => {
+        this.loading = false;
+        this.dialogRef.close();
+      });
   }
 }
