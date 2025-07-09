ca06dca067a332f42644659a22961ad67fcbd0d8
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-07-09 09:54:30 +0200

    lint

--- libs/vre/pages/ontology/ontology/src/lib/resource-class-info/resource-class-property-info/resource-class-property-info.component.ts
+++ libs/vre/pages/ontology/ontology/src/lib/resource-class-info/resource-class-property-info/resource-class-property-info.component.ts
@@ -23,8 +23,7 @@ import { DspApiConnectionToken } from '@dasch-swiss/vre/core/config';
 import { ListsSelectors, LoadOntologyAction, OntologiesSelectors, PropToDisplay } from '@dasch-swiss/vre/core/state';
 import { DefaultClass, DefaultProperty, OntologyService } from '@dasch-swiss/vre/shared/app-helper-services';
 import { Store } from '@ngxs/store';
-import { Subject } from 'rxjs';
-import { take, takeUntil, tap } from 'rxjs';
+import { Subject, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-resource-class-property-info',
