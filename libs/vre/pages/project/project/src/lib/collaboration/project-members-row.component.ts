44bdb5ce6b35c473c1aa77a709373bd2a5a42dcf
Julien Schneider <julien.schneider.1991@gmail.com>
2025-07-10 16:03:06 +0200

    wip

--- libs/vre/pages/project/project/src/lib/collaboration/project-members-row.component.ts
+++ libs/vre/pages/project/project/src/lib/collaboration/project-members-row.component.ts
@@ -1,32 +1,48 @@
-import { Component, Input } from '@angular/core';
-import { ReadUser } from '@dasch-swiss/dsp-js';
+import { Component, Input, OnInit } from '@angular/core';
+import { ReadProject, ReadUser } from '@dasch-swiss/dsp-js';
 import { PermissionsData } from '@dasch-swiss/dsp-js/src/models/admin/permissions-data';
+import { ProjectsSelectors } from '@dasch-swiss/vre/core/state';
 import { ProjectService } from '@dasch-swiss/vre/shared/app-helper-services';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-project-members-row',
   template: `
-    <div style="display: flex">
-      <app-user-description [user]="user" />
+    <div style="display: flex; align-items: center; gap: 8px;">
+      <app-user-description [user]="user" style="flex: 1" />
 
       <mat-chip-listbox>
         <mat-chip class="admin-chip" *ngIf="isProjectAdmin(user.permissions)">Admin</mat-chip>
       </mat-chip-listbox>
 
       <app-select-group
-        *ngIf="(isProjectOrSystemAdmin$ | async) === true"
-        [projectCode]="(project$ | async)?.shortcode"
-        [projectid]="(project$ | async)?.id"
-        [permissions]="user.permissions.groupsPerProject[(project$ | async)?.id]"
-        [disabled]="!status"
+        *ngIf="project"
+        [projectCode]="project.shortcode"
+        [projectid]="project.id"
+        [permissions]="user.permissions.groupsPerProject[project.id]"
+        [disabled]="!user.status"
         (groupChange)="updateGroupsMembership(user.id, $event)" />
 
-      <app-project-members-row-menu [user]="user" />
+      <app-project-members-row-menu [user]="user" *ngIf="project" [project]="project" />
     </div>
   `,
 })
-export class ProjectMembersRowComponent {
+export class ProjectMembersRowComponent implements OnInit {
   @Input({ required: true }) user!: ReadUser;
+
+  project?: ReadProject;
+
+  constructor(private _store: Store) {}
+
+  ngOnInit() {
+    this._store.select(ProjectsSelectors.currentProject).subscribe(project => {
+      this.project = project;
+    });
+  }
+
+  updateGroupsMembership(userId: string, groups: string[]): void {
+    // TODO
+  }
 
   isProjectAdmin(permissions: PermissionsData): boolean {
     if (!permissions.groupsPerProject) {
