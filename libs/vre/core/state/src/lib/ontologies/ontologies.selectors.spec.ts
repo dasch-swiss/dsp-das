d135f26f1f8c44f29f0ac70326657f21348852a8
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-07-11 11:53:10 +0200

    fmt

--- libs/vre/core/state/src/lib/ontologies/ontologies.selectors.spec.ts
+++ libs/vre/core/state/src/lib/ontologies/ontologies.selectors.spec.ts
@@ -1,16 +1,9 @@
-/*
- * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
- *  SPDX-License-Identifier: Apache-2.0
- */
-
 import { Params } from '@angular/router';
 import {
   ClassDefinition,
   OntologyMetadata,
   ReadOntology,
   ReadProject,
-  ResourceClassDefinition,
-  ResourceClassDefinitionWithAllLanguages,
   ReadResource,
   StringLiteral,
 } from '@dasch-swiss/dsp-js';
--- libs/vre/core/state/src/lib/projects/projects.selectors.spec.ts
+++ libs/vre/core/state/src/lib/projects/projects.selectors.spec.ts
@@ -1,8 +1,3 @@
-/*
- * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
- *  SPDX-License-Identifier: Apache-2.0
- */
-
 import { Params } from '@angular/router';
 import {
   Constants,
