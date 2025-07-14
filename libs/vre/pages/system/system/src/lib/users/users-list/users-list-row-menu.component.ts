aa62a7656a0817c88ddd999bf33d5f5ad2de0bd4
Julien Schneider <julien.schneider.1991@gmail.com>
2025-07-14 13:49:39 +0200

    fix

--- libs/vre/pages/system/system/src/lib/users/users-list/users-list-row-menu.component.ts
+++ libs/vre/pages/system/system/src/lib/users/users-list/users-list-row-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Constants, ReadUser } from '@dasch-swiss/dsp-js';
 import { PermissionsData } from '@dasch-swiss/dsp-js/src/models/admin/permissions-data';
@@ -7,9 +7,8 @@ import { DspDialogConfig } from '@dasch-swiss/vre/core/config';
 import { SetUserAction, UserSelectors } from '@dasch-swiss/vre/core/state';
 import { EditUserDialogComponent } from '@dasch-swiss/vre/pages/user-settings/user';
 import { DialogService } from '@dasch-swiss/vre/ui/ui';
-import { Actions, Store } from '@ngxs/store';
-import { Subject } from 'rxjs';
-import { take, takeUntil } from 'rxjs/operators';
+import { Store } from '@ngxs/store';
+import { take } from 'rxjs/operators';
 import { EditPasswordDialogComponent } from '../edit-password-dialog.component';
 import { ManageProjectMembershipDialogComponent } from '../manage-project-membership-dialog.component';
 
@@ -41,27 +40,19 @@ import { ManageProjectMembershipDialogComponent } from '../manage-project-member
     </mat-menu>
   `,
 })
-export class UsersListRowMenuComponent implements OnDestroy {
+export class UsersListRowMenuComponent {
   @Input({ required: true }) user!: ReadUser;
   @Output() refreshParent = new EventEmitter<void>();
-
-  private readonly _destroy$ = new Subject<void>();
 
   isSysAdmin$ = this._store.select(UserSelectors.isSysAdmin);
   username$ = this._store.select(UserSelectors.username);
 
   constructor(
     private _matDialog: MatDialog,
-    private _actions$: Actions,
     private _store: Store,
     private _dialog: DialogService,
     private readonly _userApiService: UserApiService
   ) {}
-
-  ngOnDestroy(): void {
-    this._destroy$.next();
-    this._destroy$.complete();
-  }
 
   isSystemAdmin(permissions: PermissionsData): boolean {
     if (!permissions.groupsPerProject) {
@@ -91,21 +82,15 @@ export class UsersListRowMenuComponent implements OnDestroy {
   }
 
   askToActivateUser(username: string, id: string) {
-    this._dialog
-      .afterConfirmation(`Do you want to reactivate user ${username}?`)
-      .pipe(takeUntil(this._destroy$))
-      .subscribe(() => {
-        this.activateUser(id);
-      });
+    this._dialog.afterConfirmation(`Do you want to reactivate user ${username}?`).subscribe(() => {
+      this.activateUser(id);
+    });
   }
 
   askToDeactivateUser(username: string, id: string) {
-    this._dialog
-      .afterConfirmation(`Do you want to suspend user ${username}?`)
-      .pipe(takeUntil(this._destroy$))
-      .subscribe(() => {
-        this.deactivateUser(id);
-      });
+    this._dialog.afterConfirmation(`Do you want to suspend user ${username}?`).subscribe(() => {
+      this.deactivateUser(id);
+    });
   }
 
   editUser(user: ReadUser) {
