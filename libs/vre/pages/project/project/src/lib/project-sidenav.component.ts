be59c3b93b500a9a47c7ef714fde04f865d150fd
Julien Schneider <julien.schneider.1991@gmail.com>
2025-07-29 14:34:30 +0200

    fix

--- libs/vre/pages/project/project/src/lib/project-sidenav.component.ts
+++ libs/vre/pages/project/project/src/lib/project-sidenav.component.ts
@@ -4,20 +4,23 @@ import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-project-sidenav',
-  template: ` <div class="main-content">
-    <!-- Project label -->
+  template: `
     <div *ngIf="currentProject$ | async as currentProject" class="project-title">
       <span [matTooltip]="currentProject.longname || ''" [matTooltipShowDelay]="500" matTooltipPosition="right">
         {{ currentProject.longname }}
       </span>
     </div>
-    <mat-list class="main-list">
-      <mat-divider />
+
+    <mat-divider />
+
+    <mat-list>
       <app-projects-sidenav-links />
+
       <mat-divider />
+
       <app-projects-sidenav-ontologies />
     </mat-list>
-  </div>`,
+  `,
 })
 export class ProjectSidenavComponent {
   currentProject$ = this._store.select(ProjectsSelectors.currentProject);
