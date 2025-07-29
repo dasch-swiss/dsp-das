69ecfd927831d0b903bcf06f392fd8cf48b70fb9
Julien Schneider <julien.schneider.1991@gmail.com>
2025-07-29 11:06:59 +0200

    fix

--- libs/vre/pages/data-browser/src/lib/list-view/list-view-normal.component.ts
+++ libs/vre/pages/data-browser/src/lib/list-view/list-view-normal.component.ts
@@ -4,11 +4,14 @@ import { MultipleViewerService } from '../multiple-viewer.service';
 
 @Component({
   selector: 'app-list-view-normal',
-  template: ` <app-resource-list-selection *ngIf="multipleViewerService.selectMode" [resources]="resources" />
+  template: ` <app-pager (pageIndexChanged)="doSearch($event)" [numberOfAllResults]="200" />
+    <app-resource-list-selection *ngIf="multipleViewerService.selectMode" [resources]="resources" />
     <app-resource-list-2 [resources]="resources" />`,
 })
 export class ListViewNormalComponent {
   @Input({ required: true }) resources!: ReadResource[];
 
   constructor(public multipleViewerService: MultipleViewerService) {}
+
+  doSearch(index: number) {}
 }
--- libs/vre/pages/data-browser/src/lib/multiple-viewer-2.component.ts
+++ libs/vre/pages/data-browser/src/lib/multiple-viewer-2.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ReadResource } from '@dasch-swiss/dsp-js';
 import { MultipleViewerService } from './multiple-viewer.service';
 
 @Component({
@@ -7,7 +6,11 @@ import { MultipleViewerService } from './multiple-viewer.service';
   template: `
     <ng-container *ngIf="multipleViewerService.selectedResourceIds$ | async as selectedResources">
       <app-comparison *ngIf="selectedResources.length <= MAX_RESOURCES" [resourceIds]="selectedResources" />
-      <div *ngIf="selectedResources.length > MAX_RESOURCES">Too many resources</div>
+      <div
+        *ngIf="selectedResources.length > MAX_RESOURCES"
+        style="height: 100%; display: flex; align-items: center; justify-content: center">
+        Too many resources are selected to be displayed.
+      </div>
     </ng-container>
   `,
 })
@@ -15,8 +18,4 @@ export class MultipleViewer2Component {
   readonly MAX_RESOURCES = 3;
 
   constructor(public multipleViewerService: MultipleViewerService) {}
-
-  updateResourceCount(resource: ReadResource) {
-    // TODO
-  }
 }
