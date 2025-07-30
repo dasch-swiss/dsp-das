8f576781be83409461cd9b4d6eead9e5619d19a4
Julien Schneider <julien.schneider.1991@gmail.com>
2025-07-30 12:02:36 +0200

    wip

---
+++ apps/dsp-app/src/app/cookie-banner.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouteConstants } from '@dasch-swiss/vre/core/config';
+
+@Component({
+  selector: 'app-cookie-banner',
+  template: ` <div class="cookie-banner" *ngIf="showCookieBanner">
+    <p class="note">
+      This web-application uses cookies to provide you with a greater user experience. By using the application you
+      accept our
+      <span class="link" (click)="goToCookiePolicy()">use of cookies</span>.
+    </p>
+    <div class="action">
+      <button mat-flat-button color="primary" (click)="closeCookieBanner()" data-cy="accept-cookies">ACCEPT</button>
+    </div>
+  </div>`,
+  styles: [``],
+})
+export class CookieBannerComponent implements OnInit {
+  showCookieBanner = true;
+
+  constructor(private _router: Router) {}
+
+  ngOnInit() {
+    if (localStorage.getItem('cookieBanner') === null) {
+      localStorage.setItem('cookieBanner', JSON.stringify(this.showCookieBanner));
+    } else {
+      this.showCookieBanner = JSON.parse(localStorage.getItem('cookieBanner'));
+    }
+  }
+
+  goToCookiePolicy() {
+    this._router.navigate([RouteConstants.cookiePolicy]);
+  }
+
+  closeCookieBanner() {
+    this.showCookieBanner = !this.showCookieBanner;
+    localStorage.setItem('cookieBanner', JSON.stringify(this.showCookieBanner));
+  }
+}
