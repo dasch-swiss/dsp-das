4b79e715c14014720ab176377a37f8e5471e2632
Julien Schneider <julien.schneider.1991@gmail.com>
2025-07-30 14:21:36 +0200

    fix: add cdkTextAutoresize

--- libs/vre/resource-editor/resource-properties/src/lib/property-value-basic-comment.component.ts
+++ libs/vre/resource-editor/resource-properties/src/lib/property-value-basic-comment.component.ts
@@ -6,7 +6,7 @@ import { FormControl } from '@angular/forms';
   template: ` <mat-form-field style="flex: 1; width: 100%; margin: 10px 0" subscriptSizing="dynamic">
     <mat-label>{{ 'resourceEditor.resourceProperties.comment' | translate }}</mat-label>
     <mat-icon matPrefix style="color: #808080" *ngIf="control.disabled">lock</mat-icon>
-    <textarea matInput rows="1" data-cy="comment-textarea" [formControl]="control"></textarea>
+    <textarea cdkTextareaAutosize matInput data-cy="comment-textarea" [formControl]="control"></textarea>
   </mat-form-field>`,
 })
 export class PropertyValueBasicCommentComponent {
