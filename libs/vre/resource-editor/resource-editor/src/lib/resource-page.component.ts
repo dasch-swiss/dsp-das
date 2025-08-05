95391baa80bf0a4a572582215dab6f4cfba9f5af
Julien Schneider <julien.schneider.1991@gmail.com>
2025-08-05 09:28:11 +0200

    fix

--- libs/vre/resource-editor/resource-editor/src/lib/resource-page.component.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/resource-page.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ReadResource } from '@dasch-swiss/dsp-js';
 import { AppConfigService, RouteConstants } from '@dasch-swiss/vre/core/config';
-import { LoadResourceClassItemsCountAction, ProjectsSelectors } from '@dasch-swiss/vre/core/state';
+import { ProjectsSelectors } from '@dasch-swiss/vre/core/state';
 import { Store } from '@ngxs/store';
 import { filter, map } from 'rxjs';
 
@@ -26,7 +25,7 @@ export class ResourcePageComponent {
     map(project => `${this._acs.dspAppConfig.iriBase}/${project.shortcode}/${this.instanceId}`)
   );
 
-  updateResourceCount(resource: ReadResource) {
-    this._store.dispatch(new LoadResourceClassItemsCountAction(resource));
+  updateResourceCount(event: any): void {
+    // TODO
   }
 }
