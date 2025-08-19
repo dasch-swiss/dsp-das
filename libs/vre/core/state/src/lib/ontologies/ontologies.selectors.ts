080bb98e7216ab3d7de469df4f7144fbe23ae099
Julien Schneider <julien.schneider.1991@gmail.com>
2025-08-19 16:21:11 +0200

    fix

--- libs/vre/core/state/src/lib/ontologies/ontologies.selectors.ts
+++ libs/vre/core/state/src/lib/ontologies/ontologies.selectors.ts
@@ -1,21 +1,10 @@
-import { OntologyMetadata, ReadOntology, ReadProject } from '@dasch-swiss/dsp-js';
+import { ReadOntology, ReadProject } from '@dasch-swiss/dsp-js';
 import { Selector } from '@ngxs/store';
 import { ProjectsSelectors } from '../projects/projects.selectors';
 import { OntologiesState } from './ontologies.state';
 import { OntologiesStateModel } from './ontologies.state-model';
 
 export class OntologiesSelectors {
-  @Selector([OntologiesState, ProjectsSelectors.currentProject])
-  static currentProjectOntologyMetadata(
-    state: OntologiesStateModel,
-    project: ReadProject | undefined
-  ): OntologyMetadata[] {
-    if (!project || !state.projectOntologies[project.id]) {
-      return [];
-    }
-    return state.projectOntologies[project.id].ontologiesMetadata;
-  }
-
   @Selector([OntologiesState, ProjectsSelectors.currentProject])
   static currentProjectOntologies(state: OntologiesStateModel, project: ReadProject | undefined): ReadOntology[] {
     if (!project || !state.projectOntologies[project.id]) {
