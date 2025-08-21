4da48b1662edf334641fd81301c1de26417ba66c
Julien Schneider <julien.schneider.1991@gmail.com>
2025-08-21 11:39:32 +0200

    fix

--- libs/vre/core/state/src/index.ts
+++ libs/vre/core/state/src/index.ts
@@ -1,16 +1,13 @@
 export * from './lib/lists/lists.selectors'; // THIS WON'T BE EXPOSED, INSTEAD THE FACADE SERVICE WILL BE USED
-export * from './lib/projects/projects.selectors';
 export * from './lib/user/user.selectors';
 
 export * from './lib/lists/lists.actions';
 export * from './lib/user/user.actions';
 
 export * from './lib/lists/lists.state-model';
-export * from './lib/projects/projects.state-model';
 export * from './lib/user/user.state-model';
 
 export * from './lib/lists/lists.state';
-export * from './lib/projects/projects.state';
 export * from './lib/user/user.state';
 
 export * from './lib/lists/lists-facade.service';
