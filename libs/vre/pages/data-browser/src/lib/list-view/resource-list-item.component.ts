82dc6269471cd14bed3ca8f27a759058dd6e7a70
Julien Schneider <julien.schneider.1991@gmail.com>
2025-08-25 16:13:50 +0200

    wip

--- libs/vre/pages/data-browser/src/lib/list-view/resource-list-item.component.ts
+++ libs/vre/pages/data-browser/src/lib/list-view/resource-list-item.component.ts
@@ -8,6 +8,7 @@ import { MultipleViewerService } from '../comparison/multiple-viewer.service';
   selector: 'app-resource-list-item',
   template: `
     <mat-list-item
+      [lines]="3"
       [ngStyle]="{
         'background-color': (isHighlighted$ | async) ? '#D6E0E8' : 'inherit',
       }"
@@ -16,9 +17,14 @@ import { MultipleViewerService } from '../comparison/multiple-viewer.service';
       (mouseleave)="showCheckbox = false"
       (click)="multipleViewerService.selectOneResource(resource)">
       <div style="display: flex">
-        <div style="flex: 1; overflow: hidden; text-overflow: ellipsis; white-space: nowrap">
-          <div class="mat-caption">{{ resource.resourceClassLabel }}</div>
-          <div>{{ resource.label }}</div>
+        <div style="flex: 1; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
+          <div class="mat-caption" matLine>{{ resource.resourceClassLabel }}</div>
+          <div matLine>
+            {{ resource.label }}
+          </div>
+          <div matLine *ngIf="hasSearchResult" class="shorten">
+            Search result LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM LOREM
+          </div>
         </div>
 
         <mat-checkbox
@@ -34,7 +40,16 @@ import { MultipleViewerService } from '../comparison/multiple-viewer.service';
     `
       mat-list-item {
         border-bottom: 1px solid #ebebeb;
-        padding: 32px 16px;
+      }
+      :host ::ng-deep {
+        .mat-line {
+          overflow: hidden;
+        }
+      }
+
+      .shorten {
+        white-space: nowrap;
+        text-overflow: ellipsis;
       }
     `,
   ],
@@ -44,6 +59,7 @@ export class ResourceListItemComponent {
 
   showCheckbox = false;
 
+  hasSearchResult = true;
   isHighlighted$ = this.multipleViewerService.selectedResources$.pipe(
     map(resources => {
       if (this.multipleViewerService.selectMode) {
