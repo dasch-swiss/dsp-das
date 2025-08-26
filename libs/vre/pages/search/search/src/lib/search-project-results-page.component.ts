59dc55bab232ec7dac417cb2506b5d6701e1e523
Julien Schneider <julien.schneider.1991@gmail.com>
2025-08-26 18:02:53 +0200

    fix

--- libs/vre/pages/search/search/src/lib/search-project-results-page.component.ts
+++ libs/vre/pages/search/search/src/lib/search-project-results-page.component.ts
@@ -1,49 +1,53 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IFulltextSearchParams, KnoraApiConnection, ReadResource } from '@dasch-swiss/dsp-js';
-import { DspApiConnectionToken } from '@dasch-swiss/vre/core/config';
+import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
+import { DspApiConnectionToken, RouteConstants } from '@dasch-swiss/vre/core/config';
 import { ResourceResultService } from '@dasch-swiss/vre/pages/data-browser';
+import { combineLatest, map, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search-project-results-page',
-  template: ` <app-multiple-viewer-gateway *ngIf="resources" [resources]="resources" [searchKeyword]="search.query" />`,
+  template: ` <app-multiple-viewer-gateway
+    *ngIf="resources$ | async as resources"
+    [resources]="resources"
+    [searchKeyword]="query" />`,
   providers: [ResourceResultService],
 })
-export class SearchProjectResultsPageComponent implements OnInit {
-  resources: ReadResource[] = [];
-  search!: {
-    query: string;
-    filter?: IFulltextSearchParams;
-    projectUUid?: string;
-  };
+export class SearchProjectResultsPageComponent {
+  query?: string;
+
+  readonly resources$ = this._route.params.pipe(
+    map(params => ({
+      query: params[RouteConstants.qParameter],
+      project: params['project'],
+    })),
+    tap(v => {
+      this.query = v.query;
+    }),
+    switchMap(params =>
+      combineLatest([
+        this._resourceResultService.pageIndex$.pipe(
+          switchMap(pageNumber =>
+            this._dspApiConnection.v2.search.doFulltextSearch(params.query, pageNumber, {
+              limitToProject: decodeURIComponent(params.project),
+            })
+          )
+        ),
+        this._dspApiConnection.v2.search.doFulltextSearchCountQuery(params.query, 0, {
+          limitToProject: decodeURIComponent(params.project),
+        }),
+      ])
+    ),
+    map(([resourceResponse, countResponse]) => {
+      this._resourceResultService.numberOfResults = countResponse.numberOfResults;
+      return resourceResponse.resources;
+    })
+  );
 
   constructor(
     private _route: ActivatedRoute,
     @Inject(DspApiConnectionToken)
-    private _dspApiConnection: KnoraApiConnection
+    private _dspApiConnection: KnoraApiConnection,
+    private _resourceResultService: ResourceResultService
   ) {}
-
-  ngOnInit() {
-    this._route.params.subscribe(params => {
-      console.log('asd', params);
-      this.search = {
-        query: params['q'],
-        filter: { limitToProject: decodeURIComponent(params['project']) },
-      };
-      this.test();
-    });
-  }
-
-  test() {
-    const index = 0;
-    this._dspApiConnection.v2.search
-      .doFulltextSearchCountQuery(this.search.query, index, this.search.filter)
-      .subscribe();
-
-    this._dspApiConnection.v2.search
-      .doFulltextSearch(this.search.query, index, this.search.filter)
-      .subscribe(response => {
-        this.resources = response.resources;
-      });
-  }
 }
