ca9144748c5a30548e1b0dca111250326634ff34
Julien Schneider <julien.schneider.1991@gmail.com>
2025-08-26 17:42:21 +0200

    wip

--- libs/vre/pages/search/search/src/lib/search-project-results-page.component.ts
+++ libs/vre/pages/search/search/src/lib/search-project-results-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, Inject, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IFulltextSearchParams, KnoraApiConnection } from '@dasch-swiss/dsp-js';
 import { DspApiConnectionToken } from '@dasch-swiss/vre/core/config';
-import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
 
 @Component({
   selector: 'app-search-project-results-page',
@@ -10,18 +10,22 @@ import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
 export class SearchProjectResultsPageComponent implements OnInit {
   search!: {
     query: string;
-    filter?: string;
+    filter?: IFulltextSearchParams;
+    projectUUid?: string;
   };
-  constructor(private _route: ActivatedRoute,
-              @Inject(DspApiConnectionToken)
-              private _dspApiConnection: KnoraApiConnection
-              ) {}
+
+  constructor(
+    private _route: ActivatedRoute,
+    @Inject(DspApiConnectionToken)
+    private _dspApiConnection: KnoraApiConnection
+  ) {}
 
   ngOnInit() {
     this._route.params.subscribe(params => {
       console.log('asd', params);
       this.search = {
-        query: params['q'];
+        query: params['q'],
+        filter: { limitToProject: params['project'] },
       };
       this.test();
     });
@@ -29,12 +33,10 @@ export class SearchProjectResultsPageComponent implements OnInit {
 
   test() {
     const index = 0;
-      this._dspApiConnection.v2.search
-        .doFulltextSearchCountQuery(this.search.query, index, this.search.filter)
-        .subscribe();
-
     this._dspApiConnection.v2.search
-      .doFulltextSearch(this.search.query, index, this.search.filter)
+      .doFulltextSearchCountQuery(this.search.query, index, this.search.filter)
       .subscribe();
+
+    this._dspApiConnection.v2.search.doFulltextSearch(this.search.query, index, this.search.filter).subscribe();
   }
 }
