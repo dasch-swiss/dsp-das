7bdcbfa23217d6b209ceee30a92b77dc667ec368
Julien Schneider <julien.schneider.1991@gmail.com>
2025-08-26 17:48:46 +0200

    wip: works

--- libs/vre/pages/search/search/src/lib/search-project-results-page.component.ts
+++ libs/vre/pages/search/search/src/lib/search-project-results-page.component.ts
@@ -1,13 +1,16 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IFulltextSearchParams, KnoraApiConnection } from '@dasch-swiss/dsp-js';
+import { IFulltextSearchParams, KnoraApiConnection, ReadResource } from '@dasch-swiss/dsp-js';
 import { DspApiConnectionToken } from '@dasch-swiss/vre/core/config';
+import { ResourceResultService } from '@dasch-swiss/vre/pages/data-browser';
 
 @Component({
   selector: 'app-search-project-results-page',
-  template: `GOOD`,
+  template: ` <app-multiple-viewer-gateway *ngIf="resources" [resources]="resources" [searchKeyword]="search.query" />`,
+  providers: [ResourceResultService],
 })
 export class SearchProjectResultsPageComponent implements OnInit {
+  resources: ReadResource[] = [];
   search!: {
     query: string;
     filter?: IFulltextSearchParams;
@@ -25,7 +28,7 @@ export class SearchProjectResultsPageComponent implements OnInit {
       console.log('asd', params);
       this.search = {
         query: params['q'],
-        filter: { limitToProject: params['project'] },
+        filter: { limitToProject: decodeURIComponent(params['project']) },
       };
       this.test();
     });
@@ -37,6 +40,10 @@ export class SearchProjectResultsPageComponent implements OnInit {
       .doFulltextSearchCountQuery(this.search.query, index, this.search.filter)
       .subscribe();
 
-    this._dspApiConnection.v2.search.doFulltextSearch(this.search.query, index, this.search.filter).subscribe();
+    this._dspApiConnection.v2.search
+      .doFulltextSearch(this.search.query, index, this.search.filter)
+      .subscribe(response => {
+        this.resources = response.resources;
+      });
   }
 }
