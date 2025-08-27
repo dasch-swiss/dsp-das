c9842cbd8e16904d964f7ee8df50cfe9ceef3b7b
domsteinbach <dominique.steinbach@unibas.ch>
2025-08-27 08:58:56 +0200

    refactor: remove unused

--- libs/vre/pages/search/search/src/index.ts
+++ libs/vre/pages/search/search/src/index.ts
@@ -3,5 +3,4 @@ export * from './lib/advanced-search/advanced-search-container.component';
 export * from './lib/expert-search/expert-search.component';
 export * from './lib/fulltext-search/fulltext-search.component';
 export * from './lib/search-panel/search-panel.component';
-export * from './lib/services/search-params.service';
 export * from './lib/results.component';
--- libs/vre/pages/search/search/src/lib/expert-search/expert-search.component.ts
+++ libs/vre/pages/search/search/src/lib/expert-search/expert-search.component.ts
@@ -19,13 +19,7 @@ import {
 } from '@angular/forms';
 import { SearchParams } from '@dasch-swiss/vre/shared/app-common-to-move';
 import { OntologyService } from '@dasch-swiss/vre/shared/app-helper-services';
-import { GravsearchSearchParams, SearchParamsService } from '../services/search-params.service';
 
-/**
- * validator checking that the query does not contain a certain term, here OFFSET
- *
- * @param {RegExp} termRe
- */
 export function forbiddenTermValidator(termRe: RegExp): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     const forbidden = termRe.test(control.value);
@@ -40,11 +34,6 @@ export function forbiddenTermValidator(termRe: RegExp): ValidatorFn {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExpertSearchComponent implements OnInit, AfterViewInit {
-  /**
-   * the data event emitter of type SearchParams
-   *
-   * @param  search
-   */
   @Output() search = new EventEmitter<SearchParams>();
 
   @ViewChild('textArea') textAreaElement: ElementRef;
@@ -54,7 +43,7 @@ export class ExpertSearchComponent implements OnInit, AfterViewInit {
 
   iriBaseUrl = this._os.getIriBaseUrl();
 
-  defaultGravsearchQuery = `PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
+  readonly defaultGravsearchQuery = `PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
 PREFIX webern: <${this.iriBaseUrl}/ontology/0806/webern-onto/v2#>
 
 CONSTRUCT {
@@ -70,14 +59,12 @@ CONSTRUCT {
 
   constructor(
     private _os: OntologyService,
-    private _searchParamsService: SearchParamsService,
     private _fb: UntypedFormBuilder,
     private _cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
     this.queryFormControl = new UntypedFormControl('');
-
     this.expertSearchForm = this._fb.group({
       gravsearchquery: ['', [Validators.required, forbiddenTermValidator(/OFFSET/i)]],
     });
@@ -85,59 +72,21 @@ CONSTRUCT {
 
   ngAfterViewInit() {
     if (this.textAreaElement?.nativeElement) {
-      // focus the text area
       this.textAreaElement.nativeElement.focus();
       this._cdr.detectChanges();
     }
   }
 
-  /**
-   * reset the form to the initial state.
-   */
   resetForm() {
     this.expertSearchForm.reset({});
-    // focus the text area
     this.textAreaElement.nativeElement.focus();
   }
 
-  /**
-   * send the gravsearch query to the result view by emitting the gravsearch as an output.
-   */
   submitQuery() {
-    const gravsearch = this._generateGravsearch(0);
-
-    if (gravsearch) {
-      this.search.emit({
-        query: gravsearch,
-        mode: 'gravsearch',
-      });
-    }
-  }
-
-  /**
-   * generate the whole gravsearch query matching the query given by the form.
-   */
-  private _generateGravsearch(offset = 0): string {
-    const query = this.expertSearchForm.controls['gravsearchquery'].value;
-
-    // offset component of the Gravsearch query
-    const offsetTemplate = `
-         OFFSET ${offset}
-         `;
-
-    // function that generates the same Gravsearch query with the given offset
-    const generateGravsearchWithCustomOffset = (localOffset: number): string => {
-      const offsetCustomTemplate = `
-             OFFSET ${localOffset}
-             `;
-
-      return query + offsetCustomTemplate;
-    };
-
-    if (offset === 0) {
-      // store the function so another Gravsearch query can be created with an increased offset
-      this._searchParamsService.changeSearchParamsMsg(new GravsearchSearchParams(generateGravsearchWithCustomOffset));
-    }
-    return query + offsetTemplate;
+    const gravsearch = `${this.expertSearchForm.controls['gravsearchquery'].value}\nOFFSET 0\n`;
+    this.search.emit({
+      query: gravsearch,
+      mode: 'gravsearch',
+    });
   }
 }
