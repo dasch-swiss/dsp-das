87b6cd1659fb74bc634a0b7acacf6afbeed86f1c
Julien Schneider <julien.schneider.1991@gmail.com>
2025-08-27 16:13:38 +0200

    fix

--- libs/vre/pages/data-browser/src/lib/comparison/multiple-viewer.component.ts
+++ libs/vre/pages/data-browser/src/lib/comparison/multiple-viewer.component.ts
@@ -7,17 +7,25 @@ import { MultipleViewerService } from './multiple-viewer.service';
   template: `
     <ng-container *ngIf="selectedResourceIds$ | async as selectedResourceIds">
       <app-comparison *ngIf="selectedResourceIds.length <= MAX_RESOURCES" [resourceIds]="selectedResourceIds" />
-      <div style="height: 100%; display: flex; align-items: center; justify-content: center">
-        <ng-container *ngIf="selectedResourceIds.length > MAX_RESOURCES">
-          Too many resources are selected to be displayed.
-        </ng-container>
+      <ng-container *ngIf="selectedResourceIds.length > MAX_RESOURCES">
+        <div class="centered">Too many resources are selected to be displayed.</div>
+      </ng-container>
 
-        <ng-container *ngIf="selectedResourceIds.length === 0">
-          Select a resource on the left panel to display.
-        </ng-container>
-      </div>
+      <ng-container *ngIf="selectedResourceIds.length === 0">
+        <div class="centered">Select a resource on the left panel to display.</div>
+      </ng-container>
     </ng-container>
   `,
+  styles: [
+    `
+      .centered {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    `,
+  ],
 })
 export class MultipleViewerComponent {
   readonly MAX_RESOURCES = 6;
