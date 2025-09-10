d4d0c9c32dde3036b0e4465140c140a18946391d
Julien Schneider <julien.schneider.1991@gmail.com>
2025-09-10 14:15:21 +0200

    wip

--- libs/vre/shared/app-common-to-move/src/lib/header/header-tabs.component.ts
+++ libs/vre/shared/app-common-to-move/src/lib/header/header-tabs.component.ts
@@ -3,51 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header-tabs',
   template: `
-    <nav mat-tab-nav-bar [tabPanel]="tabPanel">
-      <a mat-tab-link [active]="true">Test </a>
+    <nav
+      mat-tab-nav-bar
+      [tabPanel]="tabPanel"
+      mat-stretch-tabs="false"
+      mat-align-tabs="start"
+      animationDuration="0"
+      style="padding-left: 16px">
+      <a mat-tab-link [routerLink]="['description']" routerLinkActive="active-link">
+        <mat-icon class="tab-icon">description</mat-icon>
+        Description
+      </a>
+      <a mat-tab-link [routerLink]="['data-models']" routerLinkActive="active-link">
+        <mat-icon class="tab-icon">bubble_chart</mat-icon>
+        Data models
+      </a>
+      <a mat-tab-link [routerLink]="['data']" routerLinkActive="active-link">
+        <mat-icon class="tab-icon">list</mat-icon>
+        Data
+      </a>
+      <a mat-tab-link [routerLink]="['advanced-search']" routerLinkActive="active-link">
+        <mat-icon class="tab-icon">search</mat-icon>
+        Search
+      </a>
+      <a mat-tab-link [routerLink]="['settings']" routerLinkActive="active-link">
+        <mat-icon class="tab-icon">settings</mat-icon>
+        Settings
+      </a>
     </nav>
     <mat-tab-nav-panel #tabPanel></mat-tab-nav-panel>
-
-    <mat-tab-group mat-stretch-tabs="false" mat-align-tabs="start" animationDuration="0">
-      <mat-tab>
-        <ng-template mat-tab-label>
-          <mat-icon class="tab-icon">description</mat-icon>
-          Description
-        </ng-template>
-      </mat-tab>
-
-      <mat-tab>
-        <ng-template mat-tab-label>
-          <mat-icon class="tab-icon">bubble_chart</mat-icon>
-          Data model
-        </ng-template>
-      </mat-tab>
-
-      <mat-tab>
-        <ng-template mat-tab-label>
-          <mat-icon class="tab-icon">list</mat-icon>
-          Data
-        </ng-template>
-      </mat-tab>
-      <mat-tab>
-        <ng-template mat-tab-label>
-          <mat-icon class="tab-icon">search</mat-icon>
-          Search
-        </ng-template>
-      </mat-tab>
-
-      <mat-tab>
-        <ng-template mat-tab-label>
-          <mat-icon class="tab-icon">settings</mat-icon>
-          Settings
-        </ng-template>
-      </mat-tab>
-    </mat-tab-group>
   `,
   styles: [
     `
       .tab-icon {
         margin-right: 8px;
+      }
+      .active-link {
+        color: var(--mat-tab-header-active-label-text-color) !important;
+        border-bottom: 2px solid var(--mat-tab-header-active-ripple-color);
+        font-weight: bold;
+      }
+
+      :host ::ng-deep .mat-mdc-tab-link {
+        padding-left: 16px;
+        padding-right: 16px;
       }
     `,
   ],
