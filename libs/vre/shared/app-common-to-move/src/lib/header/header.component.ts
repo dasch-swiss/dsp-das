03a1530f55b24878136902d01a3b01494a2ecbc2
Julien Schneider <julien.schneider.1991@gmail.com>
2025-09-22 14:16:17 +0200

    fix

--- libs/vre/shared/app-common-to-move/src/lib/header/header.component.ts
+++ libs/vre/shared/app-common-to-move/src/lib/header/header.component.ts
@@ -11,7 +11,7 @@ import { SearchParams } from '../search-params.interface';
     <mat-toolbar style="background: inherit; height: 56px; justify-content: space-between">
       <span style="display: flex; align-items: center">
         <app-header-logo />
-        <h1 style="font-size: 18px">Dasch Service Platform</h1>
+        <h1 style="font-size: 18px">DaSCH Service Platform</h1>
       </span>
 
       <app-fulltext-search />
