3ac9ba61bcc9de910c788ad8cc4fd8ebc3afea63
Julien Schneider <julien.schneider.1991@gmail.com>
2025-09-22 16:49:27 +0200

    wip

--- libs/vre/pages/data-browser/src/lib/comparison/multiple-viewer.component.ts
+++ libs/vre/pages/data-browser/src/lib/comparison/multiple-viewer.component.ts
@@ -7,11 +7,23 @@ import { MultipleViewerService } from './multiple-viewer.service';
   template: `
     @if (selectedResourceIds$ | async; as selectedResourceIds) {
       @if (selectedResourceIds.length === 0) {
-        <div class="centered">Select a resource on the left panel to display.</div>
+        <div class="centered empty-state">
+          <div class="empty-content">
+            <mat-icon style="font-size">search</mat-icon>
+            <h3>Select resources to compare</h3>
+            <p>Choose one or more resources from the left panel to display and compare them here.</p>
+          </div>
+        </div>
       } @else if (selectedResourceIds.length <= MAX_RESOURCES) {
         <app-comparison [resourceIds]="selectedResourceIds" />
       } @else if (selectedResourceIds.length > MAX_RESOURCES) {
-        <div class="centered">Too many resources are selected to be displayed.</div>
+        <div class="centered">
+          <div class="error-content">
+            <mat-icon class="error-icon">warning</mat-icon>
+            <h3>Too many resources selected</h3>
+            <p>Maximum {{ MAX_RESOURCES }} resources can be compared at once. Please deselect some resources.</p>
+          </div>
+        </div>
       }
     }
   `,
