2214b1eef0f880879a5728e5cc3d91f5d0dd50e3
Julien Schneider <julien.schneider.1991@gmail.com>
2025-09-25 11:24:46 +0200

    wip

--- libs/vre/pages/project/project/src/project.components.ts
+++ libs/vre/pages/project/project/src/project.components.ts
@@ -3,7 +3,9 @@ import { HeaderProjectComponent } from './lib/header-project.component';
 import { ProjectPageComponent } from './lib/project-page.component';
 import { AddUserComponent } from './lib/project-settings/collaboration/add-user/add-user.component';
 import { CollaborationPageComponent } from './lib/project-settings/collaboration/collaboration-page.component';
-import { ProjectMembersRowMenuComponent } from './lib/project-settings/collaboration/project-members-row-menu.component';
+import {
+  ProjectMembersRowMenuComponent
+} from './lib/project-settings/collaboration/project-members-row-menu.component';
 import { ProjectMembersRowComponent } from './lib/project-settings/collaboration/project-members-row.component';
 import { ProjectMembersComponent } from './lib/project-settings/collaboration/project-members.component';
 import { SelectGroupComponent } from './lib/project-settings/collaboration/select-group/select-group.component';
@@ -11,7 +13,9 @@ import { ImageDisplayAbsoluteComponent } from './lib/project-settings/image-sett
 import { ImageDisplayRatioComponent } from './lib/project-settings/image-settings/image-display-ratio.component';
 import { ImageSettingsComponent } from './lib/project-settings/image-settings/image-settings.component';
 import { SettingsPageComponent } from './lib/project-settings/image-settings/settings-page.component';
-import { CreateCopyrightHolderDialogComponent } from './lib/project-settings/legal/create-copyright-holder-dialog.component';
+import {
+  CreateCopyrightHolderDialogComponent
+} from './lib/project-settings/legal/create-copyright-holder-dialog.component';
 import { LegalSettingsLicensesComponent } from './lib/project-settings/legal/legal-settings-licenses.component';
 import { LegalSettingsComponent } from './lib/project-settings/legal/legal-settings.component';
 import { LicensesEnabledTableComponent } from './lib/project-settings/legal/licenses-enabled-table.component';
@@ -51,6 +55,5 @@ export const ProjectComponents = [
   ProjectSidenavLinksComponent,
   ProjectSidenavLinksItemComponent,
   ProjectSidenavOntologiesComponent,
-  ProjectSidenavLinkObjectsComponent,
   HeaderProjectComponent,
 ];
