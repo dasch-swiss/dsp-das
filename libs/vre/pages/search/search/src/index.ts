46f62115a10eef0606d188744e6dc6467349b8cf
Julien Schneider <julien.schneider.1991@gmail.com>
2025-09-25 11:02:41 +0200

    wip

--- libs/vre/pages/search/search/src/index.ts
+++ libs/vre/pages/search/search/src/index.ts
@@ -9,3 +9,4 @@ export * from './lib/project-fulltext-search-page.component';
 export * from './lib/fulltext-search-results-page.component';
 export * from './lib/global-page.component';
 export * from './lib/search-tips.component';
+export * from './lib/fulltext-search-result.component';
