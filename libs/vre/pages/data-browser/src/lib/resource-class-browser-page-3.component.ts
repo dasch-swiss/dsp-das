d797a36e96cb49cb72e4d4347276530c8b4fc4b7
Julien Schneider <julien.schneider.1991@gmail.com>
2025-09-30 14:13:46 +0200

    wip

--- libs/vre/pages/data-browser/src/lib/resource-class-browser-page-3.component.ts
+++ libs/vre/pages/data-browser/src/lib/resource-class-browser-page-3.component.ts
@@ -8,22 +8,22 @@ import { ResourceResultService } from './resource-result.service';
 @Component({
   selector: 'app-resource-class-browser-page-3',
   template: `<div style="display: flex; flex-direction: column; height: 100%">
-    @if (multipleViewerService.selectMode) {
-      <app-resource-list-selection />
-    }
     <as-split direction="horizontal">
-      <as-split-area [size]="25">
+      <as-split-area [size]="20">
         <app-project-sidenav />
       </as-split-area>
       @if (_abtestService.resourceClasSelected; as classSelected) {
-        <as-split-area [size]="25" cdkScrollable>
+        <as-split-area [size]="20" cdkScrollable>
           <app-resource-class-panel [classSelected]="classSelected" />
         </as-split-area>
-        <as-split-area [size]="50">
+        <as-split-area [size]="60">
+          @if (multipleViewerService.selectMode) {
+            <app-resource-list-selection />
+          }
           <app-multiple-viewer />
         </as-split-area>
       } @else {
-        <as-split-area [size]="75">
+        <as-split-area [size]="80">
           <app-centered-box>
             <app-centered-message
               [icon]="'arrow_circle_left'"
