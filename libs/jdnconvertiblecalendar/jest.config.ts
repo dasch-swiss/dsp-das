8f5f3f51eaeaaa59f7ed00d6aecb0ad227d7a835
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-10-07 10:56:15 +0200

    fis

--- libs/jdnconvertiblecalendar/jest.config.ts
+++ libs/jdnconvertiblecalendar/jest.config.ts
@@ -2,9 +2,9 @@
 export default {
   displayName: 'jdnconvertiblecalendar',
   preset: '../../jest.preset.js',
+  setupFilesAfterEnv: undefined,
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 };
