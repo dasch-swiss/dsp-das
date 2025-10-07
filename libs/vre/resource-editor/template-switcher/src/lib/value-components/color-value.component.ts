2bb2f70bfd29456aa8091e0830e9ef3ad19155f1
domsteinbach <dominique.steinbach@unibas.ch>
2025-10-07 17:29:15 +0200

    refactor: color picker

--- libs/vre/resource-editor/template-switcher/src/lib/value-components/color-value.component.ts
+++ libs/vre/resource-editor/template-switcher/src/lib/value-components/color-value.component.ts
@@ -1,6 +1,5 @@
-import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ColorPickerDirective } from 'ngx-color-picker';
 
 @Component({
   selector: 'app-color-value',
@@ -8,16 +7,14 @@ import { ColorPickerDirective } from 'ngx-color-picker';
     <app-nullable-editor [formControl]="control" [defaultValue]="'#000000'">
       <div
         #colorPickerTrigger="ngxColorPicker"
-        #triggerDiv
-        [colorPicker]="control.value ?? '#ff0000'"
+        [colorPicker]="control.value ?? DEFAULT_COLOR"
         [cpOutputFormat]="'hex'"
         [cpToggle]="true"
         [cpSaveClickOutside]="false"
-        [cpFallbackColor]="'#ff0000'"
+        [cpFallbackColor]="DEFAULT_COLOR"
         [cpDialogDisplay]="'inline'"
         [cpUseRootViewContainer]="true"
-        (colorPickerChange)="onColorChange($event)"
-        style="position: fixed; top: 0; left: 0; height: 0; width: 0;"></div>
+        (colorPickerChange)="onColorChange($event)"></div>
     </app-nullable-editor>
 
     @if (control && control.errors; as errors) {
@@ -26,14 +23,11 @@ import { ColorPickerDirective } from 'ngx-color-picker';
       </mat-error>
     }
   `,
-  styles: [``],
   standalone: false,
 })
 export class ColorValueComponent {
+  readonly DEFAULT_COLOR = '#ff0000';
   @Input({ required: true }) control!: FormControl<string | null>;
-  @ViewChild('colorPickerTrigger') colorPickerDirective?: ColorPickerDirective;
-  @ViewChild('inputElement') inputElement?: ElementRef<HTMLInputElement>;
-  @ViewChild('triggerDiv') triggerDiv?: ElementRef<HTMLDivElement>;
 
   onColorChange(color: string) {
     this.control.setValue(color);
