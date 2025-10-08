06915d915495ff44eee9b06e7ae2f03a204b1632
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-10-08 15:08:28 +0200

    chore: remove Karma completely (DEV-5472) (#2561)
    Co-authored-by: domsteinbach <36757218+domsteinbach@users.noreply.github.com>

--- libs/vre/ui/string-literal/jest.config.ts
+++ libs/vre/ui/string-literal/jest.config.ts
@@ -2,21 +2,6 @@
 export default {
     displayName: 'vre-library-app-string-literal',
     preset: '../../../../jest.preset.js',
-    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     coverageDirectory:
         '../../../../coverage/libs/vre/library/app-string-literal',
-    transform: {
-        '^.+\\.(ts|mjs|js|html)$': [
-            'jest-preset-angular',
-            {
-                tsconfig: '<rootDir>/tsconfig.spec.json',
-                stringifyContentPathRegex: '\\.(html|svg)$',
-            },
-        ],
-    },
-    snapshotSerializers: [
-        'jest-preset-angular/build/serializers/no-ng-attributes',
-        'jest-preset-angular/build/serializers/ng-snapshot',
-        'jest-preset-angular/build/serializers/html-comment',
-    ],
 };
