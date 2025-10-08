f0e22ff6d38dc99e485d38a32e4e7ae15cbc8434
domsteinbach <36757218+domsteinbach@users.noreply.github.com>
2025-10-08 14:30:14 +0200

    fix: color picker is clipped by parents (DEV-5464) (#2554)

--- libs/vre/resource-editor/template-switcher/src/lib/value-components/color-value.component.ts
+++ libs/vre/resource-editor/template-switcher/src/lib/value-components/color-value.component.ts
@@ -5,44 +5,15 @@ import { FormControl } from '@angular/forms';
   selector: 'app-color-value',
   template: `
     <app-nullable-editor [formControl]="control" [defaultValue]="'#000000'">
-      <mat-form-field appearance="outline" style="cursor: pointer">
-        <mat-label>{{ control.value }}</mat-label>
-        <!-- check the ngx-color-picker doc to know more about the options - https://www.npmjs.com/package/ngx-color-picker -->
-        <input
-          data-cy="color-picker-input"
-          matInput
-          placeholder="Select a color"
-          class="color-picker-input color"
-          [formControl]="control"
-          [colorPicker]="control.value"
-          [style.background]="control.value"
-          [style.color]="control.value"
-          (colorPickerChange)="control.patchValue($event)"
-          [cpOutputFormat]="'hex'"
-          [cpFallbackColor]="'#ff0000'"
-          [cpDisabled]="false"
-          style="cursor: pointer"
-          readonly />
-      </mat-form-field>
+      <app-color-picker [formControl]="control" [hexColor]="'#ff0000'"></app-color-picker>
     </app-nullable-editor>
-    @if (control.touched && control.errors; as errors) {
+
+    @if (control && control.errors; as errors) {
       <mat-error>
         {{ errors | humanReadableError }}
       </mat-error>
     }
   `,
-  styles: [
-    `
-      :host {
-        z-index: 1;
-        position: relative;
-
-        ::ng-deep .mat-mdc-form-field-subscript-wrapper {
-          display: none !important;
-        }
-      }
-    `,
-  ],
   standalone: false,
 })
 export class ColorValueComponent {
