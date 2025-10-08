edbf5cc58e65b8e7311ef22306eecf1987edba60
Julien Schneider <julien.schneider.1991@gmail.com>
2025-10-08 11:23:43 +0200

    wip

--- libs/vre/pages/data-browser/src/lib/rcbp-class.component.ts
+++ libs/vre/pages/data-browser/src/lib/rcbp-class.component.ts
@@ -1,15 +1,16 @@
 import { Component, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { KnoraApiConnection, ResourceClassDefinitionWithAllLanguages } from '@dasch-swiss/dsp-js';
+import { DspApiConnectionToken } from '@dasch-swiss/vre/core/config';
+import { ProjectPageService } from '@dasch-swiss/vre/pages/project/project';
+import { combineLatest, map } from 'rxjs';
 import { MultipleViewerService } from './comparison/multiple-viewer.service';
 import { AbTestService } from './resource-class-sidenav/ab-test.service';
-import { map } from 'rxjs';
-import { DspApiConnectionToken } from '@dasch-swiss/vre/core/config';
-import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
 
 @Component({
   selector: 'app-rcbp-class',
   template: `
-    @if (abtestService.resourceClasSelected; as classSelected) {
+    @if (data$ | async; as classSelected) {
       <as-split>
         <as-split-area [size]="34" cdkScrollable>
           <app-resource-class-panel [classSelected]="classSelected" />
@@ -26,11 +27,28 @@ import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
   standalone: false,
 })
 export class RcbpClassComponent {
-  data$ = this._route.params.pipe(map(params => this._dspApiConnection.v2.onto.))
+  data$ = combineLatest([this._route.params, this._projectPageService.ontologies$]).pipe(
+    map(([params, ontologies]) => {
+      console.log(params, ontologies);
+      const ontologyLabel = params['ontologyLabel'] as string;
+      const classLabel = params['classLabel'] as string;
+
+      const ontology = ontologies.find(ontology => ontology.label === ontologyLabel);
+      if (!ontology) {
+        throw new Error('TODO: guard');
+      }
+      const resClass = Object.values(ontology.classes).find(cls => cls.label === classLabel);
+      if (!resClass) {
+        throw new Error('TODO: guard');
+      }
+      return { ontologyLabel, classLabel, ontology, resClass: resClass as ResourceClassDefinitionWithAllLanguages };
+    })
+  );
   constructor(
     public multipleViewerService: MultipleViewerService,
     public abtestService: AbTestService,
+    private _projectPageService: ProjectPageService,
     private _route: ActivatedRoute,
-    @Inject(DspApiConnectionToken) private _dspApiConnection: KnoraApiConnection,
+    @Inject(DspApiConnectionToken) private _dspApiConnection: KnoraApiConnection
   ) {}
 }
