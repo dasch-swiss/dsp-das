2e2ea1a9d4de343c04f84c90c29ff928adbaad38
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-10-08 10:02:01 +0200

    chore: post Angular upgrades cleanup (DEV-5459) (#2547)

--- libs/vre/core/error-handler/jest.config.ts
+++ libs/vre/core/error-handler/jest.config.ts
@@ -2,20 +2,5 @@
 export default {
   displayName: 'vre-core-error-handler',
   preset: '../../../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../../../coverage/libs/vre/shared/app-error-handler',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
-  },
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
 };
--- libs/vre/pages/search/search/jest.config.ts
+++ libs/vre/pages/search/search/jest.config.ts
@@ -2,20 +2,5 @@
 export default {
   displayName: 'vre-pages-search-search',
   preset: '../../../../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../../../../coverage/libs/vre/pages/search/search',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
-  },
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
 };
