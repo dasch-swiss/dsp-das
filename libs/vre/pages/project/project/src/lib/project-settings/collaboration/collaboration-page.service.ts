6997c1c90509907297a3962725a9cec7b0abc3c8
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-10-17 10:21:33 +0200

    fix backend OpenApi redesign

--- libs/vre/pages/project/project/src/lib/project-settings/collaboration/collaboration-page.service.ts
+++ libs/vre/pages/project/project/src/lib/project-settings/collaboration/collaboration-page.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AdminGroupsApiService, AdminProjectsApiService } from '@dasch-swiss/vre/3rd-party-services/open-api';
+import { AdminAPIApiService } from '@dasch-swiss/vre/3rd-party-services/open-api';
 import { BehaviorSubject, EMPTY, filter, map, shareReplay, switchMap } from 'rxjs';
 import { ProjectPageService } from '../../project-page.service';
 
@@ -15,7 +15,7 @@ export class CollaborationPageService {
         if (!project) {
           return EMPTY;
         }
-        return this._adminGroupsApi.getAdminGroups().pipe(
+        return this._adminApiService.getAdminGroups().pipe(
           map(response => {
             return (response.groups ?? []).filter(group => (group.project!.id as unknown as string) === project.id);
           })
@@ -27,15 +27,14 @@ export class CollaborationPageService {
   projectMembers$ = this.reloadProjectMembers$.asObservable().pipe(
     switchMap(() => this._project$),
     filter(project => project !== undefined),
-    switchMap(project => this._adminProjectsApi.getAdminProjectsIriProjectiriMembers(project!.id)),
+    switchMap(project => this._adminApiService.getAdminProjectsIriProjectiriMembers(project!.id)),
     map(response => response.members || []),
     shareReplay({ bufferSize: 1, refCount: true })
   );
 
   constructor(
-    private _adminGroupsApi: AdminGroupsApiService,
-    private _projectPageService: ProjectPageService,
-    private _adminProjectsApi: AdminProjectsApiService
+    private _adminApiService: AdminAPIApiService,
+    private _projectPageService: ProjectPageService
   ) {}
 
   reloadProjectMembers() {
