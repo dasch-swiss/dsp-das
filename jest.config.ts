270b892ef16d22bf02d778254a157ec2551b4d35
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-10-21 14:24:06 +0200

    enable unit test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,8 @@ import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
     projects: await getJestProjectsAsync(),
+    // Combined coverage configuration
+    collectCoverage: true,
+    coverageDirectory: './coverage',
+    coverageReporters: ['html', 'lcov', 'text-summary', 'json'],
 });
