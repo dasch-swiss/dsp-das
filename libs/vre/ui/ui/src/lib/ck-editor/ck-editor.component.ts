2b49a2da3c40f04de76cea9ecf3c523e2d3243f6
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-11-04 13:09:46 +0100

    validator cleanup

--- libs/vre/ui/ui/src/lib/ck-editor/ck-editor.component.ts
+++ libs/vre/ui/ui/src/lib/ck-editor/ck-editor.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { TranslateModule } from '@ngx-translate/core';
 import * as Editor from 'ckeditor5-custom-build';
-import { startWith } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { startWith, takeUntil } from 'rxjs/operators';
 import { HumanReadableErrorPipe } from '../human-readable-error.pipe';
 import { ckEditor } from './ck-editor';
 import { crossProjectLinkValidator } from './cross-project-link.validator';
@@ -24,13 +25,15 @@ import { unescapeHtml } from './unescape-html';
   imports: [CKEditorModule, HumanReadableErrorPipe, MatFormFieldModule, ReactiveFormsModule, TranslateModule],
   standalone: true,
 })
-export class CkEditorComponent implements OnInit {
+export class CkEditorComponent implements OnInit, OnDestroy {
   @Input({ required: true }) control!: FormControl<string | null>;
   @Input() projectShortcode?: string | null;
   footnoteControl = new FormControl('');
 
   readonly editor = Editor;
   protected readonly ckEditor = ckEditor;
+
+  private readonly _destroy$ = new Subject<void>();
 
   ngOnInit() {
     if (this.projectShortcode) {
@@ -39,16 +42,18 @@ export class CkEditorComponent implements OnInit {
     }
     let updating = false;
 
-    this.control.valueChanges.pipe(startWith(this.control.value)).subscribe(change => {
-      if (updating) {
-        return;
-      }
-      updating = true;
-      this.footnoteControl.patchValue(change === null ? null : this._parseToFootnote(change));
-      updating = false;
-    });
+    this.control.valueChanges
+      .pipe(startWith(this.control.value), takeUntil(this._destroy$))
+      .subscribe(change => {
+        if (updating) {
+          return;
+        }
+        updating = true;
+        this.footnoteControl.patchValue(change === null ? null : this._parseToFootnote(change));
+        updating = false;
+      });
 
-    this.footnoteControl.valueChanges.subscribe(value => {
+    this.footnoteControl.valueChanges.pipe(takeUntil(this._destroy$)).subscribe(value => {
       if (updating) {
         return;
       }
@@ -56,6 +61,17 @@ export class CkEditorComponent implements OnInit {
       this.control.patchValue(value ? this._parseFromFootnote(value) : '');
       updating = false;
     });
+  }
+
+  ngOnDestroy() {
+    this._destroy$.next();
+    this._destroy$.complete();
+
+    // Remove the validator that was added in ngOnInit
+    if (this.projectShortcode) {
+      this.control.removeValidators(crossProjectLinkValidator(this.projectShortcode));
+      this.control.updateValueAndValidity();
+    }
   }
 
   private _parseToFootnote(rawHtml: string) {
