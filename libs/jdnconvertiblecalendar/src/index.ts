4153a485c8c2ffcca109da53d4c7c895a475416e
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-11-05 14:25:28 +0100

    chore: remove copyright header

--- libs/jdnconvertiblecalendar/src/index.ts
+++ libs/jdnconvertiblecalendar/src/index.ts
@@ -1,8 +1,3 @@
-/*
- * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
- *  SPDX-License-Identifier: Apache-2.0
- */
-
 export { CalendarDate } from './lib/CalendarDate';
 export { CalendarPeriod } from './lib/CalendarPeriod';
 export { JDNPeriod } from './lib/JDNPeriod';
