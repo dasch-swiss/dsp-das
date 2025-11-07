44a44c8cc9f2b777854aa9cd0644089f3c660a55
Julien Schneider <julien.schneider.1991@gmail.com>
2025-11-07 11:53:10 +0100

    chore: migrate ui/progress-indicators to standalone (#2621)

--- libs/vre/ui/progress-indicator/src/lib/app-progress-indicator/progress-indicator-overlay.component.ts
+++ libs/vre/ui/progress-indicator/src/lib/app-progress-indicator/progress-indicator-overlay.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { AppProgressIndicatorComponent } from './app-progress-indicator.component';
 
 @Component({
   selector: 'app-progress-indicator-overlay',
+  imports: [AppProgressIndicatorComponent],
+  standalone: true,
   template: ` <div
     style="position: fixed; top: 0; left: 0; z-index: 3; width: 100%; height: 100%; background-color: rgba(255, 255, 255, 0.7); display: flex; justify-content: center; align-items: center;">
     <app-progress-indicator />
   </div>`,
-  standalone: false,
 })
 export class ProgressIndicatorOverlayComponent {}
