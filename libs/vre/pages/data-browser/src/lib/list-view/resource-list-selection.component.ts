7ed39d073455da3e2442969729d5c5a8a5799f21
Julien Schneider <julien.schneider.1991@gmail.com>
2025-11-12 14:12:37 +0100

    fix

--- libs/vre/pages/data-browser/src/lib/list-view/resource-list-selection.component.ts
+++ libs/vre/pages/data-browser/src/lib/list-view/resource-list-selection.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ReadResource } from '@dasch-swiss/dsp-js';
+import { UserService } from '@dasch-swiss/vre/core/session';
+import { ProjectService } from '@dasch-swiss/vre/shared/app-helper-services';
 import { combineLatest, map } from 'rxjs';
 import { MultipleViewerService } from '../comparison/multiple-viewer.service';
 import { ResourceLinkDialogComponent, ResourceLinkDialogProps } from './resource-link-dialog.component';
@@ -16,12 +18,15 @@ import { ResourceLinkDialogComponent, ResourceLinkDialogProps } from './resource
         @if ((showCreateLink$ | async) && (multipleViewerService.selectedResources$ | async); as selectedResources) {
           <div
             style="display: inline-block"
-            [matTooltip]="'pages.dataBrowser.resourceListSelection.createLinkObjectTooltip' | translate"
-            [matTooltipDisabled]="(isCreateLinkButtonDisabled$ | async) === false">
+            [matTooltip]="
+              (isCreateLinkButtonDisabled$ | async)?.reason ||
+              ('pages.dataBrowser.resourceListSelection.createLinkObjectTooltip' | translate)
+            "
+            [matTooltipDisabled]="!(isCreateLinkButtonDisabled$ | async)?.disabled">
             <button
               mat-flat-button
               (click)="openCreateLinkDialog(selectedResources)"
-              [disabled]="isCreateLinkButtonDisabled$ | async">
+              [disabled]="(isCreateLinkButtonDisabled$ | async)?.disabled">
               {{ 'pages.dataBrowser.resourceListSelection.createLinkObject' | translate }}
             </button>
           </div>
@@ -35,20 +40,40 @@ import { ResourceLinkDialogComponent, ResourceLinkDialogProps } from './resource
 })
 export class ResourceListSelectionComponent {
   count$ = this.multipleViewerService.selectedResources$.pipe(map(resources => resources.length));
-  showCreateLink$ = combineLatest([this.count$, this.multipleViewerService.hasRightsToShowCreateLinkObject$]).pipe(
-    map(([count, hasProjectMemberRights]) => count > 1 && hasProjectMemberRights)
-  );
+  showCreateLink$ = this.count$.pipe(map(count => count > 1));
 
-  isCreateLinkButtonDisabled$ = this.multipleViewerService.selectedResources$.pipe(
-    map(resources => {
-      if (resources.length === 0) return true;
+  isCreateLinkButtonDisabled$ = combineLatest([
+    this.multipleViewerService.selectedResources$,
+    this._userService.user$,
+  ]).pipe(
+    map(([resources, user]) => {
+      if (resources.length === 0) {
+        return { disabled: true, reason: 'No resources selected' };
+      }
+
       const projectUuid = resources[0].attachedToProject;
-      return !resources.every(resource => resource.attachedToProject === projectUuid);
+      const allSameProject = resources.every(resource => resource.attachedToProject === projectUuid);
+
+      if (!allSameProject) {
+        return { disabled: true, reason: 'Resources must be from the same project' };
+      }
+
+      if (!user) {
+        return { disabled: true, reason: 'User not authenticated' };
+      }
+
+      const hasProjectMemberRights = ProjectService.HasProjectMemberRights(user, projectUuid);
+      if (!hasProjectMemberRights) {
+        return { disabled: true, reason: 'You do not have project member rights' };
+      }
+
+      return { disabled: false };
     })
   );
 
   constructor(
     public multipleViewerService: MultipleViewerService,
+    private readonly _userService: UserService,
     private readonly _dialog: MatDialog
   ) {}
 
