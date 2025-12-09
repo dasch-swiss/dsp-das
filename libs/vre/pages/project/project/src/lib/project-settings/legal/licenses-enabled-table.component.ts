342659869538b922e92d8d1c3f0eedb5c4a84058
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-12-09 14:09:05 +0100

    fix: handle license checkboxes correctly

--- libs/vre/pages/project/project/src/lib/project-settings/legal/licenses-enabled-table.component.ts
+++ libs/vre/pages/project/project/src/lib/project-settings/legal/licenses-enabled-table.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { ReadProject } from '@dasch-swiss/dsp-js';
 import { AdminAPIApiService, ProjectLicenseDto } from '@dasch-swiss/vre/3rd-party-services/open-api';
 
@@ -20,9 +21,7 @@ import { AdminAPIApiService, ProjectLicenseDto } from '@dasch-swiss/vre/3rd-part
             </a>
           </td>
           <td>
-            <mat-checkbox
-              [checked]="license.isEnabled"
-              (change)="$event.checked ? enable(license.id) : disable(license.id)" />
+            <mat-checkbox [checked]="license.isEnabled" (change)="onLicenseToggle($event, license.id)" />
           </td>
         </tr>
       }
@@ -74,16 +73,34 @@ export class LicensesEnabledTableComponent {
   enable(licenseIri: string) {
     this._adminApiService
       .putAdminProjectsShortcodeProjectshortcodeLegalInfoLicensesLicenseiriEnable(this.project.shortcode, licenseIri)
-      .subscribe(() => {
-        this.refresh.emit();
+      .subscribe({
+        error: () => {
+          this.refresh.emit();
+        },
       });
   }
 
   disable(licenseIri: string) {
     this._adminApiService
       .putAdminProjectsShortcodeProjectshortcodeLegalInfoLicensesLicenseiriDisable(this.project.shortcode, licenseIri)
-      .subscribe(() => {
-        this.refresh.emit();
+      .subscribe({
+        error: () => {
+          this.refresh.emit();
+        },
       });
+  }
+
+  onLicenseToggle(event: MatCheckboxChange, licenseId: string) {
+    // Optimistic update: immediately update local state
+    const license = this.licenses.find(l => l.id === licenseId);
+    if (license) {
+      license.isEnabled = event.checked;
+    }
+
+    if (event.checked) {
+      this.enable(licenseId);
+    } else {
+      this.disable(licenseId);
+    }
   }
 }
