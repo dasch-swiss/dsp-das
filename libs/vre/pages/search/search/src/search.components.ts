14246ba751a90f89f71e912fa15dc0cc7de07301
Julien Schneider <julien.schneider.1991@gmail.com>
2025-12-09 12:19:15 +0100

    fix

--- libs/vre/pages/search/search/src/search.components.ts
+++ libs/vre/pages/search/search/src/search.components.ts
@@ -1,5 +1,4 @@
 import { AdvancedSearchPageComponent } from './lib/advanced-search/advanced-search-page.component';
 import { GlobalPageComponent } from './lib/global-page.component';
-import { ProjectSearchPageComponent } from './lib/project-search-page.component';
 
-export const SearchComponents = [AdvancedSearchPageComponent, ProjectSearchPageComponent, GlobalPageComponent];
+export const SearchComponents = [AdvancedSearchPageComponent, GlobalPageComponent];
