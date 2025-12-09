af6f69118296fe6a06f1ebcb8bf30a6dfdbb2ee9
Julien Schneider <julien.schneider.1991@gmail.com>
2025-12-09 10:13:07 +0100

    fix

--- libs/vre/pages/data-browser/src/index.ts
+++ libs/vre/pages/data-browser/src/index.ts
@@ -1,21 +1,7 @@
-export * from './lib/resource-class-sidenav/resource-class-sidenav.component';
-export * from './lib/resource-class-sidenav/resource-class-sidenav-item.component';
-
-export * from './lib/comparison/comparison.component';
-
-export * from './lib/list-view/resource-link-dialog.component';
-
-export * from './lib/list-view/resources-list.component';
-export * from './lib/comparison/multiple-viewer.component';
-export * from './lib/comparison/resource-browser.component';
-export * from './lib/list-view/resource-list.component';
-export * from './lib/list-view/resource-list-item.component';
-export * from './lib/comparison/multiple-viewer.service';
-export * from './lib/list-view/resource-list-selection.component';
-
-export * from './lib/resource-result.service';
-export * from './lib/resource-class-sidenav/resources-list-fetcher.component';
+// Main page components used in routing
 export * from './lib/data-browser-page.component';
-export * from './lib/data-class-panel.component';
 export * from './lib/data-class-view.component';
 export * from './lib/data-overview.component';
+
+// Services used by other libraries
+export * from './lib/resource-result.service';
