b5749b132349e5258aace99895f0b82d7e023858
Julien Schneider <julien.schneider.1991@gmail.com>
2025-12-10 11:29:12 +0100

    fix

--- libs/vre/resource-editor/resource-editor/src/index.ts
+++ libs/vre/resource-editor/resource-editor/src/index.ts
@@ -2,4 +2,4 @@ export * from './lib/single-resource-page.component';
 export * from './lib/resource-fetcher.component';
 export * from './lib/resource-fetcher-dialog.component';
 export * from './lib/closing-dialog.component';
-export { CreateResourceDialogComponent, CreateResourceDialogProps } from './lib/template-switcher/create-resource-dialog.component';
+export * from './lib/template-switcher/create-resource-dialog.component';
