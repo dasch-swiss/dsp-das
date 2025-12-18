592abba41dbf64e7e2989258f49a25e2103f568e
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-12-18 09:46:56 +0100

    remove deprecated HelpPageComponent and unused test

--- libs/vre/shared/app-help-page/src/index.ts
+++ libs/vre/shared/app-help-page/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/help-page/help-page.component';
 export * from './lib/footer/footer.component';
 export * from './lib/grid/grid.component';
-export * from './help-page.components';
