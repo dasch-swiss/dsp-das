5c1e7ffc772122292f70e0992b777f732c33d5da
Julien Schneider <julien.schneider.1991@gmail.com>
2026-01-19 11:48:55 +0100

    translation

--- libs/vre/resource-editor/resource-editor/src/lib/representations/representation-error-message.component.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/representations/representation-error-message.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NoResultsFoundComponent } from '@dasch-swiss/vre/ui/ui';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-representation-error-message',
-  imports: [NoResultsFoundComponent],
+  imports: [NoResultsFoundComponent, TranslatePipe],
   template: ` <app-no-results-found
-    [message]="'The resource has not loaded.'"
+    [message]="'shared.status.resourceNotLoaded' | translate"
     style="display: block; padding: 16px" />`,
 })
 export class RepresentationErrorMessageComponent {}
