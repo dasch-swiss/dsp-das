14ae18ab7d2792e03fad1a25b4d243f6db0fb0ab
Julien Schneider <julien.schneider.1991@gmail.com>
2026-01-20 09:49:14 +0100

    remove showBackFormButton

--- libs/vre/pages/data-browser/src/lib/list-view/resources-list.component.ts
+++ libs/vre/pages/data-browser/src/lib/list-view/resources-list.component.ts
@@ -1,25 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { MatAnchor } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
-import { ActivatedRoute, Router } from '@angular/router';
 import { ReadResource } from '@dasch-swiss/dsp-js';
-import { RouteConstants } from '@dasch-swiss/vre/core/config';
 import { ResourceResultService } from '@dasch-swiss/vre/shared/app-helper-services';
 import { PagerComponent } from '@dasch-swiss/vre/ui/ui';
-import { TranslatePipe } from '@ngx-translate/core';
 import { ResourceListComponent } from './resource-list.component';
 
 @Component({
   selector: 'app-resources-list',
-  template: ` @if (showBackToFormButton) {
-      <div style="padding: 16px; display: flex; flex-direction: row-reverse">
-        <a mat-stroked-button (click)="navigate()">
-          <mat-icon>chevron_left</mat-icon>{{ 'pages.dataBrowser.resourcesList.backToSearchForm' | translate }}
-        </a>
-      </div>
-    }
-
-    @if (resourceResultService.numberOfResults > resourceResultService.MAX_RESULTS_PER_PAGE) {
+  template: ` @if (resourceResultService.numberOfResults > resourceResultService.MAX_RESULTS_PER_PAGE) {
       <app-pager
         (pageIndexChanged)="updatePageIndex($event)"
         [numberOfAllResults]="resourceResultService.numberOfResults" />
@@ -33,24 +21,14 @@ import { ResourceListComponent } from './resource-list.component';
       }
     `,
   ],
-  imports: [MatAnchor, MatIcon, TranslatePipe, PagerComponent, ResourceListComponent],
+  imports: [MatAnchor, PagerComponent, ResourceListComponent],
 })
 export class ResourcesListComponent {
   @Input({ required: true }) resources!: ReadResource[];
-  @Input({ required: true }) showBackToFormButton!: boolean;
 
-  constructor(
-    public resourceResultService: ResourceResultService,
-    private readonly _router: Router,
-    private readonly _route: ActivatedRoute
-  ) {}
+  constructor(public resourceResultService: ResourceResultService) {}
 
   updatePageIndex(index: number) {
     this.resourceResultService.updatePageIndex(index);
-  }
-
-  navigate() {
-    const projectUuid = this._route.parent?.snapshot.params['uuid'];
-    this._router.navigate([RouteConstants.project, projectUuid, RouteConstants.advancedSearch]);
   }
 }
--- libs/vre/pages/search/advanced-search/src/lib/advanced-search-results-page.component.ts
+++ libs/vre/pages/search/advanced-search/src/lib/advanced-search-results-page.component.ts
@@ -1,11 +1,8 @@
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectorRef, Component, Inject, Input, OnChanges } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
 import { KnoraApiConnection } from '@dasch-swiss/dsp-js';
-import { DspApiConnectionToken, RouteConstants } from '@dasch-swiss/vre/core/config';
+import { DspApiConnectionToken } from '@dasch-swiss/vre/core/config';
 import { ResourceBrowserComponent } from '@dasch-swiss/vre/pages/data-browser';
 import { ResourceResultService } from '@dasch-swiss/vre/shared/app-helper-services';
 import { CenteredBoxComponent, NoResultsFoundComponent } from '@dasch-swiss/vre/ui/ui';
@@ -13,18 +10,15 @@ import { combineLatest, map, Subject, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-advanced-search-results-page',
-  imports: [AsyncPipe, MatButton, MatIcon, CenteredBoxComponent, NoResultsFoundComponent, ResourceBrowserComponent],
+  imports: [AsyncPipe, CenteredBoxComponent, NoResultsFoundComponent, ResourceBrowserComponent],
   template: `
     @if (resources$ | async; as resources) {
       @if (resources.length === 0) {
         <app-centered-box>
           <app-no-results-found [message]="noResultMessage" />
-          <a mat-stroked-button (click)="navigate()"><mat-icon>chevron_left</mat-icon>Back to search form</a>
         </app-centered-box>
       } @else {
-        <app-resource-browser
-          [data]="{ resources: resources, selectFirstResource: true }"
-          [showBackToFormButton]="true" />
+        <app-resource-browser [data]="{ resources: resources, selectFirstResource: true }" />
       }
     }
   `,
@@ -55,8 +49,6 @@ export class AdvancedSearchResultsPageComponent implements OnChanges {
   constructor(
     @Inject(DspApiConnectionToken) private readonly _dspApiConnection: KnoraApiConnection,
     private readonly _resourceResultService: ResourceResultService,
-    private readonly _route: ActivatedRoute,
-    private readonly _router: Router,
     private readonly _titleService: Title,
     private readonly _cd: ChangeDetectorRef
   ) {
@@ -82,9 +74,4 @@ export class AdvancedSearchResultsPageComponent implements OnChanges {
 
   private _numberOfAllResults$ = (query_: string) =>
     this._dspApiConnection.v2.search.doExtendedSearchCountQuery(`${this._getQuery(query_)}OFFSET 0`);
-
-  navigate() {
-    const projectUuid = this._route.parent?.snapshot.params['uuid'];
-    this._router.navigate([RouteConstants.project, projectUuid, RouteConstants.advancedSearch]);
-  }
 }
