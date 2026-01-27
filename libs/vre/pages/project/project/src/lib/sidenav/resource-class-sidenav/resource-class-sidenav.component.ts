4926e251552d9ec959e1525ba253363d00e9b6c4
domsteinbach <36757218+domsteinbach@users.noreply.github.com>
2026-01-27 16:21:34 +0100

    fix: reintroduce sorting of class labels acc. to language (#2812)

--- libs/vre/pages/project/project/src/lib/sidenav/resource-class-sidenav/resource-class-sidenav.component.ts
+++ libs/vre/pages/project/project/src/lib/sidenav/resource-class-sidenav/resource-class-sidenav.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input } from '@angular/core';
-import { OntologyAndResourceClasses } from '@dasch-swiss/vre/3rd-party-services/open-api';
+import { Component, Input, OnChanges } from '@angular/core';
+import {
+  OntologyAndResourceClasses,
+  ResourceClassAndCountDto,
+  ResourceClassDto,
+} from '@dasch-swiss/vre/3rd-party-services/open-api';
+import { LocalizationService } from '@dasch-swiss/vre/shared/app-helper-services';
 import { ResourceClassSidenavItemComponent } from './resource-class-sidenav-item.component';
 
 @Component({
   selector: 'app-resource-class-sidenav',
   template: `
-    @for (classToDisplay of ontology.classesAndCount; track $index) {
+    @for (classToDisplay of resourceClassCounts; track $index) {
       <app-resource-class-sidenav-item
         [count]="classToDisplay.itemCount"
         [label]="classToDisplay.resourceClass.label!"
@@ -15,6 +20,24 @@ import { ResourceClassSidenavItemComponent } from './resource-class-sidenav-item
   `,
   imports: [ResourceClassSidenavItemComponent],
 })
-export class ResourceClassSidenavComponent {
+export class ResourceClassSidenavComponent implements OnChanges {
   @Input({ required: true }) ontology!: OntologyAndResourceClasses;
+  resourceClassCounts: ResourceClassAndCountDto[] = [];
+
+  constructor(private readonly _localizationService: LocalizationService) {}
+
+  ngOnChanges() {
+    const lang = this._localizationService.getCurrentLanguage();
+    const classesCount = this.ontology.classesAndCount || [];
+
+    this.resourceClassCounts = [...classesCount].sort((a, b) => {
+      const labelA = this.getLabelInLanguage(a.resourceClass, lang).toLowerCase();
+      const labelB = this.getLabelInLanguage(b.resourceClass, lang).toLowerCase();
+      return labelA.localeCompare(labelB);
+    });
+  }
+
+  getLabelInLanguage(resourceClass: ResourceClassDto, lang = 'en'): string {
+    return resourceClass.label ? resourceClass.label.find(l => l.language === lang)?.value || '' : '';
+  }
 }
