01e3785d6049f5462f32fcefb611f83a99ce97cd
Marcin Procyk <marcin.procyk@dasch.swiss>
2026-02-26 16:24:00 +0100

    fix failing e2e test

--- apps/dsp-app/cypress/support/pages/add-resource-instance-page.ts
+++ apps/dsp-app/cypress/support/pages/add-resource-instance-page.ts
@@ -23,8 +23,9 @@ export class AddResourceInstancePage {
   };
 
   saveEdit() {
+    cy.intercept('PUT', '**/v2/values').as('saveValue');
     cy.get('[data-cy=save-button]').click({ force: true });
-    cy.reload();
+    cy.wait('@saveValue');
     cy.get('[data-cy=property-value]').should('be.visible');
   }
 
