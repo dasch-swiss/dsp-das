0759c30ddb23c2da044f236f64098c2cf8605445
Julien Schneider <julien.schneider.1991@gmail.com>
2026-03-03 11:42:01 +0100

    wip

--- libs/vre/resource-editor/resource-editor/src/lib/representations/download-menu-item.component.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/representations/download-menu-item.component.ts
@@ -1,64 +1,32 @@
 import { Clipboard } from '@angular/cdk/clipboard';
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatMenu, MatMenuItem } from '@angular/material/menu';
 import { ReadFileValue, ReadResource } from '@dasch-swiss/dsp-js';
+import { TranslatePipe } from '@ngx-translate/core';
 import { RepresentationService } from './representation.service';
 
 @Component({
   selector: 'app-download-menu-item',
   standalone: true,
-  imports: [MatButton, MatIcon, MatMenu, MatMenuItem],
+  imports: [MatButton, MatIcon, TranslatePipe],
   template: `
-    <div>
-      <button mat-flat-button style="margin-right: 16px">
+    <div style="display: flex; gap: 16px">
+      <button mat-flat-button (click)="download()">
         <mat-icon>download</mat-icon>
-        Download File
+        {{ 'resourceEditor.representations.downloadFile' | translate }}
       </button>
 
-      <button mat-flat-button>
+      <button mat-flat-button (click)="copyUrl()">
         <mat-icon>link</mat-icon>
         Copy link
       </button>
     </div>
-
-    <mat-menu #menu="matMenu" xPosition="before">
-      <button mat-menu-item>
-        <mat-icon>link</mat-icon>
-        <span>Copy link</span>
-      </button>
-    </mat-menu>
   `,
-  styles: [
-    `
-      .split-button-container {
-        display: inline-flex;
-        align-items: stretch;
-        overflow: hidden;
-
-        .main-button {
-          border-top-right-radius: 0;
-          border-bottom-right-radius: 0;
-          border-right: 1px solid rgba(255, 255, 255, 0.2); /* Subtle separator */
-        }
-
-        .arrow-button {
-          border-top-left-radius: 0;
-          border-bottom-left-radius: 0;
-          min-width: 40px;
-          padding: 0 4px;
-        }
-      }
-    `,
-  ],
 })
 export class DownloadMenuItemComponent {
   @Input({ required: true }) src!: ReadFileValue;
   @Input({ required: true }) parentResource!: ReadResource;
-
-  copied = signal(false);
-  copyStatus = signal('Copy URL to clipboard');
 
   constructor(
     private readonly _rs: RepresentationService,
@@ -70,16 +38,6 @@ export class DownloadMenuItemComponent {
   }
 
   copyUrl() {
-    const url = this.src.fileUrl; // Assuming fileUrl exists on ReadFileValue
-    if (this._clipboard.copy(url)) {
-      this.copied.set(true);
-      this.copyStatus.set('Copied!');
-
-      // Reset after 2 seconds
-      setTimeout(() => {
-        this.copied.set(false);
-        this.copyStatus.set('Copy URL to clipboard');
-      }, 2000);
-    }
+    const link = this._rs.getIngestUrl(this.src, this.parentResource);
   }
 }
