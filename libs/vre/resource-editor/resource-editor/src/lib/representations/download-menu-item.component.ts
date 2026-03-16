35f112bad18cc0e3f41f22a55a01f85837c5cfcb
Julien Schneider <julien.schneider.1991@gmail.com>
2026-03-16 17:11:10 +0100

    fix: add correcty "copy link" button to resources (#2890)

--- libs/vre/resource-editor/resource-editor/src/lib/representations/download-menu-item.component.ts
+++ libs/vre/resource-editor/resource-editor/src/lib/representations/download-menu-item.component.ts
@@ -1,26 +1,50 @@
-import { Component, Input } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { Component, inject, Input } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { ReadFileValue, ReadResource } from '@dasch-swiss/dsp-js';
-import { TranslatePipe } from '@ngx-translate/core';
+import { NotificationService } from '@dasch-swiss/vre/ui/notification';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { RepresentationService } from './representation.service';
 
 @Component({
   selector: 'app-download-menu-item',
-  imports: [TranslatePipe, MatButton, MatIcon],
+  standalone: true,
+  imports: [MatButton, MatIcon, TranslatePipe],
   template: `
-    <button mat-flat-button (click)="download()" data-cy="download-file-button">
-      <mat-icon>download</mat-icon>
-      {{ 'resourceEditor.representations.downloadFile' | translate }}
-    </button>
+    <div style="display: flex; gap: 16px">
+      <button mat-flat-button (click)="download()">
+        <mat-icon>download</mat-icon>
+        {{ 'resourceEditor.representations.downloadFile' | translate }}
+      </button>
+
+      <button mat-flat-button (click)="copyUrl()">
+        <mat-icon>link</mat-icon>
+        {{ 'resourceEditor.representations.copyLink' | translate }}
+      </button>
+    </div>
   `,
 })
 export class DownloadMenuItemComponent {
   @Input({ required: true }) src!: ReadFileValue;
   @Input({ required: true }) parentResource!: ReadResource;
 
-  constructor(private readonly _rs: RepresentationService) {}
+  private readonly _translateService = inject(TranslateService);
+
+  constructor(
+    private readonly _rs: RepresentationService,
+    private _clipboard: Clipboard,
+    private readonly _notification: NotificationService
+  ) {}
+
   download() {
     this._rs.downloadProjectFile(this.src, this.parentResource);
+  }
+
+  copyUrl() {
+    this._rs.getIngestOriginalUrl(this.src, this.parentResource).subscribe(link => {
+      this._clipboard.copy(link);
+      this._notification.openSnackBar(this._translateService.instant('resourceEditor.representations.fileLinkCopied'));
+    });
   }
 }
